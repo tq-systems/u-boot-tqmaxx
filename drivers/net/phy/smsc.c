23af3d2db3b7e9b59f30148ce02b5feee0345e51
Markus Niebel <Markus.Niebel@tq-group.com>
2015-05-11 17:27:56 +0200

    net: phy: add SMSC8820 Phy
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>

--- drivers/net/phy/smsc.c
+++ drivers/net/phy/smsc.c
@@ -93,6 +93,50 @@ static struct phy_driver lan8742_driver = {
 	.shutdown = &genphy_shutdown,
 };
 
+/*
+ * SMSC8820
+ */
+#define SMSC8820_ARAP_ADDR	0x007F
+#define SMSC8820_ARAP_READ	0x8180
+
+#define SMSC8820_ARAP		0x14
+#define SMSC8820_ARRDP		0x15
+
+/* Accessors to extended registers*/
+int smsc8820_extended_read(struct phy_device *phydev, int regnum)
+{
+	if ((regnum & SMSC8820_ARAP_ADDR) != SMSC8820_ARAP_ADDR)
+		return -1;
+	phy_write(phydev, MDIO_DEVAD_NONE, SMSC8820_ARAP,
+		  regnum | SMSC8820_ARAP_READ);
+	return phy_read(phydev, MDIO_DEVAD_NONE, SMSC8820_ARRDP);
+}
+
+static int smsc8820_extread(struct phy_device *phydev,
+			    int addr, int devad, int reg)
+{
+	return smsc8820_extended_read(phydev, reg);
+};
+
+static int smsc8820_extwrite(struct phy_device *phydev, int addr,
+			     int devad, int reg, u16 val)
+{
+	return -1;
+};
+
+static struct phy_driver lan8820_driver = {
+	.name = "SMSC LAN8820",
+	.uid = 0x0007c0e0,
+	.mask = 0xffffff0,
+	.features = PHY_GBIT_FEATURES,
+	.config = &genphy_config_aneg,
+	.startup = &genphy_startup,
+	.shutdown = &genphy_shutdown,
+
+	.writeext = &smsc8820_extwrite,
+	.readext = &smsc8820_extread,
+};
+
 int phy_smsc_init(void)
 {
 	phy_register(&lan8710_driver);
@@ -100,6 +144,7 @@ int phy_smsc_init(void)
 	phy_register(&lan8700_driver);
 	phy_register(&lan8740_driver);
 	phy_register(&lan8742_driver);
+	phy_register(&lan8820_driver);
 
 	return 0;
 }
