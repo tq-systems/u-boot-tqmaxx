c87dc24ca69f8afdd6994bf96579b3521f5ded05
Markus Niebel <Markus.Niebel@tq-group.com>
2015-06-12 08:41:13 +0200

    imx6: tqma6: rely on default setting for tftp and nfs
    Playing with USB-to-Ethernet dongles it turns out,
    that these will not work with special settings
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- include/configs/tqma6.h
+++ include/configs/tqma6.h
@@ -138,10 +138,6 @@
 #define CONFIG_MII
 
 #define CONFIG_ARP_TIMEOUT		200UL
-/* Network config - Allow larger/faster download for TFTP/NFS */
-#define CONFIG_IP_DEFRAG
-#define CONFIG_TFTP_BLOCKSIZE	4096
-#define CONFIG_NFS_READ_SIZE	4096
 
 /* allow to overwrite serial and ethaddr */
 #define CONFIG_ENV_OVERWRITE
