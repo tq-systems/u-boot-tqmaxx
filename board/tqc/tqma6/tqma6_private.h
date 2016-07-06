8543a3f5668d144d5a4ee7bb88156c920e6e557b
Markus Niebel <Markus.Niebel@tq-group.com>
2016-07-06 11:46:07 +0200

    board/tqc: make baseboard header common
    This is done in preparation for more boards. Following transformations:
    
    - rename header to reflect common usage
    - rename functions to reflect common usage
    - factor out RTQMa6 specific stuff to own header
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

---
+++ board/tqc/tqma6/tqma6_private.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2013, 2014, 2016 TQ Systems
+ * Author: Markus Niebel <markus.niebel@tq-group.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __TQMA6_PRIVATE__
+#define __TQMA6_PRIVATE__
+
+#include <common.h>
+
+int tqma6_get_enet_workaround(void);
+int tqma6_get_system_i2c_bus(void);
+
+#endif
