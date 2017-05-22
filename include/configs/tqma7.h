a759f98eff89b698049db15ed929931b91eaf6ae
Markus Niebel <Markus.Niebel@tq-group.com>
2017-05-22 12:29:09 +0200

    tqma7: add mfgtool support
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    (cherry picked from commit 96353961cf56c89e7b9b53ed7aeebdee937b8db9)

--- include/configs/tqma7.h
+++ include/configs/tqma7.h
@@ -164,6 +164,25 @@
 /* Size of malloc() pool */
 #define CONFIG_SYS_MALLOC_LEN		(32 * SZ_1M)
 
+/*
+ * above 128 MiB offset as in ARM related docu for linux suggested
+ * DTB is loaded at 128 MiB, so use just 16 MiB more
+ */
+#define TQMA7_INITRD_ADDRESS		0x89000000
+
+#define TQMA7_MFG_ENV_SETTINGS \
+	"mfgtool_args=setenv bootargs console=" CONFIG_CONSOLE_DEV ",115200 "    \
+		"rdinit=/linuxrc "                                               \
+		"g_mass_storage.stall=0 g_mass_storage.removable=1 "             \
+		"g_mass_storage.file=/fat g_mass_storage.ro=1 "                  \
+		"g_mass_storage.idVendor=0x066F g_mass_storage.idProduct=0x37FF "\
+		"g_mass_storage.iSerialNumber=\"\" "                             \
+		"enable_wait_mode=off "                                          \
+		"\0"                                                             \
+		"initrd_addr="__stringify(TQMA7_INITRD_ADDRESS)"\0"              \
+		"initrd_high=0xffffffff\0"                                       \
+		"bootcmd_mfg=run mfgtool_args;bootz ${loadaddr} ${initrd_addr} ${fdt_addr};\0" \
+
 #if defined(CONFIG_TQMA7_MMC_BOOT)
 
 #define CONFIG_ENV_IS_IN_MMC
@@ -474,6 +493,7 @@
 		"run loadfdtfit; "                                             \
 		"else run loadfdtsingle; fi\0"                                 \
 	TQMA7_EXTRA_BOOTDEV_ENV_SETTINGS                                       \
+	TQMA7_MFG_ENV_SETTINGS                                                 \
 
 /* Miscellaneous configurable options */
 #define CONFIG_SYS_LONGHELP
