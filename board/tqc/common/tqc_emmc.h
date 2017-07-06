cb17ef55d99100532c402690274e91843db022c3
Markus Niebel <Markus.Niebel@tq-group.com>
2017-07-06 16:23:41 +0200

    tqc: add shared e-MMC helper funtions to tqc common dir
    This will be used in a later patch to decide whether we have to
    use DSR or not for e-MMC
    
    also prepare reuse of dt fixup for dsr
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    (cherry picked from commit 6cabd9eca25058e374e241f6c94ccd0c002bf5b6)
    Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>

---
+++ board/tqc/common/tqc_emmc.h
@@ -0,0 +1,13 @@
+/*
+ * (C) Copyright 2017 TQ Systems GmbH
+ * Markus Niebel <Markus.Niebel@tq-group.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#ifndef __TQC_EMMC_H__
+#define __TQC_EMMC_H__
+
+int tqc_emmc_need_dsr(struct mmc *mmc);
+int tqc_ft_fixup_emmc_dsr(void *blob, const char *path, u32 value);
+
+#endif /* __TQC_EMMC_H__ */
