336857b77ec5d63dc3e1faa3d5770be3d8545948
Markus Niebel <Markus.Niebel@tq-group.com>
2017-11-10 16:13:33 +0100

    tqma6: nav: disable more stuff for secure boot
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- include/configs/tqma6_nav.h
+++ include/configs/tqma6_nav.h
@@ -26,12 +26,39 @@
 
 /*
  * Remove all unused interfaces / commands that are defined in
- * the common header tqms6.h
+ * the common header tqma6.h
  */
 #undef CONFIG_CMD_SF
 #undef CONFIG_CMD_SPI
 #undef CONFIG_MXC_SPI
+
+#if defined(CONFIG_SECURE_BOOT)
 #undef CONFIG_BMODE
 #undef CONFIG_FIT
+
+#undef CONFIG_SUPPORT_EMMC_RPMB
+#undef CONFIG_FEC_MXC
+#undef CONFIG_CMD_PING
+#undef CONFIG_CMD_DHCP
+#undef CONFIG_CMD_MII
+#undef CONFIG_PHYLIB
+#undef CONFIG_MII
+#undef CMD_I2C
+#undef CMD_DTT
+#undef CMD_EEPROM
+
+#undef CONFIG_USB_HOST_ETHER
+
+#undef CONFIG_ENV_OVERWRITE
+
+#define CONFIG_SILENT_CONSOLE
+#define CONFIG_SYS_DEVICE_NULLDEV
+/* TODO: set to -1 to force no wait at all */
+#if defined(CONFIG_BOOTDELAY)
+#undef CONFIG_BOOTDELAY
+#endif
+#define CONFIG_BOOTDELAY 0
+
+#endif
 
 #endif /* __CONFIG_TQMA6_NAV_H */
