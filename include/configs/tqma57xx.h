868b5e38f6398b2a225fb8879168df46eba46aea
Stefan Lange <s.lange@gateware.de>
2018-04-12 10:46:41 +0200

    [cosmetic]: tqma57xx:cleanup and rearrange header file
    Cleanup and rearrange header file for better readability.
    Remove unsupported/unnecessary configuration.
    
    Signed-off-by: Stefan Lange <s.lange@gateware.de>

--- include/configs/tqma57xx.h
+++ include/configs/tqma57xx.h
@@ -27,19 +27,13 @@
 
 #define CONFIG_SYS_OMAP_ABE_SYSCK
 
-/* MMC ENV related defines */
-#define CONFIG_SYS_MMC_ENV_DEV		1		/* eMMC */
-#define CONFIG_SYS_MMC_ENV_PART		0
-#define CONFIG_ENV_SIZE			SZ_128K
-#define CONFIG_ENV_OFFSET		0x260000
-#define CONFIG_ENV_OFFSET_REDUND	(CONFIG_ENV_OFFSET + CONFIG_ENV_SIZE)
-#define CONFIG_SYS_REDUNDAND_ENVIRONMENT
-
 /* console */
 #define CONSOLEDEV			"ttyO2"
-#define CONFIG_SYS_NS16550_COM1		UART1_BASE	/* Base EVM has UART0 */
-#define CONFIG_SYS_NS16550_COM2		UART2_BASE	/* UART2 */
-#define CONFIG_SYS_NS16550_COM3		UART3_BASE	/* UART3 */
+#define CONFIG_SYS_NS16550_COM1                UART1_BASE      /* UART0 */
+#define CONFIG_SYS_NS16550_COM2                UART2_BASE      /* UART2 */
+#define CONFIG_SYS_NS16550_COM3                UART3_BASE      /* UART3 */
+
+#include <configs/ti_omap5_common.h>
 
 /* i2c eeprom */
 #define CONFIG_ENV_EEPROM_IS_ON_I2C
@@ -51,39 +45,6 @@
 
 /* PCA9555 GPIO expander support */
 #define CONFIG_PCA953X
-
-/* Define the default GPT table for eMMC */
-#define PARTS_DEFAULT \
-	/* Linux partitions */ \
-	"uuid_disk=${uuid_gpt_disk};" \
-	"name=bootloader,start=384K,size=1792K,uuid=${uuid_gpt_bootloader};" \
-	"name=rootfs,start=2688K,size=-,uuid=${uuid_gpt_rootfs}\0" \
-	/* Android partitions */ \
-	"partitions_android=" \
-	"uuid_disk=${uuid_gpt_disk};" \
-	"name=xloader,start=128K,size=256K,uuid=${uuid_gpt_xloader};" \
-	"name=bootloader,size=1792K,uuid=${uuid_gpt_bootloader};" \
-	"name=environment,size=128K,uuid=${uuid_gpt_environment};" \
-	"name=misc,size=128K,uuid=${uuid_gpt_misc};" \
-	"name=reserved,size=256K,uuid=${uuid_gpt_reserved};" \
-	"name=efs,size=16M,uuid=${uuid_gpt_efs};" \
-	"name=crypto,size=16K,uuid=${uuid_gpt_crypto};" \
-	"name=recovery,size=10M,uuid=${uuid_gpt_recovery};" \
-	"name=boot,size=10M,uuid=${uuid_gpt_boot};" \
-	"name=system,size=768M,uuid=${uuid_gpt_system};" \
-	"name=vendor,size=256M,uuid=${uuid_gpt_vendor};" \
-	"name=cache,size=256M,uuid=${uuid_gpt_cache};" \
-	"name=ipu1,size=1M,uuid=${uuid_gpt_ipu1};" \
-	"name=ipu2,size=1M,uuid=${uuid_gpt_ipu2};" \
-	"name=userdata,size=-,uuid=${uuid_gpt_userdata}"
-
-#define DFUARGS \
-	"dfu_bufsiz=0x10000\0" \
-	DFU_ALT_INFO_MMC \
-	DFU_ALT_INFO_EMMC \
-	DFU_ALT_INFO_RAM \
-
-#include <configs/ti_omap5_common.h>
 
 /* SD/eMMC */
 #define CONFIG_SUPPORT_EMMC_BOOT
@@ -114,6 +75,17 @@
 #define CONFIG_SYS_SCSI_MAX_DEVICE	(CONFIG_SYS_SCSI_MAX_SCSI_ID * \
 						CONFIG_SYS_SCSI_MAX_LUN)
 
+/* SPI */
+#define CONFIG_TI_SPI_MMAP
+#define CONFIG_SF_DEFAULT_SPEED                76800000
+#define CONFIG_SF_DEFAULT_MODE                 SPI_MODE_0
+#define CONFIG_QSPI_QUAD_SUPPORT
+
+/* SPI SPL */
+#define CONFIG_TI_EDMA3
+#define CONFIG_SPL_SPI_LOAD
+#define CONFIG_SYS_SPI_U_BOOT_OFFS     0x40000
+
 /*
  * Default to using SPI for environment, etc.
  * 0x000000 - 0x040000 : QSPI.SPL (256KiB)
@@ -128,15 +100,12 @@
 #define CONFIG_SYS_SPI_ARGS_OFFS        0x140000
 #define CONFIG_SYS_SPI_ARGS_SIZE        0x80000
 
-/* SPI SPL */
-#define CONFIG_TI_EDMA3
-#define CONFIG_SPL_SPI_LOAD
-#define CONFIG_SYS_SPI_U_BOOT_OFFS     0x40000
-
-/* SPI */
-#define CONFIG_TI_SPI_MMAP
-#define CONFIG_SF_DEFAULT_SPEED                76800000
-#define CONFIG_SF_DEFAULT_MODE                 SPI_MODE_0
-#define CONFIG_QSPI_QUAD_SUPPORT
+/* MMC ENV related defines */
+#define CONFIG_SYS_MMC_ENV_DEV          1               /* eMMC */
+#define CONFIG_SYS_MMC_ENV_PART         0
+#define CONFIG_ENV_SIZE                 SZ_128K
+#define CONFIG_ENV_OFFSET               0x260000
+#define CONFIG_ENV_OFFSET_REDUND        (CONFIG_ENV_OFFSET + CONFIG_ENV_SIZE)
+#define CONFIG_SYS_REDUNDAND_ENVIRONMENT
 
 #endif /* __CONFIG_TQMA57XX_H */
