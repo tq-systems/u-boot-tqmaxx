141b5924fa1c265036650edc4a40c8e35252b2a3
Stefan Lange <s.lange@gateware.de>
2018-04-12 10:52:09 +0200

    tqma572x_mba57xx: change mmc wp, cd pin function to gpio
    Change mmc1 wp and mmc1 cd pin function to gpio as kernel device tree
    uses gpios for these features.
    
    Signed-off-by: Stefan Lange <s.lange@gateware.de>

--- board/tqc/tqma57xx/tqma57xx_mba57xx.c
+++ board/tqc/tqma57xx/tqma57xx_mba57xx.c
@@ -150,8 +150,8 @@ const struct pad_conf_entry core_padconf_array_bb_tqma57xx[] = {
 	{MMC1_DAT1, (M0 | PIN_INPUT_PULLUP)},   /* mmc1_dat1.mmc1_dat1 */
 	{MMC1_DAT2, (M0 | PIN_INPUT_PULLUP)},   /* mmc1_dat2.mmc1_dat2 */
 	{MMC1_DAT3, (M0 | PIN_INPUT_PULLUP)},   /* mmc1_dat3.mmc1_dat3 */
-	{MMC1_SDCD, (M0 | PIN_INPUT | SLEWCONTROL)},   /* mmc1_sdcd.mmc1_sdcd */
-	{MMC1_SDWP, (M0 | PIN_INPUT | SLEWCONTROL)},   /* mmc1_sdwp.mmc1_sdwp */
+	{MMC1_SDCD, (M15 | PIN_INPUT | SLEWCONTROL)},   /* mmc1_sdcd.gpio6_27 */
+	{MMC1_SDWP, (M15 | PIN_INPUT | SLEWCONTROL)},   /* mmc1_sdwp.gpio6_28 */
 
 	/* HDMI */
 	{SPI1_CS2, (M6 | PIN_INPUT_PULLUP)},     /* spi1_cs2.hdmi1_hpd */
