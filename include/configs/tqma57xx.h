58e0806f6001e2560b58a8a86bdeb2a5dc502ba1
Stefan Lange <s.lange@gateware.de>
2018-05-03 13:10:22 +0200

    tqma57xx: customize default u-boot environment
    Set a basic default u-boot environment for the TQMa57xx.
    This leaves out some of the default TI environment variables and is
    thus a little more slim.
    
    Some of the contents of "configs/ti_omap5_common.h"
    have been moved to "configs/tqma57xx.h"
    to achieve this.
    
    Signed-off-by: Stefan Lange <s.lange@gateware.de>

--- include/configs/tqma57xx.h
+++ include/configs/tqma57xx.h
@@ -33,7 +33,74 @@
 #define CONFIG_SYS_NS16550_COM2                UART2_BASE      /* UART2 */
 #define CONFIG_SYS_NS16550_COM3                UART3_BASE      /* UART3 */
 
-#include <configs/ti_omap5_common.h>
+/* Use General purpose timer 1 */
+#define CONFIG_SYS_TIMERBASE            GPT2_BASE
+
+/*
+ * For the DDR timing information we can either dynamically determine
+ * the timings to use or use pre-determined timings (based on using the
+ * dynamic method.  Default to the static timing information.
+ */
+#define CONFIG_SYS_EMIF_PRECALCULATED_TIMING_REGS
+#ifndef CONFIG_SYS_EMIF_PRECALCULATED_TIMING_REGS
+#define CONFIG_SYS_AUTOMATIC_SDRAM_DETECTION
+#define CONFIG_SYS_DEFAULT_LPDDR2_TIMINGS
+#endif
+
+#define CONFIG_PALMAS_POWER
+
+#include <asm/arch/cpu.h>
+#include <asm/arch/omap.h>
+
+#include <configs/ti_armv7_omap.h>
+
+/*
+ * Hardware drivers
+ */
+#define CONFIG_SYS_NS16550_CLK          48000000
+#if !defined(CONFIG_DM_SERIAL)
+#define CONFIG_SYS_NS16550_SERIAL
+#define CONFIG_SYS_NS16550_REG_SIZE     (-4)
+#endif
+
+/*
+ * SPL related defines.  The Public RAM memory map the ROM defines the
+ * area between 0x40300000 and 0x4031E000 as a download area for OMAP5.
+ * On DRA7xx/AM57XX the download area is between 0x40300000 and 0x4037E000.
+ * We set CONFIG_SPL_DISPLAY_PRINT to have omap_rev_string() called and
+ * print some information.
+ */
+#ifdef CONFIG_TI_SECURE_DEVICE
+/*
+ * For memory booting on HS parts, the first 4KB of the internal RAM is
+ * reserved for secure world use and the flash loader image is
+ * preceded by a secure certificate. The SPL will therefore run in internal
+ * RAM from address 0x40301350 (0x40300000+0x1000(reserved)+0x350(cert)).
+ */
+#define TI_OMAP5_SECURE_BOOT_RESV_SRAM_SZ       0x1000
+#define CONFIG_SPL_TEXT_BASE    0x40301350
+/* If no specific start address is specified then the secure EMIF
+ * region will be placed at the end of the DDR space. In order to prevent
+ * the main u-boot relocation from clobbering that memory and causing a
+ * firewall violation, we tell u-boot that memory is protected RAM (PRAM)
+ */
+#if (CONFIG_TI_SECURE_EMIF_REGION_START == 0)
+#define CONFIG_PRAM (CONFIG_TI_SECURE_EMIF_TOTAL_REGION_SIZE) >> 10
+#endif
+#else
+/*
+ * For all booting on GP parts, the flash loader image is
+ * downloaded into internal RAM at address 0x40300000.
+ */
+#define CONFIG_SPL_TEXT_BASE    0x40300000
+#endif
+
+#define CONFIG_SYS_SPL_ARGS_ADDR        (CONFIG_SYS_SDRAM_BASE + \
+					 (128 << 20))
+
+#ifdef CONFIG_SPL_BUILD
+#undef CONFIG_TIMER
+#endif
 
 /* i2c eeprom */
 #define CONFIG_ENV_EEPROM_IS_ON_I2C
@@ -117,4 +184,62 @@
 #define CONFIG_ENV_OFFSET_REDUND        (CONFIG_ENV_OFFSET + \
 					 CONFIG_ENV_SECT_SIZE)
 
+/* Default environment */
+#include <environment/ti/boot.h>
+#include <environment/ti/mmc.h>
+
+#ifdef CONFIG_CMD_NET
+#define NETARGS_TQMA57XX \
+	"static_ip=${ipaddr}:${serverip}:${gatewayip}:${netmask}:${hostname}" \
+		"::off\0" \
+	"nfsopts=nolock\0" \
+	"rootpath=/srv/nfs/rootfs\0" \
+	"netloadimage=nfs ${loadaddr} $serverip:$rootpath$bootdir/$bootfile\0" \
+	"netloadfdt=nfs ${fdtaddr} $serverip:$rootpath$bootdir/$fdtfile\0" \
+	"netargs=setenv bootargs console=${console} " \
+		"${optargs} " \
+		"root=/dev/nfs " \
+		"nfsroot=${serverip}:${rootpath},${nfsopts} rw " \
+		"ip=dhcp\0" \
+	"netboot=echo Booting from network ...; " \
+		"setenv autoload no; " \
+		"dhcp; " \
+		"run netloadimage; " \
+		"run netloadfdt; " \
+		"run netargs; " \
+		"bootz ${loadaddr} - ${fdtaddr}\0"
+#else
+#define NETARGS_TQMA57XX ""
+#endif
+
+#define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	DEFAULT_LINUX_BOOT_ENV \
+	DEFAULT_MMC_TI_ARGS \
+	DEFAULT_COMMON_BOOT_TI_ARGS \
+	DEFAULT_FIT_TI_ARGS \
+	NETARGS_TQMA57XX \
+	"bootfile=zImage_tqma572x-mba57xx.bin \0" \
+	"fdtfile=tqma572x-mba57xx.dtb \0" \
+	"devtype=mmc \0" \
+	"upd_sd=if tftp u-boot/u-boot_tqma572x.img; " \
+		"then echo updating u-boot on sd card...; " \
+		"fatwrite mmc 0:1 $loadaddr u-boot.img $filesize; " \
+		"tftp u-boot/MLO_tqma572x; " \
+		"fatwrite mmc 0:1 $loadaddr MLO $filesize; " \
+		" else echo u-boot file not found!; fi \0" \
+	"upd_spi=if tftp u-boot/u-boot_tqma572x.img; " \
+		"then echo updating u-boot on spi flash...; " \
+		"sf probe 0; sf erase 0x00000 0x100000; " \
+		"sf write ${loadaddr} 0x40000 ${filesize}; " \
+		"tftp u-boot/MLO_tqma572x; " \
+		"sf write ${loadaddr} 0x00000 ${filesize}; " \
+		"else echo u-boot file not found!; fi \0" \
+	"bootcmd=if test ${dofastboot} -eq 1; then echo " \
+		"Boot fastboot requested, resetting dofastboot ...; " \
+		"setenv dofastboot 0; saveenv; " \
+		"echo Booting into fastboot ...; fastboot 1; fi; " \
+		"if test ${boot_fit} -eq 1; then run update_to_fit;fi; " \
+		"run envboot; run mmcboot; \0 " \
+	"boot_fit=0\0"
 #endif /* __CONFIG_TQMA57XX_H */
