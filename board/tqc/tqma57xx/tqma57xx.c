c042b8a68c9a59309b75c71417fc19977b2a46c6
Stefan Lange <s.lange@gateware.de>
2018-05-08 09:48:40 +0200

    Add support for the TQMa571x module on a MBa57xx base board
    TQMa57xx is a family of modules based on TI AM57xx.
    
    AM571x and AM572x share the same pinout.
    AM571x only has one EMIF (DDR3 controller bank). This one runs on
    different clock and timings as the AM572x.
    
    Signed-off-by: Stefan Lange <s.lange@gateware.de>

--- board/tqc/tqma57xx/tqma57xx.c
+++ board/tqc/tqma57xx/tqma57xx.c
@@ -49,48 +49,60 @@ const struct omap_sysinfo sysinfo = {
 	"Board: UNKNOWN TQMa57xx ??? REV UNKNOWN ?????\n"
 };
 
-static const struct dmm_lisa_map_regs tqma57xx_lisa_regs = {
+#ifdef CONFIG_TQMA572X
+static const struct dmm_lisa_map_regs tqma572x_lisa_regs = {
 	.dmm_lisa_map_3 = 0x80740300,
 	.is_ma_present  = 0x1
 };
+#elif CONFIG_TQMA571X
+static const struct dmm_lisa_map_regs tqma571x_lisa_regs = {
+	.dmm_lisa_map_3 = 0x80600100,
+	.is_ma_present  = 0x1
+};
+#endif
 
 void emif_get_dmm_regs(const struct dmm_lisa_map_regs **dmm_lisa_regs)
 {
-		*dmm_lisa_regs = &tqma57xx_lisa_regs;
+#ifdef CONFIG_TQMA572X
+	*dmm_lisa_regs = &tqma572x_lisa_regs;
+#elif CONFIG_TQMA571X
+	*dmm_lisa_regs = &tqma571x_lisa_regs;
+#endif
 }
 
 void emif_get_reg_dump(u32 emif_nr, const struct emif_regs **regs)
 {
-#ifdef CONFIG_TQMA572X
 	switch (emif_nr) {
 	case 1:
+#ifdef CONFIG_TQMA572X
 		*regs = &tqma572x_emif1_ddr3_532mhz_emif_regs;
+#elif CONFIG_TQMA571X
+		*regs = &tqma571x_emif1_ddr3_666mhz_emif_regs;
+#endif
 		break;
 	case 2:
 		*regs = &tqma572x_emif2_ddr3_532mhz_emif_regs;
 		break;
 	}
-#else
-	printf("No ddr config values implemented\n");
-#endif
 }
 
 void emif_get_ext_phy_ctrl_const_regs(u32 emif_nr, const u32 **regs, u32 *size)
 {
-#ifdef CONFIG_TQMA572X
 	switch (emif_nr) {
 	case 1:
+#ifdef CONFIG_TQMA572X
 		*regs = tqma572x_emif1_ddr3_ext_phy_ctrl_const_regs;
 		*size = ARRAY_SIZE(tqma572x_emif1_ddr3_ext_phy_ctrl_const_regs);
+#elif CONFIG_TQMA571X
+		*regs = tqma571x_emif1_ddr3_ext_phy_ctrl_const_regs;
+		*size = ARRAY_SIZE(tqma571x_emif1_ddr3_ext_phy_ctrl_const_regs);
+#endif
 		break;
 	case 2:
 		*regs = tqma572x_emif2_ddr3_ext_phy_ctrl_const_regs;
 		*size = ARRAY_SIZE(tqma572x_emif2_ddr3_ext_phy_ctrl_const_regs);
 		break;
 	}
-#else
-	printf("No ddr config values implemented\n");
-#endif
 }
 
 struct vcores_data tqma57xx_volts = {
