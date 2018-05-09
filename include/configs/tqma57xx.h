c80193736eaf1ef217403f632a4ebe90d630cfd0
Stefan Lange <s.lange@gateware.de>
2018-05-09 15:13:31 +0200

    tqma57xx: default env: set filenames conformant to yocto-generated
    Set filenames in u-boot default environment to filenames generated
    by yocto.
    
    Signed-off-by: Stefan Lange <s.lange@gateware.de>

--- include/configs/tqma57xx.h
+++ include/configs/tqma57xx.h
@@ -214,12 +214,12 @@
 
 #ifdef CONFIG_TQMA572X
 #define BOOTFILES_TQMA57XX \
-	"fdtfile=tqma572x-mba57xx.dtb \0" \
+	"fdtfile=zImage-tqma572x-mba57xx.dtb \0" \
 	"u-boot=u-boot_tqma572x.img \0" \
 	"MLO=MLO_tqma572x \0"
 #elif CONFIG_TQMA571X
 #define BOOTFILES_TQMA57XX \
-	"fdtfile=tqma571x-mba57xx.dtb \0" \
+	"fdtfile=zImage-tqma571x-mba57xx.dtb \0" \
 	"u-boot=u-boot_tqma571x.img \0" \
 	"MLO=MLO_tqma571x \0"
 #endif
