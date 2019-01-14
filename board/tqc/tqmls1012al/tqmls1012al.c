56bdd35181e81e6496dc64c89b10b0496d491313
Max Merchel <Max.Merchel@tq-group.com>
2019-01-14 09:11:01 +0100

    tqmls1012al: read modul and board information from EEPROM
    Signed-off-by: Max Merchel <Max.Merchel@tq-group.com>

--- board/tqc/tqmls1012al/tqmls1012al.c
+++ board/tqc/tqmls1012al/tqmls1012al.c
@@ -129,32 +129,33 @@ int enable_i2c_clk(unsigned char enable, unsigned int i2c_num)
 int misc_init_r(void)
 {
 	int ret = -1;
-	struct tqmaxx_eeprom_data eedat;
+	struct tqmaxx_eeprom_data eedat_modul, eedat_board;
 	/* must hold largest field of eeprom data */
 	char safe_string[0x41];
 
 	ret = tqmaxx_read_eeprom(CONFIG_SYS_I2C_EEPROM_BUS,
-				 TQMLS1012AL_I2C_EEPROM1_ADDR, &eedat);
+				 TQMLS1012AL_I2C_EEPROM1_ADDR, &eedat_modul);
 
 	if (ret) {
 		printf("EEPROM: (0x%x) err %d\n", TQMLS1012AL_I2C_EEPROM1_ADDR,
 		       ret);
 	} else {
-		/* ID */
-		tqmaxx_parse_eeprom_id(&eedat, safe_string,
+		/* Modul ID */
+		tqmaxx_parse_eeprom_id(&eedat_modul, safe_string,
 				       ARRAY_SIZE(safe_string));
-		if (strncmp(safe_string, "TQM", 3) == 0)
-			env_set("boardtype", safe_string);
+		if (strncmp(safe_string, "TQMLS1012AL", 11) == 0)
+			env_set("modultype", safe_string);
 
-		/* Serial# */
-		if (tqmaxx_parse_eeprom_serial(&eedat, safe_string,
-					       ARRAY_SIZE(safe_string)) == 0)
+		/* Modul Serial# */
+		if (tqmaxx_parse_eeprom_serial(&eedat_modul, safe_string,
+							  ARRAY_SIZE(safe_string
+								     )) == 0)
 			env_set("serial#", safe_string);
 		else
 			env_set("serial#", "???");
 
-		/* MAC */
-		if (tqmaxx_parse_eeprom_mac(&eedat, safe_string,
+		/* Modul MAC */
+		if (tqmaxx_parse_eeprom_mac(&eedat_modul, safe_string,
 					    ARRAY_SIZE(safe_string)) == 0) {
 			u32 mac = 0;
 			u8 addr[6];
@@ -183,8 +184,74 @@ int misc_init_r(void)
 				eth_env_set_enetaddr("eth1addr", addr);
 			}
 		}
+	}
 
-		tqmaxx_show_eeprom(&eedat, "TQM");
+	ret = tqmaxx_read_eeprom_at(CONFIG_SYS_I2C_EEPROM_BUS,
+				    TQMLS1012AL_I2C_EEPROM1_ADDR,
+				    &eedat_board, 0x80);
+
+	if (ret) {
+		printf("EEPROM: (0x%x) err %d\n", TQMLS1012AL_I2C_EEPROM1_ADDR,
+		       ret);
+	} else {
+		/* Board ID */
+		tqmaxx_parse_eeprom_id(&eedat_board, safe_string,
+				       ARRAY_SIZE(safe_string));
+		if (strncmp(safe_string, "MBLS1012AL", 10) == 0)
+			env_set("boardtype", safe_string);
+
+		/* Board Serial# */
+		if (tqmaxx_parse_eeprom_serial(&eedat_board, safe_string,
+					       ARRAY_SIZE(safe_string)) == 0)
+			env_set("boardserial#", safe_string);
+		else
+			env_set("boardserial#", "???");
+
+		/* Board MAC */
+		if (tqmaxx_parse_eeprom_mac(&eedat_board, safe_string,
+					    ARRAY_SIZE(safe_string)) == 0) {
+			u32 mac = 0;
+			u8 addr[6];
+			char *eth2addr = env_get("eth2addr");
+
+			if (eth2addr &&
+			    strncmp(safe_string, eth2addr, 18)) {
+				printf("\n");
+				printf("Warning: MAC addresses don't match:\n");
+				printf("Address in EEPROM is      %s\n",
+				       safe_string);
+				printf("Address in environment is %s\n",
+				       eth2addr);
+			} else {
+				env_set("eth2addr", safe_string);
+
+				/* Increment ethaddr for eth1addr, eth2addr */
+				eth_parse_enetaddr(safe_string, addr);
+				mac = (addr[3] << 16) | (addr[4] << 8) |
+				       addr[5];
+
+				mac++;
+				addr[3] = (uint8_t)(mac >> 16);
+				addr[4] = (uint8_t)(mac >>  8);
+				addr[5] = (uint8_t)(mac >>  0);
+				eth_env_set_enetaddr("eth3addr", addr);
+
+				mac++;
+				addr[3] = (uint8_t)(mac >> 16);
+				addr[4] = (uint8_t)(mac >>  8);
+				addr[5] = (uint8_t)(mac >>  0);
+				eth_env_set_enetaddr("eth4addr", addr);
+
+				mac++;
+				addr[3] = (uint8_t)(mac >> 16);
+				addr[4] = (uint8_t)(mac >>  8);
+				addr[5] = (uint8_t)(mac >>  0);
+				eth_env_set_enetaddr("eth5addr", addr);
+			}
+		}
+
+		tqmaxx_show_eeprom(&eedat_modul, "TQMLS1012AL");
+		tqmaxx_show_eeprom(&eedat_board, "MBLS1012AL");
 	}
 
 	tqmls1012al_bb_late_init();
