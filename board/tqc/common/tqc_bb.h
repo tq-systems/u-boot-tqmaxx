94505d9d398152c69b2bf1dc8bc9d632ffca3430
Markus Niebel <Markus.Niebel@tq-group.com>
2019-01-16 10:40:12 +0100

    board/tqc: common: prepare prototypes for split spl into module / baseboard part
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    (cherry picked from commit 5d25bfcccf3358a433fe77bf7d3d328254cd5b1f)
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tq-group.com>

--- board/tqc/common/tqc_bb.h
+++ board/tqc/common/tqc_bb.h
@@ -14,11 +14,16 @@ int tqc_bb_board_mmc_getwp(struct mmc *mmc);
 int tqc_bb_board_mmc_getcd(struct mmc *mmc);
 int tqc_bb_board_mmc_init(bd_t *bis);
 
+void tqc_bb_board_init_f(ulong);
 int tqc_bb_board_early_init_f(void);
 int tqc_bb_board_init(void);
 int tqc_bb_board_late_init(void);
 int tqc_bb_checkboard(void);
 const char *tqc_bb_get_boardname(void);
+
+#if defined(CONFIG_SPL_BUILD)
+void tqc_bb_spl_board_init(void);
+#endif
 
 #if defined(MX8QX)
 void tqc_bb_board_quiesce_devices(void);
