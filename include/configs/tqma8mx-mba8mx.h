3984fa165bbca0e9d319b8ea560866ded1154467
Markus Niebel <Markus.Niebel@tq-group.com>
2019-01-17 09:57:08 +0100

    boards: tqma8mx-mba8mx: enable net support per config
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- include/configs/tqma8mx-mba8mx.h
+++ include/configs/tqma8mx-mba8mx.h
@@ -7,6 +7,20 @@
 #if !defined(__TQMA8MX_MBA8MX_H)
 #define __TQMA8MX_MBA8MX_H
 
+/* ENET Config */
+/* ENET1 */
+#if defined(CONFIG_CMD_NET)
+
+#define CONFIG_MII
+#define CONFIG_ETHPRIME			"FEC"
+
+#define CONFIG_FEC_XCV_TYPE		RGMII
+#define CONFIG_FEC_MXC_PHYADDR		0
+#define FEC_QUIRK_ENET_MAC
+
+/* #define CONFIG_PHY_GIGE */
+#endif
+
 #if !defined(CONFIG_SPL_BUILD)
 /* #define CONFIG_DM_GPIO */
 #define CONFIG_DM_PCA953X
