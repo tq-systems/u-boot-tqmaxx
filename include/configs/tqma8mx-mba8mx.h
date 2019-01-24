ccffec7e759e5f1d0f121d241fdb4e235097b000
Markus Niebel <Markus.Niebel@tq-group.com>
2019-01-24 10:27:59 +0100

    HACK: tqma8mq: add video support
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- include/configs/tqma8mx-mba8mx.h
+++ include/configs/tqma8mx-mba8mx.h
@@ -26,4 +26,16 @@
 #define CONFIG_DM_PCA953X
 #endif
 
+/* Framebuffer */
+#ifdef CONFIG_VIDEO
+#define CONFIG_VIDEO_IMXDCSS
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_BMP_16BPP
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_VIDEO_BMP_LOGO
+#define CONFIG_IMX_VIDEO_SKIP
+#endif
+
 #endif /* __TQMA8MX_MBA8MX_H */
