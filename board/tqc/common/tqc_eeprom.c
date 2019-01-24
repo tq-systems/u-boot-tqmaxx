38678e492a4617f91ea58ace1e384f0705834799
Markus Niebel <Markus.Niebel@tq-group.com>
2019-01-24 14:36:56 +0100

    tqc: tqc_eeprom: add spl support
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>
    (cherry picked from commit 5b7bfbbe35b8e4a80afbf4b15b2041621b6bc4fc)
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tq-group.com>

--- board/tqc/common/tqc_eeprom.c
+++ board/tqc/common/tqc_eeprom.c
@@ -11,6 +11,42 @@
 
 #include "tqc_eeprom.h"
 
+/*
+ * read_eeprom - read the given EEPROM into memory
+ */
+int tqc_read_eeprom_buf(unsigned int bus, unsigned int i2c_addr,
+			unsigned int alen, unsigned int addr,
+			size_t bsize, uchar *buf)
+{
+	int ret;
+#ifdef CONFIG_DM_I2C
+	struct udevice *dev;
+#else
+	unsigned int oldbus;
+#endif
+
+	if (!buf)
+		return -1;
+
+#ifdef CONFIG_DM_I2C
+	ret = i2c_get_chip_for_busnum(bus, i2c_addr, alen, &dev);
+	if (ret) {
+		debug("%s: Cannot find I2C chip for bus %d\n", __func__, bus);
+		return ret;
+	}
+
+	ret = dm_i2c_read(dev, addr, buf, bsize);
+#else
+	oldbus = i2c_get_bus_num();
+	i2c_set_bus_num(bus);
+	ret = i2c_read(i2c_addr, addr, alen, buf, bsize);
+	i2c_set_bus_num(oldbus);
+#endif
+	return ret;
+}
+
+#if !defined(CONFIG_SPL_BUILD)
+
 int tqc_parse_eeprom_mac(struct tqc_eeprom_data *eeprom, char *buf,
 			 size_t len)
 {
@@ -113,31 +149,9 @@ int tqc_read_eeprom_at(unsigned int bus, unsigned int i2c_addr,
 		       unsigned int alen, unsigned int addr,
 		       struct tqc_eeprom_data *eeprom)
 {
-	int ret;
-#ifdef CONFIG_DM_I2C
-	struct udevice *dev;
-#else
-	unsigned int oldbus;
-#endif
-
-	if (!eeprom)
-		return -1;
-
-#ifdef CONFIG_DM_I2C
-	ret = i2c_get_chip_for_busnum(bus, i2c_addr, alen, &dev);
-	if (ret) {
-		debug("%s: Cannot find I2C chip for bus %d\n", __func__, bus);
-		return ret;
-	}
-
-	ret = dm_i2c_read(dev, addr, (uchar *)eeprom, sizeof(*eeprom));
-#else
-	oldbus = i2c_get_bus_num();
-	i2c_set_bus_num(bus);
-	ret = i2c_read(i2c_addr, addr, alen, (uchar *)eeprom, sizeof(*eeprom));
-	i2c_set_bus_num(oldbus);
-#endif
-	return ret;
+	return tqc_read_eeprom_buf(bus, i2c_addr, alen, addr, sizeof(*eeprom),
+				   (uchar *)eeprom)
+;
 }
 
 #if defined(CONFIG_SYS_I2C_EEPROM_ADDR_LEN)
@@ -148,4 +162,6 @@ int tqc_read_eeprom(unsigned int bus, unsigned int i2c_addr,
 				  CONFIG_SYS_I2C_EEPROM_ADDR_LEN,
 				  addr, eeprom);
 }
+#endif
+
 #endif
