ca71dd1cce7d050c273e72aa2b8d892cb2add8ee
Markus Niebel <Markus.Niebel@tq-group.com>
2019-04-10 15:04:22 +0200

    boards: tqma8qx: use device tree configured in KConfig for env
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- include/configs/tqma8qx-mba8xx.h
+++ include/configs/tqma8qx-mba8xx.h
@@ -24,6 +24,6 @@
 
 #define BB_ENV_SETTINGS \
 	"console=ttyLP1,115200 earlycon=lpuart32,5a070000,115200\0" \
-	"fdt_file=fsl-imx8qxp-tqma8qxp-mba8xx.dtb\0"
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0"
 
 #endif /* __TQMA8QX_MBA8XX_H */
