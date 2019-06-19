f5d10428f5300cc822a0f274faa36f1636ad63ec
Michael Krummsdorf <michael.krummsdorf@tq-group.com>
2019-06-19 13:28:37 +0200

    tqmls1028a_bb.h: fix include guards
    Signed-off-by: Michael Krummsdorf <michael.krummsdorf@tq-group.com>

--- board/tqc/tqmls1028a/tqmls1028a_bb.h
+++ board/tqc/tqmls1028a/tqmls1028a_bb.h
@@ -2,9 +2,9 @@
 /*
  * Copyright 2019 TQ Systems GmbH
  */
-#ifndef __TQMLS1028AL_BB_H__
-#define __TQMLS1028AL_BB_H__
+#ifndef __TQMLS1028A_BB_H__
+#define __TQMLS1028A_BB_H__
 
 void tqmls1028a_bb_late_init(void);
 int board_bb_init(void);
-#endif /* __TQMLS1028AL_BB_H__ */
+#endif /* __TQMLS1028A_BB_H__ */
