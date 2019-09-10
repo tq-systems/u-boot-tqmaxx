0e816bc7b24640ab3ac3dd344efb0927246b18ac
Binh Nguyen <binh.nguyen.uw@renesas.com>
2019-09-10 14:17:21 +0700

    usb: xhci-rcar: add compatible support for r8a774b1
    Signed-off-by : Shinji Hirai <shinji.hirai.zn@renesas.com>
    Tested-by: Huynh Thanh Hung <hung.huynh.wz@renesas.com>
    Signed-off-by: Binh Nguyen <binh.nguyen.uw@renesas.com>

--- drivers/usb/host/xhci-rcar.c
+++ drivers/usb/host/xhci-rcar.c
@@ -146,6 +146,7 @@ static const struct udevice_id xhci_rcar_ids[] = {
 	{ .compatible = "renesas,xhci-r8a7796" },
 	{ .compatible = "renesas,xhci-r8a77965" },
 	{ .compatible = "renesas,xhci-r8a774a1" },
+	{ .compatible = "renesas,xhci-r8a774b1" },
 	{ }
 };
 
