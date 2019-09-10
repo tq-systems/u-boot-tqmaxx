e93e53e471ab1b9747323d51615b32a3740b9729
Binh Nguyen <binh.nguyen.uw@renesas.com>
2019-09-10 14:18:46 +0700

    arm: add new support for hihope-rzg2n
    Add support for Hihope-RZG2N board : SoC RZG2N (R8A774B1)
    
    Signed-off-by : Shinji Hirai <shinji.hirai.zn@renesas.com>
    Tested-by: Huynh Thanh Hung <hung.huynh.wz@renesas.com>
    Signed-off-by: Binh Nguyen <binh.nguyen.uw@renesas.com>

---
+++ include/configs/hihope-rzg2n.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * include/configs/hihope-rzg2n.h
+ *     This file is HOPERUN HiHope-RZG2M board configuration.
+ *
+ * Copyright (C) 2019 Renesas Electronics Corporation
+ */
+
+#ifndef __HIHOPE_RZG2N_H
+#define __HIHOPE_RZG2N_H
+
+#include "rcar-gen3-common.h"
+
+/* Ethernet RAVB */
+#define CONFIG_BITBANGMII
+#define CONFIG_BITBANGMII_MULTI
+
+/* Generic Timer Definitions (use in assembler source) */
+#define COUNTER_FREQUENCY	0xFE502A	/* 16.66MHz from CPclk */
+
+/* Environment in eMMC, at the end of 2nd "boot sector" */
+#define CONFIG_ENV_OFFSET		(-CONFIG_ENV_SIZE)
+#define CONFIG_SYS_MMC_ENV_DEV		1
+#define CONFIG_SYS_MMC_ENV_PART		2
+
+#endif /* __HIHOPE_RZG2N_H */
