3452c319a3392fb103141aaae0bafe07a7896596
Wasim Khan <wasim.khan@nxp.com>
2019-10-10 00:34:47 +0530

    pci: layerscape: Common device tree fixup for NXP SoCs
    Add Common device tree fixup for NXP SoCs. Based on
    SoC and revision call pcie_layerscape or pcie_layerscape_gen4
    fixup.
    
    Signed-off-by: Wasim Khan <wasim.khan@nxp.com>

---
+++ drivers/pci/pcie_layerscape_fixup_common.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2019 NXP
+ *
+ */
+
+#include <common.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/soc.h>
+#include "pcie_layerscape_fixup_common.h"
+
+void ft_pci_setup(void *blob, bd_t *bd)
+{
+	uint svr;
+
+	svr = SVR_SOC_VER(get_svr());
+
+	if (svr == SVR_LX2160A && IS_SVR_REV(get_svr(), 1, 0))
+		ft_pci_setup_ls_gen4(blob, bd);
+	else
+		ft_pci_setup_ls(blob, bd);
+}
