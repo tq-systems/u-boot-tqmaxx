tq-systems/u-boot-tqmaxx

commit 24a920331cab5bb7b4c19d009aad306acc55e960

Revert "tiny-printf: Reorder code to support %p"
This reverts commit 831c1611195961bf79ac55a8deaac9034112ef5f.

This patch is causing ethernet boot failures because it leads to the
wrongly formatted mac address getting assigned to the port. A proper fix
is being done in upstream.

Signed-off-by: Faiz Abbas <faiz_abbas@ti.com>

lib/tiny-printf.c
@@ -157,8 +157,7 @@ static void ip4_addr_string(struct printf_info *info, u8 *addr)
  *       decimal).
  */
 
-static void __maybe_unused pointer(struct printf_info *info, const char *fmt,
-				   void *ptr)
+static void pointer(struct printf_info *info, const char *fmt, void *ptr)
 {
 #ifdef DEBUG
 	unsigned long num = (uintptr_t)ptr;
@@ -267,21 +266,6 @@ static int _vprintf(struct printf_info *info, const char *fmt, va_list va)
 						div_out(info, &num, div);
 				}
 				break;
-			case 'p':
-#ifdef DEBUG
-				pointer(info, fmt, va_arg(va, void *));
-				/*
-				 * Skip this because it pulls in _ctype which is
-				 * 256 bytes, and we don't generally implement
-				 * pointer anyway
-				 */
-				while (isalnum(fmt[0]))
-					fmt++;
-				break;
-#else
-				islong = true;
-				/* no break */
-#endif
 			case 'x':
 				if (islong) {
 					num = va_arg(va, unsigned long);
@@ -302,6 +286,18 @@ static int _vprintf(struct printf_info *info, const char *fmt, va_list va)
 				break;
 			case 's':
 				p = va_arg(va, char*);
+				break;
+			case 'p':
+				pointer(info, fmt, va_arg(va, void *));
+				/*
+				 * Skip this because it pulls in _ctype which is
+				 * 256 bytes, and we don't generally implement
+				 * pointer anyway
+				 */
+#ifdef DEBUG
+				while (isalnum(fmt[0]))
+					fmt++;
+#endif
 				break;
 			case '%':
 				out(info, '%');

commit dd2db0186611cbf71fc36877da642f8618765e6e

lib: tiny-printf: Fix printing pointers
Commit 24a920331cab ("Revert "tiny-printf: Reorder code to support %p"")
breaks printing pointers(%p) in SPL which breaks DFU boot mode on J721e.
SYSFW loader relies on feature to build dfu_alt interface dynamically
to load SYSFW.

Fixes: 24a920331cab ("Revert "tiny-printf: Reorder code to support %p"")
Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

lib/tiny-printf.c
@@ -157,7 +157,8 @@ static void ip4_addr_string(struct printf_info *info, u8 *addr)
  *       decimal).
  */
 
-static void pointer(struct printf_info *info, const char *fmt, void *ptr)
+static void __maybe_unused pointer(struct printf_info *info, const char *fmt,
+				   void *ptr)
 {
 #ifdef DEBUG
 	unsigned long num = (uintptr_t)ptr;
@@ -266,6 +267,16 @@ static int _vprintf(struct printf_info *info, const char *fmt, va_list va)
 						div_out(info, &num, div);
 				}
 				break;
+			case 'p':
+#ifdef CONFIG_SPL_NET_SUPPORT
+				pointer(info, fmt, va_arg(va, void *));
+				while (isalnum(fmt[0]))
+					fmt++;
+				break;
+#else
+				islong = true;
+				/* no break */
+#endif
 			case 'x':
 				if (islong) {
 					num = va_arg(va, unsigned long);
@@ -286,18 +297,6 @@ static int _vprintf(struct printf_info *info, const char *fmt, va_list va)
 				break;
 			case 's':
 				p = va_arg(va, char*);
-				break;
-			case 'p':
-				pointer(info, fmt, va_arg(va, void *));
-				/*
-				 * Skip this because it pulls in _ctype which is
-				 * 256 bytes, and we don't generally implement
-				 * pointer anyway
-				 */
-#ifdef DEBUG
-				while (isalnum(fmt[0]))
-					fmt++;
-#endif
 				break;
 			case '%':
 				out(info, '%');