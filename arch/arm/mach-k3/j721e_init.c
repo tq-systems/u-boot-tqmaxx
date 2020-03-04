tq-systems/u-boot-tqmaxx

commit 2d36c99ad8fe4973702eec396d92a73e7c9d39ba

HACK: armv7R: K3: j721e: Setup initiator credentials at bootup
J721e device has additional QoS hardware blocks which can be used
to configure some of the initiator credentials like atype, virtid

atype is a side-band signal used to indicate if the IO traffic is
routed via any IOMMU in the system. virtid is another side-band
signal which indicate the context of the IO translation used by
the IOMMU for second stage address translation.

These registers are protected via firewalls and only DMSC should have
access to them. In the absence of TISCI APIs, bootloader has to
program them while firewalls are OFF.

Do this from R5 uboot before loading SYSFW.

Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>
Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

arch/arm/mach-k3/j721e_init.c
@@ -74,6 +74,62 @@ static void mmr_unlock(u32 base, u32 partition)
 	writel(CTRLMMR_LOCK_KICK1_UNLOCK_VAL, part_base + CTRLMMR_LOCK_KICK1);
 }
 
+static inline void cbass_qos_rmw(uint32_t addr, uint32_t val, uint32_t mask)
+{
+	writel(addr, (readl(addr) & ~mask) | (val & mask));
+}
+
+void setup_initiator_credentials(void)
+{
+	u32 i, mask;
+
+	mask = QOS_ATYPE_MASK | QOS_VIRTID_MASK;
+
+	/* Initiators for root_cell, virtid = 2 */
+	/* MMC1*/
+	cbass_qos_rmw(QOS_MMC1_RD_CBASS_MAP(0), QOS_PVU_CTX(2), mask);
+	cbass_qos_rmw(QOS_MMC1_WR_CBASS_MAP(0), QOS_PVU_CTX(2), mask);
+
+	/* DSS VID1, VID2, VIDL2 */
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(0), QOS_PVU_CTX(2), mask);
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(1), QOS_PVU_CTX(2), mask);
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(4), QOS_PVU_CTX(2), mask);
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(5), QOS_PVU_CTX(2), mask);
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(6), QOS_PVU_CTX(2), mask);
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(7), QOS_PVU_CTX(2), mask);
+
+	/* GPU OS_id = 0 uses chanid = [0-3] */
+	for (i = 0; i < 4; i++) {
+		cbass_qos_rmw(QOS_GPU_M0_RD_CBASS_MAP(i), QOS_PVU_CTX(2), mask);
+		cbass_qos_rmw(QOS_GPU_M0_WR_CBASS_MAP(i), QOS_PVU_CTX(2), mask);
+		cbass_qos_rmw(QOS_GPU_M1_RD_CBASS_MAP(i), QOS_PVU_CTX(2), mask);
+		cbass_qos_rmw(QOS_GPU_M1_WR_CBASS_MAP(i), QOS_PVU_CTX(2), mask);
+	}
+
+	/* D5520 chanid=[0-1] */
+	for (i = 0; i < 2; i++) {
+		cbass_qos_rmw(QOS_D5520_RD_CBASS_MAP(2), QOS_PVU_CTX(2), mask);
+		cbass_qos_rmw(QOS_D5520_WR_CBASS_MAP(2), QOS_PVU_CTX(2), mask);
+	}
+
+	/* Initiators for inmate_cell, virtid=3 */
+	/* MMC0 */
+	cbass_qos_rmw(QOS_MMC0_RD_CBASS_MAP(0), QOS_PVU_CTX(3), mask);
+	cbass_qos_rmw(QOS_MMC0_WR_CBASS_MAP(0), QOS_PVU_CTX(3), mask);
+
+	/* DSS VIDL1 */
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(2), QOS_PVU_CTX(3), mask);
+	cbass_qos_rmw(QOS_DSS0_DMA_CBASS_MAP(3), QOS_PVU_CTX(3), mask);
+
+	/* GPU OS_id = 1 uses chanid = [4-7] */
+	for (i = 4; i < 8; i++) {
+		cbass_qos_rmw(QOS_GPU_M0_RD_CBASS_MAP(i), QOS_PVU_CTX(3), mask);
+		cbass_qos_rmw(QOS_GPU_M0_WR_CBASS_MAP(i), QOS_PVU_CTX(3), mask);
+		cbass_qos_rmw(QOS_GPU_M1_RD_CBASS_MAP(i), QOS_PVU_CTX(3), mask);
+		cbass_qos_rmw(QOS_GPU_M1_WR_CBASS_MAP(i), QOS_PVU_CTX(3), mask);
+	}
+}
+
 static void ctrl_mmr_unlock(void)
 {
 	/* Unlock all WKUP_CTRL_MMR0 module registers */
@@ -218,6 +274,7 @@ void board_init_f(ulong dummy)
 
 	setup_navss_nb();
 	setup_dss_credentials();
+	setup_initiator_credentials();
 #endif
 
 	/* Init DM early */

arch/arm/mach-k3/include/mach/j721e_hardware.h
@@ -46,6 +46,11 @@
 #define CTRLMMR_LOCK_KICK1				0x0100c
 #define CTRLMMR_LOCK_KICK1_UNLOCK_VAL			0xd172bc5a
 
+#define QOS_ATYPE_MASK			0x30000000
+#define QOS_VIRTID_MASK			0x0fff0000
+#define QOS_PVU_CTX(virtid)		((0x1 << 28) | (virtid << 16))
+#define QOS_SMMU_CTX(virtid)		((0x2 << 28) | (virtid << 16))
+
 /* MCU SCRATCHPAD usage */
 #define TI_SRAM_SCRATCH_BOARD_EEPROM_START	CONFIG_SYS_K3_MCU_SCRATCHPAD_BASE
 
@@ -59,6 +64,19 @@
 #define QOS_DSS0_FBDC_CBASS_GRP_MAP1(j)		(QOS_DSS0_FBDC + 0x0 + (j) * 8)
 #define QOS_DSS0_FBDC_CBASS_GRP_MAP2(j)		(QOS_DSS0_FBDC + 0x4 + (j) * 8)
 #define QOS_DSS0_FBDC_CBASS_MAP(i)		(QOS_DSS0_FBDC + 0x100 + (i) * 4)
+
+#define QOS_MMC0_RD_CBASS_MAP(i)		(0x45d9a100 + (i) * 4)
+#define QOS_MMC0_WR_CBASS_MAP(i)		(0x45d9a500 + (i) * 4)
+#define QOS_MMC1_RD_CBASS_MAP(i)		(0x45d82100 + (i) * 4)
+#define QOS_MMC1_WR_CBASS_MAP(i)		(0x45d82500 + (i) * 4)
+
+#define QOS_GPU_M0_RD_CBASS_MAP(i)		(0x45dc5100 + (i) * 4)
+#define QOS_GPU_M0_WR_CBASS_MAP(i)		(0x45dc5900 + (i) * 4)
+#define QOS_GPU_M1_RD_CBASS_MAP(i)		(0x45dc6100 + (i) * 4)
+#define QOS_GPU_M1_WR_CBASS_MAP(i)		(0x45dc6900 + (i) * 4)
+
+#define QOS_D5520_RD_CBASS_MAP(i)		(0x45dc0500 + (i) * 4)
+#define QOS_D5520_WR_CBASS_MAP(i)		(0x45dc0900 + (i) * 4)
 
 /* NAVSS North Bridge (NB) */
 #define NAVSS0_NBSS_NB0_CFG_MMRS		0x3802000

commit 3735cfc99f876d51d2f166bbe31eb414723ce143

HACK: armv7R: K3: j721e: Fix writing into qos registers
Apparently cbass_qos_rmw() is writing the qos register address into
the memory location qos val. Fix it by writing the value into address.

While at it fix the following build error with j721e_evm_a72 defconfig:

./arch/arm/include/asm/io.h:45:28: warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
   45 | #define __arch_getl(a)   (*(volatile unsigned int *)(a))
      |                            ^

arch/arm/mach-k3/j721e_init.c:79:2: note: in expansion of macro ‘writel’
   79 |  writel(addr, (readl(addr) & ~mask) | (val & mask));
      |  ^~~~~~

Fixes: 2d36c99ad8 ("HACK: armv7R: K3: j721e: Setup initiator credentials at bootup")
Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

arch/arm/mach-k3/j721e_init.c
@@ -74,9 +74,9 @@ static void mmr_unlock(u32 base, u32 partition)
 	writel(CTRLMMR_LOCK_KICK1_UNLOCK_VAL, part_base + CTRLMMR_LOCK_KICK1);
 }
 
-static inline void cbass_qos_rmw(uint32_t addr, uint32_t val, uint32_t mask)
+static inline void cbass_qos_rmw(uintptr_t addr, uint32_t val, uint32_t mask)
 {
-	writel(addr, (readl(addr) & ~mask) | (val & mask));
+	writel((readl(addr) & ~mask) | (val & mask), addr);
 }
 
 void setup_initiator_credentials(void)