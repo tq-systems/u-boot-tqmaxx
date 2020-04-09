46e231643a1dcc9c71d1739c99045dd4cdbb04b6
Khoi Nguyen <khoi.nguyen.aj@renesas.com>
2020-04-09 16:43:04 +0700

    clk: renesas: clk-rcar-gen3: Add parent clocks of watchdog and calculate clock rates
    This patch adds parent clocks of watchdog used in different
     SoCs ( r8a774c0, r8a774a1, r8a774b1 ):
    - CLK_TYPE_GEN3_R
    - CLK_TYPE_GEN3_OSC
    - CLK_TYPE_FR
    - CLK_TYPE_DIV6_RO
    - CLK_TYPE_GEN3_RCKSEL
    
    Signed-off-by: Khoi Nguyen <khoi.nguyen.aj@renesas.com>

--- drivers/clk/renesas/clk-rcar-gen3.c
+++ drivers/clk/renesas/clk-rcar-gen3.c
@@ -24,6 +24,8 @@
 
 #define CPG_RST_MODEMR		0x0060
 
+#define CPG_RCKCR_CKSEL BIT(15) /* RCLK Clock Source Select */
+
 #define CPG_PLL0CR		0x00d8
 #define CPG_PLL2CR		0x002c
 #define CPG_PLL4CR		0x01f4
@@ -162,6 +164,7 @@ static u64 gen3_clk_get_rate64(struct clk *clk)
 					priv->cpg_pll_config;
 	u32 value, mult, div, prediv, postdiv;
 	u64 rate = 0;
+	unsigned int rckcr_div;
 	int i, ret;
 
 	debug("%s[%i] Clock: id=%lu\n", __func__, __LINE__, clk->id);
@@ -309,6 +312,43 @@ static u64 gen3_clk_get_rate64(struct clk *clk)
 
 		return -EINVAL;
 
+	case CLK_TYPE_FR:
+		rate = core->mult;
+		return rate;
+
+	case CLK_TYPE_DIV6_RO:
+		div = core->div;
+		rckcr_div = (readl(priv->base + core->offset) & 0x3f);
+		if (rckcr_div != 0x2f) {
+			writel(readl(priv->base + core->offset) | BIT(8)
+				, priv->base + core->offset);
+			udelay(105);
+			writel(((readl(priv->base + core->offset) & (BIT(15) |
+				BIT(12))) | 0x2f), priv->base + core->offset);
+			rckcr_div = (readl(priv->base + core->offset) & 0x3f);
+		}
+		div *= (readl(priv->base + core->offset) & 0x3f) + 1;
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
+
+	case CLK_TYPE_GEN3_RCKSEL:
+		if (readl(priv->base + CPG_RCKCR) & CPG_RCKCR_CKSEL) {
+			div = core->div & 0xffff;
+		} else {
+			parent.id = parent.id >> 16;
+			div = core->div >> 16;
+		}
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
+
+	case CLK_TYPE_GEN3_R:
+		rate = gen3_clk_get_rate64(&parent);
+		return rate;
+
+	case CLK_TYPE_GEN3_OSC:
+		div = pll_config->osc_prediv * core->div;
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
 	}
 
 	printf("%s[%i] unknown fail\n", __func__, __LINE__);
