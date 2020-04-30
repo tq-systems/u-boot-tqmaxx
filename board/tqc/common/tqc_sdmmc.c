8a9e0bfa5d410f247f12c54c9266321c7d6151e3
Markus Niebel <Markus.Niebel@tq-group.com>
2020-04-30 12:17:12 +0200

    tqc: common: add common mmc boot env stuff for tqc boards
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

---
+++ board/tqc/common/tqc_sdmmc.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright (C) 2018-2019 TQ Systems GmbH
+ */
+
+#include <common.h>
+#include <asm/arch/sys_proto.h>
+#include <linux/errno.h>
+#include <asm/io.h>
+#include <mmc.h>
+#include <stdbool.h>
+
+static int check_mmc_autodetect(void)
+{
+	char *autodetect_str = env_get("mmcautodetect");
+
+	if (autodetect_str && (strcmp(autodetect_str, "yes") == 0))
+		return 1;
+
+	return 0;
+}
+
+/* This should be defined for each board */
+__weak int mmc_map_to_kernel_blk(int dev_no)
+{
+	return dev_no;
+}
+
+void board_late_mmc_env_init(void)
+{
+	u32 dev_no = mmc_get_env_dev();
+
+	if (!check_mmc_autodetect())
+		return;
+
+	env_set_ulong("mmcdev", dev_no);
+	env_set_ulong("mmcblkdev", mmc_map_to_kernel_blk(dev_no));
+}
