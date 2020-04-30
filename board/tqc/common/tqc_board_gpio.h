b966f4c83c2de9b3754ba2ece287cf8eefe2c058
Markus Niebel <Markus.Niebel@tq-group.com>
2020-04-30 12:27:44 +0200

    board/tqc: forward port tqc_board_gpio from rel_imx_4.14.98_2.2.0
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

---
+++ board/tqc/common/tqc_board_gpio.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2019 TQ-Systems GmbH <u-boot@ew.tq-group.com>, D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#ifndef __TQC_BOARD_GPIO_H__
+#define __TQC_BOARD_GPIO_H__
+
+struct tqc_gpio_init_data {
+	const char *name;
+	const char *label;
+	const unsigned long flags;
+	struct gpio_desc desc;
+};
+
+#if defined(CONFIG_DM_GPIO)
+
+#define GPIO_INIT_DATA_ENTRY(IDX, NAME, FLAGS) \
+	[IDX] = { \
+		.name = (NAME), \
+		.label = __stringify(IDX), \
+		.flags = (FLAGS), \
+	}
+
+int tqc_board_gpio_init(struct tqc_gpio_init_data *data, int count);
+
+#else
+static inline int tqc_board_gpio_init(struct tqc_gpio_init_data *data,
+				      int count)
+{
+	return 0;
+}
+#endif
+
+#endif
