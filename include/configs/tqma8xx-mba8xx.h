10e81411c9dd2983b9a836c6d2ea8723155f3921
Markus Niebel <Markus.Niebel@tq-group.com>
2020-05-06 13:00:44 +0200

    tqma8xx: update env settings
    port from sumo / 4.14.98_2.2.0 branch:
    
    - use generic filesystem functions in env
    - restruct env to use BB ENV part
    - add update scripts for mmc
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- include/configs/tqma8xx-mba8xx.h
+++ include/configs/tqma8xx-mba8xx.h
@@ -6,4 +6,8 @@
 #if !defined(__TQMA8XX_MBA8XX_H)
 #define __TQMA8XX_MBA8XX_H
 
+#define BB_ENV_SETTINGS \
+	"console=ttyLP1\0" \
+	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0"
+
 #endif /* __TQMA8XX_MBA8XX_H */
