34880d9f387875362169015f3b9aadaead305cc6
Ye Li <ye.li@nxp.com>
2020-05-21 03:26:34 -0700

    MLK-24079 imx8mp: Disable DWC3 gadget before UUU jumping to kernel
    When UUU runs acmd command to boot into kernel, the fastboot never
    get chance to exit, so the usb gadget is not disconnected. This causes
    kernel USB gadget failed to work.
    
    Re-use iMX8MQ implementation to disconnect DWC3 gadget before jumping
    to kernel.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jun Li <jun.li@nxp.com>
    (cherry picked from commit 27cb9ee66bccabea702c10d909788cac7ebe306e)
    (cherry picked from commit a0717e53b38f6385f705c14c6e34c03f6464b4ed)
    (cherry picked from commit 1967e6a0082235364ec1b6409984940dfc7c57e6)

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -1068,6 +1068,7 @@ static int check_mipi_dsi_nodes(void *blob)
 
 	return disable_mipi_dsi_nodes(blob);
 }
+#endif
 
 void board_quiesce_devices(void)
 {
@@ -1076,7 +1077,6 @@ void board_quiesce_devices(void)
 		disconnect_from_pc();
 #endif
 }
-#endif
 
 int disable_vpu_nodes(void *blob)
 {
