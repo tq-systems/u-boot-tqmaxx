6cd70d5a49e79c2803136f3ead70e2652c251aef
Gregor Herburger <gregor.herburger@tq-group.com>
2020-07-15 10:29:29 +0200

    TQMLS1046A_8G: Add new DDR4-RAM Configuration and timing
    Change to Micron MT40A1G16 DDR4 requires timing adaptions.
    
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

--- board/tqc/tqmls1046a/ddr.h
+++ board/tqc/tqmls1046a/ddr.h
@@ -44,14 +44,74 @@ static const struct board_specific_parameters *udimms[] = {
 };
 #endif
 
-fsl_ddr_cfg_regs_t ddr_cfg_regs_2000 = {
 #ifdef CONFIG_TQMLS1046A_DDR_8G
+fsl_ddr_cfg_regs_t ddr_cfg_regs_2000 = {
 	.cs[0].bnds         = 0x000001FF,
-	.cs[0].config       = 0x80010422,
+	.cs[0].config       = 0x80010512,
+	.cs[0].config_2     = 0x00000000,
+	.cs[1].bnds         = 0x00000000,
+	.cs[1].config       = 0x00000000,
+	.cs[1].config_2     = 0x00000000,
+	.cs[2].bnds         = 0x00000000,
+	.cs[2].config       = 0x00000000,
+	.cs[2].config_2     = 0x00000000,
+	.cs[3].bnds         = 0x00000000,
+	.cs[3].config       = 0x00000000,
+	.cs[3].config_2     = 0x00000000,
+	.timing_cfg_3       = 0x12551100,
+	.timing_cfg_0       = 0xA0660008,
+	.timing_cfg_1       = 0x060E6278,
+	.timing_cfg_2       = 0x0058625E,
+	.ddr_sdram_cfg      = 0x65200008,
+	.ddr_sdram_cfg_2    = 0x00401070,
+	.ddr_sdram_cfg_3    = 0x40000000,
+	.ddr_sdram_mode     = 0x05030635,
+	.ddr_sdram_mode_2   = 0x00100000,
+	.ddr_sdram_mode_3   = 0x00000000,
+	.ddr_sdram_mode_4   = 0x00000000,
+	.ddr_sdram_mode_5   = 0x00000000,
+	.ddr_sdram_mode_6   = 0x00000000,
+	.ddr_sdram_mode_7   = 0x00000000,
+	.ddr_sdram_mode_8   = 0x00000000,
+	.ddr_sdram_mode_9   = 0x00000701,
+	.ddr_sdram_mode_10  = 0x08800000,
+	.ddr_sdram_mode_11  = 0x00000000,
+	.ddr_sdram_mode_12  = 0x00000000,
+	.ddr_sdram_mode_13  = 0x00000000,
+	.ddr_sdram_mode_14  = 0x00000000,
+	.ddr_sdram_mode_15  = 0x00000000,
+	.ddr_sdram_mode_16  = 0x00000000,
+	.ddr_sdram_interval = 0x0F3C079E,
+	.ddr_data_init      = 0xDEADBEEF,
+	.ddr_sdram_clk_cntl = 0x02400000,
+	.ddr_init_addr      = 0x00000000,
+	.ddr_init_ext_addr  = 0x00000000,
+	.timing_cfg_4       = 0x00224502,
+	.timing_cfg_5       = 0x06401400,
+	.timing_cfg_6       = 0x00000000,
+	.timing_cfg_7       = 0x25540000,
+	.timing_cfg_8       = 0x06445A00,
+	.timing_cfg_9       = 0x00000000,
+	.ddr_zq_cntl        = 0x8A090705,
+	.ddr_wrlvl_cntl     = 0x86750608,
+	.ddr_wrlvl_cntl_2   = 0x07070605,
+	.ddr_wrlvl_cntl_3   = 0x05040407,
+	.ddr_sr_cntr        = 0x00000000,
+	.ddr_sdram_rcw_1    = 0x00000000,
+	.ddr_sdram_rcw_2    = 0x00000000,
+	.ddr_sdram_rcw_3    = 0x00000000,
+	.ddr_cdr1           = 0x80080000,
+	.ddr_cdr2           = 0x00000080,
+	.dq_map_0           = 0x06106104,
+	.dq_map_1           = 0x84184184,
+	.dq_map_2           = 0x06106104,
+	.dq_map_3           = 0x84184000,
+	.debug[28]          = 0x00700046,
+};
 #else
+fsl_ddr_cfg_regs_t ddr_cfg_regs_2000 = {
 	.cs[0].bnds         = 0x0000007F,
 	.cs[0].config       = 0x80010312,
-#endif
 	.cs[0].config_2     = 0x00000000,
 	.cs[1].bnds         = 0x00000000,
 	.cs[1].config       = 0x00000000,
@@ -64,11 +124,7 @@ fsl_ddr_cfg_regs_t ddr_cfg_regs_2000 = {
 	.cs[3].config_2     = 0x00000000,
 	.timing_cfg_3       = 0x020F1100,
 	.timing_cfg_0       = 0xF7660008,
-#ifdef CONFIG_TQMLS1046A_DDR_8G
-	.timing_cfg_1       = 0xF1FCC178,
-#else
 	.timing_cfg_1       = 0xF1FC4178,
-#endif
 	.timing_cfg_2       = 0x00590160,
 	.ddr_sdram_cfg      = 0x65000008,
 	.ddr_sdram_cfg_2    = 0x00401150,
@@ -116,6 +172,7 @@ fsl_ddr_cfg_regs_t ddr_cfg_regs_2000 = {
 	.dq_map_3           = 0x00000000,
 	.debug[28]          = 0x00700046,
 };
+#endif
 
 fixed_ddr_parm_t fixed_ddr_parm_0[] = {
 	{1950, 2050, &ddr_cfg_regs_2000},
