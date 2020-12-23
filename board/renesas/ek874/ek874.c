9b1ecff043cda4a7f0a9817d940bc7adc8037656
Hien Huynh <hien.huynh.px@renesas.com>
2020-12-23 13:43:24 +0700

    board: renesas: ek874: add function to check board revision
    This commit is created to add a new function to check board revision
    and use right devicetree files corresponding with board revisions.
    Currently, we can monitor the value of GP5_19 pin to identify
    EK874 between Rev.C and the latest Rev.E.
    
    In latest EK874 Revisions, this pin is pulled down with an external
    4.7kOhm resistor. Therefore, when we enable internal pull-up then
    setting general input mode for this pin, we will find the different
    value of GP5_19 in each revisions:
    - Rev.C and the older: 1
    - The latest Rev.E: 0
    
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- board/renesas/ek874/ek874.c
+++ board/renesas/ek874/ek874.c
@@ -27,6 +27,55 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
+#define	PFC_PMMR		0xE6060000
+#define	PFC_PUEN4		0xE6060410
+#define	PFC_PUD4		0xE6060450
+#define	PFC_GPSR5		0xE6060114
+#define	GPIO_IOINTSEL5		0xE6055000
+#define	GPIO_INOUTSEL5		0xE6055004
+#define	GPIO_INDT5		0xE605500C
+#define	MSTP907_GPIO5		BIT(7)
+
+static char board_rev;
+
+char check_rev(void)
+{
+	u32 reg;
+	int ret;
+
+	/* Check GPIO/Peripheral Function mode */
+	reg = readl(PFC_GPSR5) & BIT(19);
+	if (reg) {
+		reg = readl(PFC_GPSR5) & ~BIT(19);
+		writel(~reg, PFC_PMMR);
+		writel(reg, PFC_GPSR5);
+	}
+
+	/* Check Clock support to Module GPIO5 */
+	reg = readl(MSTPSR9) & MSTP907_GPIO5;
+	if (reg)
+		writel(readl(SMSTPCR9) & ~MSTP907_GPIO5, SMSTPCR9);
+
+	/* Enable Pull up/down function */
+	writel(readl(PFC_PUEN4) | BIT(17), PFC_PUEN4);
+	/* Enable Pull up */
+	writel(readl(PFC_PUD4) | BIT(17), PFC_PUD4);
+
+	/* Set General input/output mode for GP5_19 */
+	writel(readl(GPIO_IOINTSEL5) & ~BIT(19), GPIO_IOINTSEL5);
+	/* Set General input mode for GP5_19 */
+	writel(readl(GPIO_INOUTSEL5) & ~BIT(19), GPIO_INOUTSEL5);
+	/* Read the value received through GP5_19 */
+	ret = readl(GPIO_INDT5) & BIT(19);
+
+	/* Enable Pull down */
+	writel(readl(PFC_PUD4) & BIT(17), PFC_PUD4);
+	/* Disable Pull up/down function */
+	writel(readl(PFC_PUEN4) & BIT(17), PFC_PUEN4);
+
+	return ret ? 'C' : 'E';
+}
+
 void s_init(void)
 {
 }
@@ -38,6 +87,7 @@ int board_early_init_f(void)
 
 int board_init(void)
 {
+	board_rev = check_rev();
 	/* adress of boot parameters */
 	gd->bd->bi_boot_params = CONFIG_SYS_TEXT_BASE + 0x50000;
 
@@ -102,5 +152,8 @@ int board_late_init(void)
 #ifdef CONFIG_WDT_RENESAS
 	reinitr_wdt();
 #endif
+
+	env_set("board_rev", &board_rev);
+
 	return 0;
 }
