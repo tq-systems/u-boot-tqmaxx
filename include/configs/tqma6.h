2bb151e7422fbe882f187cefac91421994eedd7a
Paul Gerber <Paul.Gerber@tq-group.com>
2021-01-19 08:42:09 +0000

    tqma6x: enable MEMTEST for TQMa6[S,DL,Q,QP]
      - enable MEMTEST in defconfig
      - configure start and end of MEMTEST area
    
    Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>

--- include/configs/tqma6.h
+++ include/configs/tqma6.h
@@ -442,6 +442,33 @@
 #define CONFIG_IMX_THERMAL
 
 /*
+ * MEMTEST configuration
+ */
+
+#if defined(CONFIG_CMD_MEMTEST)
+
+/*
+ * config alternate mtest:
+ * enable 3/4 of RAM to test
+ * U-Boot is relocated to the end of RAM
+ */
+#if !defined(__ASSEMBLY__)
+
+unsigned imx_ddr_size(void);
+
+#endif
+
+#define CONFIG_SYS_ALT_MEMTEST
+
+#define CONFIG_SYS_MEMTEST_START	CONFIG_SYS_SDRAM_BASE
+#define CONFIG_SYS_MEMTEST_END		(CONFIG_SYS_MEMTEST_START + \
+					((imx_ddr_size() / 4) * 3))
+
+#define CONFIG_SYS_MEMTEST_SCRATCH	CONFIG_SYS_MEMTEST_END
+
+#endif /* CONFIG_CMD_MEMTEST */
+
+/*
  * All the defines above are for the TQMa6 SoM
  *
  * Now include the baseboard specific configuration
