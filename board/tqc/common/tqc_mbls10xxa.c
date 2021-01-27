tq-systems/u-boot-tqmaxx

commit 165ba21b1faa0779f463f48100bf729beed8777f

board: tqc: mbls10xxa: configure SerDes TQ equalization in XFI mode
Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

board/tqc/common/tqc_mbls10xxa.c
@@ -8,6 +8,8 @@
 #include <fm_eth.h>
 #include <fsl_dtsec.h>
 #include <fsl_mdio.h>
+#include <asm/io.h>
+#include <asm/arch/immap_lsch2.h>
 #include "tqc_mbls10xxa.h"
 
 struct tqc_mbls10xxa_i2c_gpio {
@@ -243,6 +245,27 @@ void tqc_mbls10xxa_retimer_init(void)
 		tqc_mbls10xxa_retimer_init_one(addrs[i]);
 
 	i2c_set_bus_num(oldbus);
+}
+
+void tqc_mbls10xxa_xfi_init(struct ccsr_serdes *serdes, int lane)
+{
+	u32 gcr0;
+
+	/* Put lane in reset */
+	gcr0 = in_be32(&serdes->lane[lane].gcr0);
+	gcr0 &= 0xFF9FFFFF;
+	out_be32(&serdes->lane[lane].gcr0, gcr0);
+
+	udelay(1);
+
+	/* Set up Transmit Equalization Control Register 0 for XFI */
+	out_be32(&serdes->lane[lane].tecr0, 0x00233827);
+
+	udelay(1);
+
+	/* Take lane out of reset */
+	gcr0 |= 0x00600000;
+	out_be32(&serdes->lane[lane].gcr0, gcr0);
 }
 
 #ifndef CONFIG_SPL_BUILD

board/tqc/common/tqc_mbls10xxa.h
@@ -13,6 +13,7 @@ int tqc_mbls10xxa_i2c_gpio_get(const char *name);
 int tqc_mbls10xxa_i2c_gpio_set(const char *name, int val);
 
 void tqc_mbls10xxa_retimer_init(void);
+void tqc_mbls10xxa_xfi_init(struct ccsr_serdes *serdes, int lane);
 
 int tqc_mbls10xxa_clk_cfg_init(void);
 

board/tqc/tqmls1043a/tqmls1043a_bb_mbls10xxa.c
@@ -63,6 +63,7 @@ static void _tqmls1043a_bb_serdes_cfg(void)
 {
 	u32 srds_s1;
 	struct ccsr_gur *gur = (void *)(CONFIG_SYS_FSL_GUTS_ADDR);
+	struct ccsr_serdes *serdes1_base = (void *)CONFIG_SYS_FSL_SERDES_ADDR;
 	struct tqc_mbls10xxa_serdes lanes[8];
 	int clk_freq;
 
@@ -93,6 +94,10 @@ static void _tqmls1043a_bb_serdes_cfg(void)
 		printf("!!! ATTENTON: SerDes1 RefClk1 is 156.25MHz,\n");
 		printf("!!!  but this is only valid for XFI operation!\n");
 	}
+
+	/* Configure TQ equalization for XFI */
+	if (TQMLS1043A_SRDS1_PROTO(srds_s1, 0) == 0x1)
+		tqc_mbls10xxa_xfi_init(serdes1_base, 0);
 
 	tqc_mbls10xxa_retimer_init();
 }

board/tqc/tqmls1046a/tqmls1046a_bb_mbls10xxa.c
@@ -65,6 +65,7 @@ static void _tqmls1046a_bb_serdes_cfg(void)
 {
 	u32 srds_s1, srds_s2;
 	struct ccsr_gur *gur = (void *)(CONFIG_SYS_FSL_GUTS_ADDR);
+	struct ccsr_serdes *serdes1_base = (void *)CONFIG_SYS_FSL_SERDES_ADDR;
 	struct tqc_mbls10xxa_serdes lanes[8];
 	int clk_freq;
 
@@ -102,6 +103,12 @@ static void _tqmls1046a_bb_serdes_cfg(void)
 		printf("!!! ATTENTON: SerDes1 RefClk2 is 156.25MHz,\n");
 		printf("!!!  but this is only valid for XFI operation!\n");
 	}
+
+	/* Configure TQ equalization for XFI */
+	if (TQMLS1046A_SRDS1_PROTO(srds_s1, 0) == 0x1)
+		tqc_mbls10xxa_xfi_init(serdes1_base, 3);
+	if (TQMLS1046A_SRDS1_PROTO(srds_s1, 1) == 0x1)
+		tqc_mbls10xxa_xfi_init(serdes1_base, 2);
 
 	tqc_mbls10xxa_retimer_init();
 }

commit 40a894cdc9e756bd28a979053cb06b3e8273a724

board: tqc: mbls10xxa: fix XFI serdes config
Commit 165ba21b1faa ("board: tqc: mbls10xxa: configure SerDes TQ
equalization in XFI mode") broke build for tqmls1088a_mbls10xxa machines
because this is a FSL_LSCH3 machine. Fix this by including correct immap
definition. Also Serdes Registers are big-endian on ls104xa but
little-endian on ls1088a. Fix this by using correct function depending
on architecture.

Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

board/tqc/common/tqc_mbls10xxa.c
@@ -9,7 +9,11 @@
 #include <fsl_dtsec.h>
 #include <fsl_mdio.h>
 #include <asm/io.h>
+#if defined(CONFIG_FSL_LSCH3)
+#include <asm/arch/immap_lsch3.h>
+#elif defined(CONFIG_FSL_LSCH2)
 #include <asm/arch/immap_lsch2.h>
+#endif
 #include "tqc_mbls10xxa.h"
 
 struct tqc_mbls10xxa_i2c_gpio {
@@ -252,20 +256,24 @@ void tqc_mbls10xxa_xfi_init(struct ccsr_serdes *serdes, int lane)
 	u32 gcr0;
 
 	/* Put lane in reset */
-	gcr0 = in_be32(&serdes->lane[lane].gcr0);
+	gcr0 = serdes_in32(&serdes->lane[lane].gcr0);
 	gcr0 &= 0xFF9FFFFF;
-	out_be32(&serdes->lane[lane].gcr0, gcr0);
+	serdes_out32(&serdes->lane[lane].gcr0, gcr0);
 
 	udelay(1);
 
 	/* Set up Transmit Equalization Control Register 0 for XFI */
-	out_be32(&serdes->lane[lane].tecr0, 0x00233827);
+#if defined(CONFIG_FSL_LSCH3)
+	serdes_out32(&serdes->lane[lane].tec0,  0x00233827);
+#elif defined(CONFIG_FSL_LSCH2)
+	serdes_out32(&serdes->lane[lane].tecr0, 0x00233827);
+#endif
 
 	udelay(1);
 
 	/* Take lane out of reset */
 	gcr0 |= 0x00600000;
-	out_be32(&serdes->lane[lane].gcr0, gcr0);
+	serdes_out32(&serdes->lane[lane].gcr0, gcr0);
 }
 
 #ifndef CONFIG_SPL_BUILD

board/tqc/common/tqc_mbls10xxa.h
@@ -7,6 +7,14 @@
 
 #include <pca953x.h>
 
+#if defined(CONFIG_FSL_LSCH3)
+#define serdes_in32(a)       in_le32(a)
+#define serdes_out32(a, v)   out_le32(a, v)
+#elif defined(CONFIG_FSL_LSCH2)
+#define serdes_in32(a)       in_be32(a)
+#define serdes_out32(a, v)   out_be32(a, v)
+#endif
+
 int tqc_mbls10xxa_i2c_gpios_init(void);
 
 int tqc_mbls10xxa_i2c_gpio_get(const char *name);