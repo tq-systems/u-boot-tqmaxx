73846e0e9f8929086298a0d8fcc522a509881866
Markus Niebel <Markus.Niebel@tq-group.com>
2021-03-23 14:02:43 +0100

    tqma8mpxl: add lpddr4 timing with reduced frequency
    Select timing by default, since this fixes some stability issues under
    investigation. Keep old timing to enable fast change for development
    reason.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@tq-group.com>

--- board/tqc/tqma8mpxl/spl.c
+++ board/tqc/tqma8mpxl/spl.c
@@ -32,6 +32,9 @@ DECLARE_GLOBAL_DATA_PTR;
 #if defined(CONFIG_TQMA8MPXL_RAM_2048MB)
 extern struct dram_timing_info dram_timing_2gb_no_ecc;
 static struct dram_timing_info *dram_timing_info = &dram_timing_2gb_no_ecc;
+#elif defined(CONFIG_TQMA8MPXL_RAM_2048MB_1G5)
+extern struct dram_timing_info dram_timing_2gb_1G5_no_ecc;
+static struct dram_timing_info *dram_timing_info = &dram_timing_2gb_1G5_no_ecc;
 #endif
 
 void spl_dram_init(void)
