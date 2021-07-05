4232489eae335307c92e43e1ff964094edd1b49f
Alice Guo <alice.guo@nxp.com>
2021-07-05 15:48:23 +0800

    LFU-183 configs: imx8ulp: add Jailhouse boot command
    Run jh_mmcboot or jh_netboot to boot Linux and then can run Jailhouse.
    
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Alice Guo <alice.guo@nxp.com>
    (cherry picked from commit ff8fc6c391b64c0713ca0e8e7591f292da14e346)
    (cherry picked from commit 85d7bb67a32a39125e696faf8e8b90db28639e85)

--- include/configs/imx8ulp_evk.h
+++ include/configs/imx8ulp_evk.h
@@ -36,6 +36,11 @@
 #define BOOTENV
 #endif
 
+#define JAILHOUSE_ENV \
+	"jh_clk= \0 " \
+	"jh_mmcboot=setenv jh_clk clk_ignore_unused mem=1GB; run loadimage; run mmcboot\0 " \
+	"jh_netboot=setenv jh_clk clk_ignore_unused mem=1GB; run netboot\0 "
+
 #define CFG_MFG_ENV_SETTINGS \
 	CFG_MFG_ENV_SETTINGS_DEFAULT \
 	"initrd_addr=0x83800000\0" \
@@ -47,6 +52,7 @@
 #define CFG_EXTRA_ENV_SETTINGS		\
 	CFG_MFG_ENV_SETTINGS \
 	BOOTENV \
+	JAILHOUSE_ENV \
 	AHAB_ENV \
 	"scriptaddr=0x83500000\0" \
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
@@ -64,7 +70,7 @@
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk2p2 rootwait rw\0" \
 	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs console=${console} root=${mmcroot}\0 " \
+	"mmcargs=setenv bootargs ${jh_clk} console=${console} root=${mmcroot}\0 " \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
@@ -92,7 +98,7 @@
 				"fi; " \
 			"fi;" \
 		"fi;\0" \
-	"netargs=setenv bootargs console=${console} " \
+	"netargs=setenv bootargs ${jh_clk} console=${console} " \
 		"root=/dev/nfs " \
 		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
 	"netboot=echo Booting from net ...; " \
