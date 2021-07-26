e9996035982e59315c849ba7721b672cedc1bcbd
Gaurav Jain <gaurav.jain@nxp.com>
2021-07-26 19:25:09 +0530

    LFU-192-5 crypto/fsl: Fix warnings in function gen_mppubk.
    warning: implicit declaration of function ‘flush_dcache_range’.
    warning: implicit declaration of function ‘invalidate_dcache_range’
    
    added header file.
    
    Fixes: efacfc87ba (MLK-18169 imx: imx7 Support for Manufacturing Protection)
    Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
    Reviewed-by: Horia Geantă <horia.geanta@nxp.com>
    (cherry picked from commit 4884bf62a79ad18b7a51c0395eed399362f1083d)

--- drivers/crypto/fsl/fsl_mfgprot.c
+++ drivers/crypto/fsl/fsl_mfgprot.c
@@ -4,6 +4,7 @@
  * Copyright 2017 NXP
  */
 
+#include <cpu_func.h>
 #include <errno.h>
 #include <fsl_sec.h>
 #include <memalign.h>
@@ -92,7 +93,7 @@ int gen_mppubk(u8 *dst)
 	flush_dcache_range((unsigned long)dst, (unsigned long)dst + size);
 
 	/* Execute Job Descriptor */
-	puts("\nGenerating Manufacturing Protection Public Key\n");
+	debug("\nGenerating Manufacturing Protection Public Key\n");
 
 	ret = run_descriptor_jr(dsc);
 	if (ret) {
