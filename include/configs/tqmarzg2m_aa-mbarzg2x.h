c60a03028c726e70bb7cce63e4ac3cbf1e28528a
Andreas Dumberger <andreas.dumberger@tq-group.com>
2021-11-05 14:52:41 +0100

    Add new configuration for TQMaRZG2M (2GiB) on a MBaRZG2
    Signed-off-by: Andreas Dumberger <andreas.dumberger@tq-group.com>

---
+++ include/configs/tqmarzg2m_aa-mbarzg2x.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * include/configs/tqmrzg2m_aa-mbarzg2x.h
+ *     This file is TQMaRZG2M-AA (2GiB) module on MBaRZG2x board configuration.
+ *
+ * Copyright (C) 2019 Renesas Electronics Corporation
+ * Copyright (C) 2021 TQ-Systems GmbH
+ */
+
+#ifndef __TQMARZG2M_AA_MBARZG2X_H
+#define __TQMARZG2M_AA_MBARZG2X_H
+
+#include "rcar-gen3-common.h"
+#include "tqmarzg2_common.h"
+
+#endif /*__TQMARZG2M_AA_MBARZG2X_H */
