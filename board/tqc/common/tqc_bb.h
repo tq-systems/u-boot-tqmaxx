969b567242b0381ca9d9b43869019ce52cbf0f48
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2022-01-27 15:13:17 +0100

    tqc/common: add common function for MTD / UBI support on [Q]SPI flash
    - generic hook to enable / disable [Q]SPI NOR based on detection state
    - generic hook for setup MTD partitions in kernel device tree before
      booting
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>
    (cherry picked from commit 7a31c999a1354054880947336d5aea818c40e9ad)
    
    Refresh of tqc-common-add-common-function

--- board/tqc/common/tqc_bb.h
+++ board/tqc/common/tqc_bb.h
@@ -1,8 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Copyright (C) 2013, 2014, 2016 TQ-Systems GmbH
+ * Copyright (C) 2013, 2014, 2016, 2022 TQ-Systems GmbH
  * Author: Markus Niebel <markus.niebel@tq-group.com>
- *
- * SPDX-License-Identifier:	GPL-2.0+
  */
 
 #ifndef __TQC_BB__
@@ -34,6 +33,27 @@ void tqc_bb_board_quiesce_devices(void);
  */
 #if defined(CONFIG_OF_BOARD_SETUP) && defined(CONFIG_OF_LIBFDT)
 int tqc_bb_ft_board_setup(void *blob, bd_t *bd);
+
+struct node_info;
+
+/**
+ * modify SPI NOR specific data in the FDT before booting the OS.
+ *
+ * @param blob		FDT blob to update
+ * @param path		path to spi controller for the SPI NOR device
+ * @param nodes		list of compatibles to scan
+ * @param node_count	path to spi controller for the SPI NOR device
+ *
+ * Test if optional SPI NOR is present and enable / disable it in device tree
+ * If present, use U-Boot standard env variables to add partition info to the
+ * SPI NOR device node. This is needed by kernel to use UBIFS in MTD partition
+ * as rootfs.
+ *
+ * If compiled without SPI NOR support no changes are applied to device tree
+ */
+void tqc_ft_spi_setup(void *blob, const char *path,
+		      const struct node_info *nodes,
+		      size_t node_count);
 #endif /* defined(CONFIG_OF_BOARD_SETUP) && defined(CONFIG_OF_LIBFDT) */
 
 #endif
