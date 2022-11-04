ee01b02764ab511fb781dd5a31feaf4ce5eb954f
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2022-11-04 12:36:13 +0100

    board/tq: forward port common baseboard API
    Ported from local fork of NXP vendor branch.
    
    - Add baseboard API header
    - Add weak defaults for baseboard functions
    
    Default implementations help to reduce code duplication for empty
    functions.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

---
+++ board/tq/common/tq_bb.h
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2013-2024, 2016-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#ifndef __TQ_BB_H
+#define __TQ_BB_H
+
+#include <linux/types.h>
+
+struct mmc;
+struct bd_info;
+
+int tq_bb_board_mmc_getwp(struct mmc *mmc);
+int tq_bb_board_mmc_getcd(struct mmc *mmc);
+int tq_bb_board_mmc_init(struct bd_info *bis);
+
+void tq_bb_board_init_f(ulong dummy);
+int tq_bb_board_early_init_f(void);
+int tq_bb_board_init(void);
+int tq_bb_board_late_init(void);
+int tq_bb_checkboard(void);
+void tq_bb_board_quiesce_devices(void);
+
+const char *tq_bb_get_boardname(void);
+
+#if IS_ENABLED(CONFIG_SPL_BUILD)
+void tq_bb_spl_board_init(void);
+#endif
+
+/*
+ * Device Tree Support
+ */
+#if IS_ENABLED(CONFIG_OF_BOARD_SETUP) && IS_ENABLED(CONFIG_OF_LIBFDT)
+int tq_bb_ft_board_setup(void *blob, struct bd_info *bis);
+#endif /* IS_ENABLED(CONFIG_OF_BOARD_SETUP) && IS_ENABLED(CONFIG_OF_LIBFDT) */
+
+#endif /* __TQ_BB_H */
