0f170524b196f984f0199a295acca604d2adfa60
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2022-11-17 12:08:50 +0100

    tq/common: forward port tq_spi_nor
    Adjust to new spi_flash_probe_bus_cs and drop non DM code.
    
    Even if flash is present, do not enable device in DT unconditionally.
    In cases the device is disabled deliberately, it gets enabled again
    without a chance to keep it disabled.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

---
+++ board/tq/common/tq_spi_nor.c
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2022-2024 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <dm.h>
+#include <fdt_support.h>
+#include <jffs2/load_kernel.h>
+#include <mtd_node.h>
+#include <spi.h>
+#include <spi_flash.h>
+
+#include "tq_bb.h"
+
+/* All this code must be called from ft_board_setup */
+#if IS_ENABLED(CONFIG_OF_BOARD_SETUP) && !IS_ENABLED(CONFIG_SPL_BUILD)
+
+static void tq_ft_fixup_spi_mtdparts(void *blob,
+				     const struct node_info *nodes,
+				     size_t node_count)
+{
+/* fdt_fixup_mtdparts is only present with CONFIG_FDT_FIXUP_PARTITIONS set */
+#if IS_ENABLED(CONFIG_FDT_FIXUP_PARTITIONS)
+	/*
+	 * Update MTD partition nodes using info from mtdparts env var
+	 * for [Q]SPI this needs the device probed.
+	 */
+	puts("   Updating MTD partitions...\n");
+	fdt_fixup_mtdparts(blob, nodes, node_count);
+#endif
+}
+
+void tq_ft_spi_setup(void *blob, const char *path,
+		     const struct node_info *nodes,
+		     size_t node_count)
+{
+	/* This code is specific for compiled in SPI FLASH support */
+	if (CONFIG_IS_ENABLED(DM_SPI_FLASH)) {
+		int off;
+		bool disable_flash = true;
+		unsigned int bus = CONFIG_SF_DEFAULT_BUS;
+		unsigned int cs = CONFIG_SF_DEFAULT_CS;
+
+		struct udevice *new;
+		int ret;
+
+		ret = spi_flash_probe_bus_cs(bus, cs, &new);
+		if (!ret) {
+			tq_ft_fixup_spi_mtdparts(blob, nodes, node_count);
+			disable_flash = false;
+		}
+
+		if (disable_flash) {
+			off = fdt_path_offset(blob, path);
+			if (off >= 0)
+				fdt_set_node_status(blob, off, FDT_STATUS_DISABLED);
+		}
+	}
+}
+
+#endif
