033a0789264c2f46274de9157cda4dff419bf966
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2022-11-17 14:47:13 +0100

    tq/common: port RTC helper functions for PCF85063
    Currently there is no driver. To apply board specific fixups,
    provide some useful functions.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- board/tq/common/tq_bb.h
+++ board/tq/common/tq_bb.h
@@ -37,4 +37,13 @@ struct bd_info;
 int tq_bb_ft_board_setup(void *blob, struct bd_info *bis);
 #endif /* defined(CONFIG_OF_BOARD_SETUP) && defined(CONFIG_OF_LIBFDT) */
 
+#if defined(CONFIG_TQ_RTC) && defined(CONFIG_DM_I2C)
+
+#define TQ_PCF85063_CLKOUT_OFF 0x07
+
+int tq_pcf85063_adjust_capacity(int bus, int address, int quartz_load);
+int tq_pcf85063_set_clkout(int bus, int address, uint8_t clkout);
+int tq_pcf85063_set_offset(int bus, int address, bool mode, int offset);
+#endif /* CONFIG_TQC_RTC */
+
 #endif /* __TQC_BB_H */
