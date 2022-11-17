146ffff38083165951761a579479ddc2a3699345
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2022-11-17 12:08:50 +0100

    tq/common: forward port tq_spi_nor
    Port from TQMa8 v2020.04 branch, rename filename and symbol prefixes
    from tqc to tq.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- board/tq/common/tq_bb.h
+++ board/tq/common/tq_bb.h
@@ -32,9 +32,28 @@ void tq_bb_spl_board_init(void);
  * Device Tree Support
  */
 #if defined(CONFIG_OF_BOARD_SETUP) && defined(CONFIG_OF_LIBFDT)
-struct bd_info;
+
+struct node_info;
 
 int tq_bb_ft_board_setup(void *blob, struct bd_info *bis);
+/**
+ * modify SPI NOR specific data in the FDT before booting the OS.
+ *
+ * @param blob		FDT blob to update
+ * @param path		path to spi controller for the SPI NOR device
+ * @param nodes		list of compatibles to scan
+ * @param node_count	path to spi controller for the SPI NOR device
+ *
+ * Test if optional SPI NOR is present and enable / disable it in device tree
+ * If present, use U-Boot standard env variables to add partition info to the
+ * SPI NOR device node. This is needed by kernel to use UBIFS in MTD partition
+ * as rootfs.
+ *
+ * If compiled without SPI NOR support no changes are applied to device tree
+ */
+void tq_ft_spi_setup(void *blob, const char *path,
+		     const struct node_info *nodes,
+		     size_t node_count);
 #endif /* defined(CONFIG_OF_BOARD_SETUP) && defined(CONFIG_OF_LIBFDT) */
 
 #if defined(CONFIG_TQ_RTC) && defined(CONFIG_DM_I2C)
---
+++ board/tq/common/tq_spi_nor.c
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2022 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <dm/lists.h>
+#include <fdt_support.h>
+#include <jffs2/load_kernel.h>
+#include <mtd_node.h>
+#include <spi.h>
+#include <spi_flash.h>
+
+#include "tq_bb.h"
+
+/* All this code must be called from ft_board_setup */
+#if defined(CONFIG_OF_BOARD_SETUP) && !defined(CONFIG_SPL_BUILD)
+
+static void tq_ft_fixup_spi_mtdparts(void *blob,
+				     const struct node_info *nodes,
+				     size_t node_count)
+{
+/* fdt_fixup_mtdparts is only present with CONFIG_FDT_FIXUP_PARTITIONS set */
+#if defined(CONFIG_FDT_FIXUP_PARTITIONS)
+	/*
+	 * Update MTD partition nodes using info from mtdparts env var
+	 * for [Q]SPI this needs the device probed.
+	 */
+	puts("   Updating MTD partitions...\n");
+	fdt_fixup_mtdparts(blob, nodes, node_count);
+#endif
+}
+
+void tq_ft_spi_setup(void *blob, const char *path,
+		     const struct node_info *nodes,
+		     size_t node_count)
+{
+/* This code is specific for compiled in SPI FLASH support */
+#if defined(CONFIG_SPI_FLASH)
+	int off;
+	bool enable_flash = false;
+
+	unsigned int bus = CONFIG_SF_DEFAULT_BUS;
+	unsigned int cs = CONFIG_SF_DEFAULT_CS;
+	unsigned int speed = CONFIG_SF_DEFAULT_SPEED;
+	unsigned int mode = CONFIG_SF_DEFAULT_MODE;
+
+	/*
+	 * TODO: to make this code portable until we land it in upstream
+	 * U-Boot, we support DM and non DM case at least in theory.
+	 * when upstreaming, non DM code should be deleted
+	 */
+	if (CONFIG_IS_ENABLED(DM_SPI_FLASH)) {
+		struct udevice *new;
+		int ret;
+
+		ret = spi_flash_probe_bus_cs(bus, cs, speed, mode, &new);
+		if (!ret) {
+			tq_ft_fixup_spi_mtdparts(blob, nodes, node_count);
+			enable_flash = 1;
+		}
+	} else {
+		struct spi_flash *new;
+
+		new = spi_flash_probe(bus, cs, speed, mode);
+		if (new) {
+			tq_ft_fixup_spi_mtdparts(blob, nodes, node_count);
+			spi_flash_free(new);
+			enable_flash = 1;
+		}
+	}
+
+	off = fdt_path_offset(blob, path);
+	if (off >= 0) {
+		printf("%s %s\n", (enable_flash) ? "enable" : "disable",
+		       path);
+		fdt_set_node_status(blob, off, (enable_flash) ?
+				    FDT_STATUS_OKAY : FDT_STATUS_DISABLED);
+	}
+#endif
+}
+
+#endif
