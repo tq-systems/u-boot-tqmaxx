be7ca0273f3f0ab62fb6761e30b4c120616cb1ff
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2022-12-21 16:20:26 +0100

    tqma8mpxl: [cosmetic] fix SPDX tag in board config header
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- include/configs/tqma8mpxl-mba8mpxl.h
+++ include/configs/tqma8mpxl-mba8mpxl.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0+ */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * Copyright (c) 2021 - 2022 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
  * D-82229 Seefeld, Germany.
