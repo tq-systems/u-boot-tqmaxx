1b37f320e42c2dd77eb5142039df06a979758b5c
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2022-12-21 15:28:23 +0100

    tq/common: Add dynamic SoM feature handling
    Our SoM usually have some optional components.
    To prevent potential warnings on SoM without optional
    components provide logic to be used from board_ft_fixup.
    
    This should be used by baseboard specific implementation of
    board_ft_fixup. This allows baseboards to use SoM variant
    with non-assembled feature but same feature realized on
    baseboard. Examples for this situation would be:
    
    - SOM without e-MMC but SD / SDIO / e-MMC at baseboard
      connected to the controller IP
    - SOM without EEPROM but EEPROM on baseboard at same I2C
      address
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

---
+++ board/tq/common/tq_som_features.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * (C) Copyright 2022-2024 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <fdt_support.h>
+
+#include "tq_som_features.h"
+
+#if !IS_ENABLED(CONFIG_SPL_BUILD)
+
+__weak const struct tq_som_feature_list *tq_board_detect_features(void)
+{
+	return NULL;
+}
+
+/* This code should be called from ft_board_setup */
+void tq_ft_fixup_features(void *blob, const struct tq_som_feature_list *features)
+{
+	size_t i;
+	int off;
+
+	if (!features || !blob)
+		return;
+
+	for (i = 0; i < features->entries; ++i) {
+		bool present = features->list[i].present;
+		const char *path = features->list[i].dt_path;
+
+		if (!present && path) {
+			off = fdt_path_offset(blob, path);
+			if (off >= 0) {
+				pr_info("disable %s\n", path);
+				fdt_set_node_status(blob, off, FDT_STATUS_DISABLED);
+			}
+		}
+	}
+}
+
+#endif /* !IS_ENABLED(CONFIG_SPL_BUILD) */
