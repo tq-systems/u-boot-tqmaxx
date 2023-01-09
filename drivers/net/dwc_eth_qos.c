nxp-imx/uboot-imx

commit 9e08ba4775934112514fb6b743477c434fb4930f

MLK-23574-29 net: Update eQos driver and FEC driver to use eth phy interfaces
Update eQoS and fec ethernet drivers to support shared MDIO framework

Signed-off-by: Ye Li <ye.li@nxp.com>
(cherry picked from commit 5ce41af29fad17b2d26a96e589716ae5b617355e)

drivers/net/dwc_eth_qos.c
@@ -41,6 +41,7 @@
 #include <wait_bit.h>
 #include <asm/gpio.h>
 #include <asm/io.h>
+#include <eth_phy.h>
 
 /* Core registers */
 
@@ -1046,7 +1047,14 @@ static int eqos_start(struct udevice *dev)
 	 * don't need to reconnect/reconfigure again
 	 */
 	if (!eqos->phy) {
-		eqos->phy = phy_connect(eqos->mii, -1, dev,
+		int addr = -1;
+#ifdef CONFIG_DM_ETH_PHY
+		addr = eth_phy_get_addr(dev);
+#endif
+#ifdef DWC_NET_PHYADDR
+		addr = DWC_NET_PHYADDR;
+#endif
+		eqos->phy = phy_connect(eqos->mii, addr, dev,
 					eqos->config->interface(dev));
 		if (!eqos->phy) {
 			pr_err("phy_connect() failed");
@@ -1740,22 +1748,31 @@ static int eqos_probe(struct udevice *dev)
 		goto err_remove_resources_core;
 	}
 
-	eqos->mii = mdio_alloc();
+#ifdef CONFIG_DM_ETH_PHY
+	eqos->mii = eth_phy_get_mdio_bus(dev);
+#endif
 	if (!eqos->mii) {
-		pr_err("mdio_alloc() failed");
-		ret = -ENOMEM;
-		goto err_remove_resources_tegra;
-	}
-	eqos->mii->read = eqos_mdio_read;
-	eqos->mii->write = eqos_mdio_write;
-	eqos->mii->priv = eqos;
-	strcpy(eqos->mii->name, dev->name);
+		eqos->mii = mdio_alloc();
+		if (!eqos->mii) {
+			pr_err("mdio_alloc() failed");
+			ret = -ENOMEM;
+			goto err_remove_resources_tegra;
+		}
+		eqos->mii->read = eqos_mdio_read;
+		eqos->mii->write = eqos_mdio_write;
+		eqos->mii->priv = eqos;
+		strcpy(eqos->mii->name, dev->name);
 
-	ret = mdio_register(eqos->mii);
-	if (ret < 0) {
-		pr_err("mdio_register() failed: %d", ret);
-		goto err_free_mdio;
+		ret = mdio_register(eqos->mii);
+		if (ret < 0) {
+			pr_err("mdio_register() failed: %d", ret);
+			goto err_free_mdio;
+		}
 	}
+
+#ifdef CONFIG_DM_ETH_PHY
+	eth_phy_set_mdio_bus(dev, eqos->mii);
+#endif
 
 	debug("%s: OK\n", __func__);
 	return 0;

drivers/net/fec_mxc.c
@@ -27,6 +27,7 @@
 #include <asm-generic/gpio.h>
 
 #include "fec_mxc.h"
+#include <eth_phy.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -1453,15 +1454,26 @@ static int fecmxc_probe(struct udevice *dev)
 
 	fec_reg_setup(priv);
 	priv->dev_id = dev->seq;
-#ifdef CONFIG_FEC_MXC_MDIO_BASE
-	bus = fec_get_miibus((ulong)CONFIG_FEC_MXC_MDIO_BASE, dev->seq);
-#else
-	bus = fec_get_miibus((ulong)priv->eth, dev->seq);
+
+#ifdef CONFIG_DM_ETH_PHY
+	bus = eth_phy_get_mdio_bus(dev);
 #endif
+
+	if (!bus) {
+#ifdef CONFIG_FEC_MXC_MDIO_BASE
+		bus = fec_get_miibus((ulong)CONFIG_FEC_MXC_MDIO_BASE, dev->seq);
+#else
+		bus = fec_get_miibus((ulong)priv->eth, dev->seq);
+#endif
+	}
 	if (!bus) {
 		ret = -ENOMEM;
 		goto err_mii;
 	}
+
+#ifdef CONFIG_DM_ETH_PHY
+	eth_phy_set_mdio_bus(dev, bus);
+#endif
 
 	priv->bus = bus;
 	priv->interface = pdata->phy_interface;

tq-systems/u-boot-tqmaxx

commit cd7bf13500f92fc91c62fab008b2647bd545e7bb

net: dwc_eth_qos: unregister mdio bus before free if needed
If we fail to probe for whatever reason, we cannot unregister/free the
MII bus unless we registered it ourselves.

To ease error handling and readability, factor out manual mdio_bus
allocation and registration the same way as implemented in FEC
driver. This saves us from using the case where mdio bus was
allocated but not registered.

Fixes: 9e08ba47759 ("MLK-23574-29 net: Update eQos driver and FEC driver to use eth phy interfaces")

Reported-by: Paul Gerber <Paul.Gerber@tq-group.com>
Tested-by: Paul Gerber <Paul.Gerber@tq-group.com>
Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

drivers/net/dwc_eth_qos.c
@@ -2039,9 +2039,37 @@ static int eqos_remove_resources_imx(struct udevice *dev)
 	return 0;
 }
 
+struct mii_dev *eqos_get_miibus(struct eqos_priv *eqos,
+				const char *name)
+{
+	struct mii_dev *bus;
+	int ret;
+
+	bus = mdio_alloc();
+	if (!bus) {
+		pr_err("mdio_alloc() failed");
+		return NULL;
+	}
+	bus->read = eqos_mdio_read;
+	bus->write = eqos_mdio_write;
+	bus->priv = eqos;
+	strncpy(bus->name, name, ARRAY_SIZE(bus->name));
+
+	ret = mdio_register(bus);
+	if (ret < 0) {
+		pr_err("mdio_register() failed: %d", ret);
+		free(bus);
+		return NULL;
+	}
+
+	return bus;
+}
+
 static int eqos_probe(struct udevice *dev)
 {
 	struct eqos_priv *eqos = dev_get_priv(dev);
+	struct mii_dev *bus = NULL;
+	bool dm_mii_bus = true;
 	int ret;
 
 	debug("%s(dev=%p):\n", __func__, dev);
@@ -2072,45 +2100,42 @@ static int eqos_probe(struct udevice *dev)
 	}
 
 #ifdef CONFIG_DM_ETH_PHY
-	eqos->mii = eth_phy_get_mdio_bus(dev);
+	bus = eth_phy_get_mdio_bus(dev);
 #endif
-	if (!eqos->mii) {
-		eqos->mii = mdio_alloc();
-		if (!eqos->mii) {
-			pr_err("mdio_alloc() failed");
-			ret = -ENOMEM;
-			goto err_remove_resources_tegra;
-		}
-		eqos->mii->read = eqos_mdio_read;
-		eqos->mii->write = eqos_mdio_write;
-		eqos->mii->priv = eqos;
-		strcpy(eqos->mii->name, dev->name);
+	if (!bus) {
+		dm_mii_bus = false;
+		bus = eqos_get_miibus(eqos, dev->name);
+	}
 
-		ret = mdio_register(eqos->mii);
-		if (ret < 0) {
-			pr_err("mdio_register() failed: %d", ret);
-			goto err_free_mdio;
-		}
+	if (!bus) {
+		ret = -ENOMEM;
+		goto err_remove_resources_tegra;
 	}
 
 #ifdef CONFIG_DM_ETH_PHY
-	eth_phy_set_mdio_bus(dev, eqos->mii);
+	eth_phy_set_mdio_bus(dev, bus);
 #endif
 
+	eqos->mii = bus;
 	ret = eqos_phy_init(eqos, dev);
 	if (ret || !eqos->phy)
 		goto err_free_mdio;
 
 	ret = phy_config(eqos->phy);
-	if (ret < 0)
+	if (ret) {
 		pr_err("phy_config() failed: %d", ret);
+		goto err_free_mdio;
+	}
 
 	debug("%s: OK\n", __func__);
 
 	return ret;
 
 err_free_mdio:
-	mdio_free(eqos->mii);
+	if (!dm_mii_bus) {
+		mdio_unregister(eqos->mii);
+		mdio_free(eqos->mii);
+	}
 err_remove_resources_tegra:
 	eqos->config->ops->eqos_remove_resources(dev);
 err_remove_resources_core: