1a109a51e2716d5ce2ad03b11745304fe24f00fb
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2023-01-13 15:58:22 +0100

    tqma8mpxl: [cosmetic] adjust style of copyright lines
    No space between start and stop year.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- board/tqc/tqma8mpxl/tqma8mpxl-usbg.c
+++ board/tqc/tqma8mpxl/tqma8mpxl-usbg.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) 2021 - 2022 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * Copyright (c) 2021-2022 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
  * D-82229 Seefeld, Germany.
  * Author: Paul Gerber
  */
