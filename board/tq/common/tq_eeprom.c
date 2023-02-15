c4bda1bd9faf6898736cb40aebafda0ef3f9e6ae
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2023-02-15 15:16:51 +0100

    tq: common: add support for new serial number format
    Old format is 'nnnnnnnn' with 8 decimal digits in ASCII
    New format is 'TQmmmmmm' with 6 decimal digits or uppercase
    characters in ASCII, e.g.  [0-9,A-Z]
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- board/tq/common/tq_eeprom.c
+++ board/tq/common/tq_eeprom.c
@@ -229,24 +229,49 @@ int tq_parse_eeprom_mac(struct tq_eeprom_data * const eeprom, char *buf,
 	return !(is_valid_ethaddr(p));
 }
 
-int tq_parse_eeprom_serial(struct tq_eeprom_data * const eeprom,
-			   char *buf, size_t len)
+static int tq_parse_eeprom_serial_new(struct tq_eeprom_data * const eeprom,
+				      char *buf)
 {
 	unsigned int i;
 
-	if (!buf || !eeprom)
-		return -1;
-	if (len < (sizeof(eeprom->serial) + 1))
-		return -1;
+	for (i = 0; i < (sizeof(eeprom->serial)) &&
+			(isdigit(eeprom->serial[i]) ||
+			 isupper(eeprom->serial[i])); i++)
+		buf[i] = eeprom->serial[i];
+	buf[i] = '\0';
+	if (sizeof(eeprom->serial) != strlen(buf))
+		return -EINVAL;
+
+	return 0;
+}
+
+static int tq_parse_eeprom_serial_old(struct tq_eeprom_data * const eeprom,
+				      char *buf)
+{
+	unsigned int i;
 
 	for (i = 0; i < (sizeof(eeprom->serial)) && isdigit(eeprom->serial[i]);
 	     i++)
 		buf[i] = eeprom->serial[i];
 	buf[i] = '\0';
 	if (sizeof(eeprom->serial) != strlen(buf))
-		return -1;
+		return -EINVAL;
 
 	return 0;
+}
+
+int tq_parse_eeprom_serial(struct tq_eeprom_data * const eeprom,
+			   char *buf, size_t len)
+{
+	if (!buf || !eeprom)
+		return -EINVAL;
+	if (len < (sizeof(eeprom->serial) + 1))
+		return -EINVAL;
+
+	if (eeprom->serial[0] == 'T' && eeprom->serial[1] == 'Q')
+		return tq_parse_eeprom_serial_new(eeprom, buf);
+	else
+		return tq_parse_eeprom_serial_old(eeprom, buf);
 }
 
 int tq_parse_eeprom_id(struct tq_eeprom_data * const eeprom, char *buf,
