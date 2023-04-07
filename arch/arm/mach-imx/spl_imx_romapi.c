c070732e89e5f5b3bd245b1abe074c0b67545a73
Ye Li <ye.li@nxp.com>
2023-04-07 10:22:40 +0800

    LFU-464-11 imx: spl_imx_romapi: Fix wrong offset for flexspi
    We use fixed offset for flexspi nor image in imx-mkimage. So fix
    the wrong offset introduced by upstream.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- arch/arm/mach-imx/spl_imx_romapi.c
+++ arch/arm/mach-imx/spl_imx_romapi.c
@@ -35,17 +35,14 @@ ulong spl_romapi_raw_seekable_read(u32 offset, u32 size, void *buf)
 
 ulong __weak spl_romapi_get_uboot_base(u32 image_offset, u32 rom_bt_dev)
 {
-	u32 sector = 0;
+	u32 offset;
 
-	/*
-	 * Some boards use this value even though MMC is not enabled in SPL, for
-	 * example imx8mn_bsh_smm_s2
-	 */
-#ifdef CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_USE_SECTOR
-	sector = CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR;
-#endif
+	if (((rom_bt_dev >> 16) & 0xff) ==  BT_DEV_TYPE_FLEXSPINOR)
+		offset = CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512;
+	else
+		offset = image_offset + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512 - 0x8000;
 
-	return image_offset + sector * 512 - 0x8000;
+	return offset;
 }
 
 static int is_boot_from_stream_device(u32 boot)
