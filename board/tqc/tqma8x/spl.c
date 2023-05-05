6287c8cdd42be25327deaf00c5a83bf7e6aaacb8
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2023-05-05 17:49:39 +0200

    tqma8x: [cosmetic]  adjust copyright header
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- board/tqc/tqma8x/spl.c
+++ board/tqc/tqma8x/spl.c
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * (C) Copyright 2020 TQ-Systems GmbH
- * Markus Niebel <Markus.Niebel@tq-group.com>
+ * (C) Copyright (c) 2020-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
  */
 
 #include <common.h>
