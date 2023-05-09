81b2c52cc33f048bb5374ed45ffed14c3535cbc1
Paul Gerber <Paul.Gerber@tq-group.com>
2023-05-09 12:20:28 +0200

    board: tqc: import TQ EEPROM handling
    Based on TQMa8-v2020.04_imx_5.4.70_2.3.0 branch.
    
    Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>

---
+++ board/tqc/common/tqc_eeprom.c
@@ -0,0 +1,229 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2014-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <common.h>
+#include <dm/uclass.h>
+#include <i2c_eeprom.h>
+#include <i2c.h>
+#include <linux/ctype.h>
+#include <malloc.h>
+#include <net.h>
+#include <u-boot/crc.h>
+
+#include "tqc_eeprom.h"
+
+/*
+ * read_eeprom - read the given EEPROM into memory
+ */
+int tqc_read_eeprom_buf(unsigned int bus, unsigned int i2c_addr,
+			unsigned int alen, unsigned int addr,
+			size_t bsize, uchar *buf)
+{
+	int ret;
+#ifdef CONFIG_DM_I2C
+	struct udevice *dev;
+#else
+	unsigned int oldbus;
+#endif
+
+	if (!buf)
+		return -1;
+
+#ifdef CONFIG_DM_I2C
+	ret = i2c_get_chip_for_busnum(bus, i2c_addr, alen, &dev);
+	if (ret) {
+		debug("%s: Cannot find I2C chip for bus %d\n", __func__, bus);
+		return ret;
+	}
+
+	ret = dm_i2c_read(dev, addr, buf, bsize);
+#else
+	oldbus = i2c_get_bus_num();
+	i2c_set_bus_num(bus);
+	ret = i2c_read(i2c_addr, addr, alen, buf, bsize);
+	i2c_set_bus_num(oldbus);
+#endif
+	return ret;
+}
+
+#if !defined(CONFIG_SPL_BUILD)
+
+#if defined(CONFIG_I2C_EEPROM)
+int tq_read_eeprom_at(int seq, uint offset, struct tqc_eeprom_data *eeprom)
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = uclass_get_device_by_seq(UCLASS_I2C_EEPROM, seq, &dev);
+	if (ret) {
+		debug("%s: Cannot find i2c_eeprom%d\n", __func__, seq);
+		return ret;
+	}
+
+	return i2c_eeprom_read(dev, offset, (u8 *)eeprom, sizeof(*eeprom));
+}
+#endif
+
+int tqc_parse_eeprom_mac(struct tqc_eeprom_data * const eeprom, char *buf,
+			 size_t len)
+{
+	u8 *p;
+	int ret;
+
+	if (!buf || !eeprom)
+		return -1;
+	/* MAC address */
+	p = eeprom->mac;
+	ret = snprintf(buf, len, "%02x:%02x:%02x:%02x:%02x:%02x",
+		       p[0], p[1], p[2], p[3], p[4], p[5]);
+	if (ret < 0)
+		return ret;
+	if (ret >= len)
+		return ret;
+
+	return !(is_valid_ethaddr(p));
+}
+
+static int tq_parse_eeprom_serial_new(struct tqc_eeprom_data * const eeprom,
+				      char *buf)
+{
+	unsigned int i;
+
+	for (i = 0; i < (sizeof(eeprom->serial)) &&
+	     (isdigit(eeprom->serial[i]) || isupper(eeprom->serial[i])); i++)
+		buf[i] = eeprom->serial[i];
+	buf[i] = '\0';
+	if (sizeof(eeprom->serial) != strlen(buf))
+		return -EINVAL;
+
+	return 0;
+}
+
+static int tq_parse_eeprom_serial_old(struct tqc_eeprom_data * const eeprom,
+				      char *buf)
+{
+	unsigned int i;
+
+	for (i = 0; i < (sizeof(eeprom->serial)) && isdigit(eeprom->serial[i]);
+	     i++)
+		buf[i] = eeprom->serial[i];
+	buf[i] = '\0';
+	if (sizeof(eeprom->serial) != strlen(buf))
+		return -EINVAL;
+
+	return 0;
+}
+
+int tqc_parse_eeprom_serial(struct tqc_eeprom_data * const eeprom,
+			    char *buf, size_t len)
+{
+	if (!buf || !eeprom)
+		return -EINVAL;
+	if (len < (sizeof(eeprom->serial) + 1))
+		return -EINVAL;
+
+	if (eeprom->serial[0] == 'T' && eeprom->serial[1] == 'Q')
+		return tq_parse_eeprom_serial_new(eeprom, buf);
+	else
+		return tq_parse_eeprom_serial_old(eeprom, buf);
+}
+
+int tqc_parse_eeprom_id(struct tqc_eeprom_data * const eeprom, char *buf,
+			 size_t len)
+{
+	unsigned int i;
+
+	if (!buf || !eeprom)
+		return -1;
+	if (len < (sizeof(eeprom->id) + 1))
+		return -1;
+
+	for (i = 0; i < sizeof(eeprom->id) && isprint(eeprom->id[i]) &&
+	     isascii(eeprom->id[i]); ++i)
+		buf[i] = eeprom->id[i];
+	buf[i] = '\0';
+
+	return 0;
+}
+
+/*
+ * show_eeprom - display the contents of the module EEPROM
+ */
+int tqc_show_eeprom(struct tqc_eeprom_data * const eeprom, const char *id)
+{
+	/* must hold largest field of eeprom data */
+	char safe_string[(TQC_EE_BDID_BYTES) + 1];
+
+	if (!eeprom)
+		return -1;
+
+	puts(id);
+	puts(" EEPROM:\n");
+	/* ID */
+	tqc_parse_eeprom_id(eeprom, safe_string, ARRAY_SIZE(safe_string));
+	if (strncmp(safe_string, id, strlen(id)) == 0)
+		printf("  ID: %s\n", safe_string);
+	else
+		puts("  unknown hardware variant\n");
+
+	/* Serial number */
+	if (tqc_parse_eeprom_serial(eeprom, safe_string,
+				    ARRAY_SIZE(safe_string)) == 0)
+		printf("  SN: %s\n", safe_string);
+	else
+		puts("  unknown serial number\n");
+	/* MAC address */
+	if (tqc_parse_eeprom_mac(eeprom, safe_string,
+				 ARRAY_SIZE(safe_string)) == 0)
+		printf("  MAC: %s\n", safe_string);
+	else
+		puts("  invalid MAC\n");
+
+	return 0;
+}
+
+/*
+ * read_eeprom - read the given EEPROM into memory
+ */
+int tqc_read_eeprom_at(unsigned int bus, unsigned int i2c_addr,
+		       unsigned int alen, unsigned int addr,
+		       struct tqc_eeprom_data *eeprom)
+{
+	return tqc_read_eeprom_buf(bus, i2c_addr, alen, addr, sizeof(*eeprom),
+				   (uchar *)eeprom);
+}
+
+#if defined(CONFIG_SYS_I2C_EEPROM_ADDR_LEN)
+int tqc_read_eeprom(unsigned int bus, unsigned int addr,
+		    struct tqc_eeprom_data *eeprom)
+{
+	return tqc_read_eeprom_at(bus, addr,
+				  CONFIG_SYS_I2C_EEPROM_ADDR_LEN, addr, eeprom);
+}
+#endif
+
+int tqc_board_handle_eeprom_data(const char *board_name,
+				 struct tqc_eeprom_data * const eeprom)
+{
+	char sstring[(TQC_EE_BDID_BYTES) + 1];
+
+	tqc_parse_eeprom_id(eeprom, sstring, ARRAY_SIZE(sstring));
+
+#ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+	if (strncmp(sstring, board_name, strlen(board_name)) == 0)
+		env_set("boardtype", sstring);
+	if (tqc_parse_eeprom_serial(eeprom, sstring,
+				    ARRAY_SIZE(sstring)) == 0)
+		env_set("serial#", sstring);
+	else
+		env_set("serial#", "???");
+#endif
+
+	return tqc_show_eeprom(eeprom, board_name);
+}
+
+#endif
---
+++ board/tqc/common/tqc_eeprom.h
@@ -0,0 +1,91 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2014-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#ifndef __TQC_EEPROM_H__
+#define __TQC_EEPROM_H__
+
+int tqc_read_eeprom_buf(unsigned int bus, unsigned int i2c_addr,
+			unsigned int alen, unsigned int addr,
+			size_t bsize, uchar *buf);
+
+#if !defined(CONFIG_SPL_BUILD)
+
+/*
+ * static EEPROM layout
+ */
+#define TQC_EE_HRCW_BYTES		0x20
+#define TQC_EE_MAC_BYTES		6
+#define TQC_EE_RSV1_BYTES		10
+#define TQC_EE_SERIAL_BYTES		8
+#define TQC_EE_RSV2_BYTES		8
+#define TQC_EE_BDID_BYTES		0x40
+
+struct __attribute__ ((__packed__)) tqc_eeprom_data {
+	u8 hrcw_primary[TQC_EE_HRCW_BYTES];
+	u8 mac[TQC_EE_MAC_BYTES];		/* 0x20 ... 0x25 */
+	u8 rsv1[TQC_EE_RSV1_BYTES];
+	u8 serial[TQC_EE_SERIAL_BYTES];		/* 0x30 ... 0x37 */
+	u8 rsv2[TQC_EE_RSV2_BYTES];
+	u8 id[TQC_EE_BDID_BYTES];		/* 0x40 ... 0x7f */
+};
+
+int tqc_parse_eeprom_mac(struct tqc_eeprom_data * const eeprom, char *buf,
+			 size_t len);
+
+int tqc_parse_eeprom_serial(struct tqc_eeprom_data * const eeprom, char *buf,
+			    size_t len);
+int tqc_parse_eeprom_id(struct tqc_eeprom_data * const eeprom, char *buf,
+			size_t len);
+int tqc_show_eeprom(struct tqc_eeprom_data *const eeprom, const char *id);
+int tqc_read_eeprom_at(unsigned int bus, unsigned int i2c_addr,
+		       unsigned int alen, unsigned int addr,
+		       struct tqc_eeprom_data *eeprom);
+int tqc_board_handle_eeprom_data(const char *board_name,
+				 struct tqc_eeprom_data * const eeprom);
+
+#if defined(CONFIG_SYS_I2C_EEPROM_ADDR_LEN)
+int tqc_read_eeprom(unsigned int bus, unsigned int addr,
+		    struct tqc_eeprom_data *eeprom);
+#endif /* CONFIG_SYS_I2C_EEPROM_ADDR_LEN */
+
+#if defined(CONFIG_I2C_EEPROM)
+/*
+ * new versions, to replace above tqc_read_eeprom_at
+ */
+
+/**
+ * Reads a tqc_eeprom_data from an EEPROM, at a given offset
+ * from the start of the EEPROM
+ */
+int tq_read_eeprom_at(int seq, uint offset, struct tqc_eeprom_data *eeprom);
+#else
+static inline int tq_read_eeprom_at(int seq, uint offset,
+				    struct tqc_eeprom_data *eeprom)
+{
+	return -ENOTSUPP;
+}
+#endif
+
+/**
+ * Reads a tqc_eeprom_data from an EEPROM give by seq nr at address zero
+ */
+static inline int tq_read_eeprom(int seq, struct tqc_eeprom_data *eeprom)
+{
+	return tq_read_eeprom_at(seq, 0, eeprom);
+}
+
+/**
+ * Reads a tqc_eeprom_data from EEPROM with by seq nr 0 at address zero
+ */
+static inline int tq_read_module_eeprom(struct tqc_eeprom_data *eeprom)
+{
+	return tq_read_eeprom(0, eeprom);
+}
+
+#endif /* CONFIG_SPL_BUILD */
+
+#endif
