c65a7c40dccf0f8998a67f7c48e9f9db5c57c817
Gregor Herburger <gregor.herburger@tq-group.com>
2023-06-20 13:04:47 +0200

    board: tq: common: tq_mbls10xxa: fix return value check
    tq_mbls10xxa_fixup_enet_fixed_link likes to return on error for
    fdt_setprop_string but never assigns return value to variable ret.
    
    Fixes: c0fbd4d8da0a ("board: tq: common: tq_mbls10xxa: remove fdt_increase_size")
    Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

--- board/tq/common/tq_mbls10xxa.c
+++ board/tq/common/tq_mbls10xxa.c
@@ -572,11 +572,11 @@ int tq_mbls10xxa_fixup_enet_fixed_link(void *fdt, char *enet_alias, int id, char
 	f_link.link_speed = cpu_to_fdt32(1000);
 	f_link.pause = 0;
 	f_link.asym_pause = 0;
-	fdt_setprop(fdt, offset, "fixed-link", &f_link, sizeof(f_link));
+	ret = fdt_setprop(fdt, offset, "fixed-link", &f_link, sizeof(f_link));
 	if (ret)
 		return ret;
 
-	fdt_setprop_string(fdt, offset, "phy-connection-type", connection);
+	ret = fdt_setprop_string(fdt, offset, "phy-connection-type", connection);
 	if (ret)
 		return ret;
 
