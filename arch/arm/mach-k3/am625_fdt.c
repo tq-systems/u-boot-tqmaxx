e60c076ab759fb9a645f817b37df1772e324579e
Matthias Schiffer <matthias.schiffer@tq-group.com>
2023-10-18 13:01:19 +0200

    arm: mach-k3: am62: disable nodes instead of deleting them
    Deleting nodes should usually be avoided, as it can lead to dangling
    phandle references, that are not handled gracefully by all drivers. For
    most devices, setting the status to "disabled" has the same effect.
    
    For historical reasons, CPU nodes are handled differently by Linux and the
    "disabled" status is ignored. Since Linux 5.17 [1] /
    Devicetree-spec 0.4 [2], the "fail" status can be used instead, but as we
    don't want to limit supported kernel versions that much, CPU nodes are
    still deleted for now.
    
    All delete/disable calls are changed to use the convenient *_by_pathf APIs.
    
    [1] https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=4fdd0736a3b1634613d1d2eeb3328d27522052fb
    [2] https://github.com/devicetree-org/devicetree-specification/commit/4294fd73f3635f81912870bd247ce1516eea5deb
    
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

--- arch/arm/mach-k3/am625_fdt.c
+++ arch/arm/mach-k3/am625_fdt.c
@@ -4,39 +4,38 @@
  */
 
 #include <asm/hardware.h>
-#include "common_fdt.h"
 #include <fdt_support.h>
 
 static void fdt_fixup_cores_nodes_am625(void *blob, u32 cores)
 {
-	char node_path[32];
 	int core_nr;
 
 	for (core_nr = 1; core_nr < 4; core_nr++) {
 		if (cores & BIT(core_nr))
 			continue;
 
-		snprintf(node_path, sizeof(node_path), "/cpus/cpu@%d", core_nr);
-		fdt_del_node_path(blob, node_path);
-		snprintf(node_path, sizeof(node_path), "/cpus/cpu-map/cluster0/core%d", core_nr);
-		fdt_del_node_path(blob, node_path);
-		snprintf(node_path, sizeof(node_path), "/bus@f0000/watchdog@e0%d0000", core_nr);
-		fdt_del_node_path(blob, node_path);
+		fdt_del_node_by_pathf(blob, "/cpus/cpu@%x", core_nr);
+		fdt_del_node_by_pathf(blob, "/cpus/cpu-map/cluster0/core%d",
+				      core_nr);
+		fdt_status_disabled_by_pathf(blob,
+					     "/bus@f0000/watchdog@e0%x0000",
+					     core_nr);
 	}
 }
 
 static void fdt_fixup_gpu_nodes_am625(void *blob, bool has_gpu)
 {
 	if (!has_gpu) {
-		fdt_del_node_path(blob, "/bus@f0000/gpu@fd00000");
-		fdt_del_node_path(blob, "/bus@f0000/watchdog@e0f0000");
+		fdt_status_disabled_by_pathf(blob, "/bus@f0000/gpu@fd00000");
+		fdt_status_disabled_by_pathf(blob,
+					     "/bus@f0000/watchdog@e0f0000");
 	}
 }
 
 static void fdt_fixup_pru_node_am625(void *blob, bool has_pru)
 {
 	if (!has_pru)
-		fdt_del_node_path(blob, "/bus@f0000/pruss@30040000");
+		fdt_status_disabled_by_pathf(blob, "/bus@f0000/pruss@30040000");
 }
 
 static u32 k3_get_cores(void)
