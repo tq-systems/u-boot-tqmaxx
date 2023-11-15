bb4bfecd2f805e3277d019186aa387dc65d8b73f
Matthias Schiffer <matthias.schiffer@tq-group.com>
2023-11-15 13:21:01 +0100

    board: tqma335x: disable boot device fallback by default
    Only try boot from the medium U-Boot was booted from, to match the
    behavior of our other boards.
    
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

--- include/configs/tqma335x.h
+++ include/configs/tqma335x.h
@@ -145,7 +145,8 @@
 		"fi; "                                                         \
 		"echo ... failed\0"                                            \
 	DFUARGS \
-	BOOTENV
+	BOOTENV \
+	"boot_targets=legacy_mmc0\0"
 #endif
 
 /* NS16550 Configuration */
