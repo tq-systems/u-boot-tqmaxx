c17de4d709ec4a83607228c8b3f5859fa9905a9c
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2023-12-04 15:20:59 +0100

    usb: f_fastboot: use FSL_FASTBOOT instead FASTBOOT_UUU_SUPPORT
    The symbol referenced by the guarded code comes from fastboot extensions
    selected by FSL_FASTBOOT. Correct the guards.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- drivers/usb/gadget/f_fastboot.c
+++ drivers/usb/gadget/f_fastboot.c
@@ -204,7 +204,7 @@ static struct usb_gadget_strings *fastboot_strings[] = {
 	NULL,
 };
 
-#if CONFIG_IS_ENABLED(FASTBOOT_UUU_SUPPORT)
+#if CONFIG_IS_ENABLED(FSL_FASTBOOT)
 extern struct stdio_dev g_fastboot_stdio;
 #endif
 
@@ -300,7 +300,7 @@ static int fastboot_bind(struct usb_configuration *c, struct usb_function *f)
 	if (s)
 		g_dnl_set_serialnumber((char *)s);
 
-#if CONFIG_IS_ENABLED(FASTBOOT_UUU_SUPPORT)
+#if CONFIG_IS_ENABLED(FSL_FASTBOOT)
 	stdio_register(&g_fastboot_stdio);
 #endif
 
@@ -313,7 +313,7 @@ static void fastboot_unbind(struct usb_configuration *c, struct usb_function *f)
 	list_del(&fb_os_desc.ext_prop);
 	memset(fastboot_func, 0, sizeof(*fastboot_func));
 
-#if CONFIG_IS_ENABLED(FASTBOOT_UUU_SUPPORT) && CONFIG_IS_ENABLED(SYS_STDIO_DEREGISTER)
+#if CONFIG_IS_ENABLED(FSL_FASTBOOT) && CONFIG_IS_ENABLED(SYS_STDIO_DEREGISTER)
 	struct stdio_dev *dev;
 	dev = stdio_get_by_name("fastboot");
 	if (dev)
