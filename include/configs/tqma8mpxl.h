tq-systems/u-boot-tqmaxx

commit 2a1eb307db6d0f2c299e92e75a1d1bf01879469b

tqma8mpxl: remove earlycon
Signed-off-by: Martin Schmiedel <Martin.Schmiedel@tq-group.com>

include/configs/tqma8mpxl.h
@@ -213,12 +213,12 @@
 		"fi; \0"                                                       \
 	"rootfsmode=ro\0"                                                      \
 	"addtty=setenv bootargs ${bootargs} console=${console},${baudrate}\0"  \
-	"mmcargs=run addtty addearlycon addmmc\0"                              \
-	"mmcrootpart=2\0"                                                      \
+	"mmcargs=run addtty addmmc\0"                              \
+	"mmcrootpart=1\0"                                                      \
 	"addmmc=setenv bootargs ${bootargs} "                                  \
 		"root=/dev/mmcblk${mmcblkdev}p${mmcrootpart} ${rootfsmode} "   \
 		"rootwait\0"                                                   \
-	"netargs=run addnfs addip addtty addearlycon\0"                        \
+	"netargs=run addnfs addip addtty\0"                        \
 	"addnfs=setenv bootargs ${bootargs} "                                  \
 		"root=/dev/nfs rw "                                            \
 		"nfsroot=${serverip}:${rootpath},v3,tcp;\0"                    \

include/configs/tqma8mpxl-mba8mp-ras314.h
@@ -27,9 +27,6 @@
 
 #define BB_ENV_SETTINGS \
 	"console=" CONSOLE_DEV "\0" \
-	"addearlycon=setenv bootargs ${bootargs} " \
-		"earlycon=ec_imx6q," __stringify(CONFIG_MXC_UART_BASE) \
-		",${baudrate}\0" \
 	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0"
 
 #endif /* __TQMA8MPXL_MBA8MPRAS314_H */

include/configs/tqma8mpxl-mba8mpxl.h
@@ -27,9 +27,6 @@
 
 #define BB_ENV_SETTINGS \
 	"console=" CONSOLE_DEV "\0" \
-	"addearlycon=setenv bootargs ${bootargs} " \
-		"earlycon=ec_imx6q," __stringify(CONFIG_MXC_UART_BASE) \
-		",${baudrate}\0" \
 	"fdt_file=" CONFIG_DEFAULT_FDT_FILE "\0"
 
 #endif /* __TQMA8MPXL_MBA8MPXL_H */

commit 81e390a605a4e69123dc7a9e3c58e886d461809e

boards: tqma8mpxl: fix wrong assignment in default env
The mmcrootpart env is assigned with a wrong default. This slipped in
by accident with the initial support for MBa8MP-RAS314.

Fixes: commit 2a1eb307db6d ("tqma8mpxl: remove earlycon")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

include/configs/tqma8mpxl.h
@@ -214,7 +214,7 @@
 	"rootfsmode=ro\0"                                                      \
 	"addtty=setenv bootargs ${bootargs} console=${console},${baudrate}\0"  \
 	"mmcargs=run addtty addmmc\0"                              \
-	"mmcrootpart=1\0"                                                      \
+	"mmcrootpart=2\0"                                                      \
 	"addmmc=setenv bootargs ${bootargs} "                                  \
 		"root=/dev/mmcblk${mmcblkdev}p${mmcrootpart} ${rootfsmode} "   \
 		"rootwait\0"                                                   \