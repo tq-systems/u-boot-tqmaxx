tq-systems/u-boot-tqmaxx

commit 1f2288251e079c438be17b5169e10839a421f9f8

include: configs: tqmlx2160a: update to auto pbl
With the new auto pbl file no distinction is needed between SD-Card and
eMMC boot. So remove pbl_sd and use pbl_mmc for both bootsources.

Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

include/configs/tqmlx2160a.h
@@ -273,8 +273,7 @@ unsigned long get_board_ddr_clk(void);
 	"uboot_max_size=" __stringify(MAX_UBOOT_SIZE) "\0"                     \
 	"pbl_spi_offset=0x0\0"						       \
 	"pbl_spi=bl2_flexspi_nor.pbl\0"					       \
-	"pbl_mmc=bl2_emmc.pbl\0"					       \
-	"pbl_sd=bl2_sd.pbl\0"					       \
+	"pbl_sdmmc=bl2_auto.pbl\0"					       \
 	"update_uboot_spi=run set_getcmd; if ${get_cmd} ${uboot}; then "       \
 		"if itest ${filesize} > 0; then "                              \
 			"echo Write u-boot image to sf address ${uboot_spi_offset};"\
@@ -301,7 +300,7 @@ unsigned long get_board_ddr_clk(void);
 			"sf update ${fileaddr} Kernel ${filesize};"\
 		"fi; fi; "                                                     \
 		"setenv filesize;\0"					       \
-	"update_pbl_mmc=run set_getcmd; if ${get_cmd} ${pbl_mmc}; then "       \
+	"update_pbl_mmc=run set_getcmd; if ${get_cmd} ${pbl_sdmmc}; then "       \
 		"if itest ${filesize} > 0; then "                              \
 			"mmc dev ${mmcdev_emmc}; mmc rescan; "	               \
 			"setexpr blkc ${filesize} + 0x1ff; "                   \
@@ -331,7 +330,7 @@ unsigned long get_board_ddr_clk(void);
 			"save mmc ${mmcdev_emmc}:${bootpart} ${fileaddr} ${kernel} ${filesize};"\
 		"fi; fi; "                                                     \
 		"setenv filesize;\0"					       \
-	"update_pbl_sd=run set_getcmd; if ${get_cmd} ${pbl_sd}; then "         \
+	"update_pbl_sd=run set_getcmd; if ${get_cmd} ${pbl_mmc}; then "        \
 		"if itest ${filesize} > 0; then "                              \
 			"mmc dev ${mmcdev_sdhc}; mmc rescan; "	               \
 			"setexpr blkc ${filesize} + 0x1ff; "                   \

commit 6bf677a71abb6092be1a9ca2a290470f426c54ae

include: configs: tqmlx2160a.h: fix update_pbl_sd command
With commit 1f2288251e07 ("include: configs: tqmlx2160a: update to auto
pbl") update scripts where modified to use common pbl file "bl2_auto.bpl".
The variable in the update_pbl_sd was set wrong. Fix it by setting it to
pbl_sdmmc.

Fixes: 1f2288251e07 ("include: configs: tqmlx2160a: update to auto pbl")
Signed-off-by: Gregor Herburger <gregor.herburger@ew.tq-group.com>

include/configs/tqmlx2160a.h
@@ -298,7 +298,7 @@ unsigned long get_board_ddr_clk(void);
 			"fi; "						       \
 		"fi; fi; "                                                     \
 		"setenv filesize;\0"					       \
-	"update_pbl_sd=run set_getcmd; if ${get_cmd} ${pbl_mmc}; then "        \
+	"update_pbl_sd=run set_getcmd; if ${get_cmd} ${pbl_sdmmc}; then "        \
 		"if itest ${filesize} > 0; then "                              \
 			"mmc dev ${mmcdev_sdhc}; mmc rescan; "	               \
 			"setexpr blkc ${filesize} + 0x1ff; "                   \