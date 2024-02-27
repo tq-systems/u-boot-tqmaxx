fbffeb402928cb03fa51791c7a84b8fdd93f0536
Maximus <maximus.sun@nxp.com>
2024-02-27 15:19:00 +0800

    MA-22125 Move the OEM unlocking flag to fbmisc partition
    Due to the vendor services has no premission to access "presistdata"
    partition, move the OEM unlocking flag to the last byte of "fbmisc"
    partition.
    This patch will read/write the last byte in "fbmisc" partition to
    get/set the OEM unlocking state.
    
    Change-Id: Ie6876339ca1851efbe9f49c46c2e7501d9047930
    Signed-off-by: Maximus <maximus.sun@nxp.com>

--- drivers/fastboot/fb_fsl/fastboot_lock_unlock.c
+++ drivers/fastboot/fb_fsl/fastboot_lock_unlock.c
@@ -418,8 +418,8 @@ fail:
 #endif
 
 
-/* Return the last byte of of FSL_FASTBOOT_PR_DATA
- * which is managed by PresistDataService
+/* Return the last byte of of FASTBOOT_PARTITION_FBMISC
+ * which is managed by OemLockService
  */
 
 #ifdef CFG_ENABLE_LOCKSTATUS_SUPPORT
@@ -441,7 +441,7 @@ void set_fastboot_lock_disable(void) {
 		return;
 	set_lock_disable_data(bdata);
 	int status;
-	mmc_id = fastboot_flash_find_index(FASTBOOT_PARTITION_PRDATA);
+	mmc_id = fastboot_flash_find_index(FASTBOOT_PARTITION_FBMISC);
 	if (mmc_id < 0) {
 		printf("%s: error in get mmc part\n", __FUNCTION__);
 		goto fail;
@@ -493,7 +493,7 @@ FbLockEnableResult fastboot_lock_enable() {
 	if (bdata == NULL)
 		return FASTBOOT_UL_ERROR;
 	int status;
-	mmc_id = fastboot_flash_find_index(FASTBOOT_PARTITION_PRDATA);
+	mmc_id = fastboot_flash_find_index(FASTBOOT_PARTITION_FBMISC);
 	if (mmc_id < 0) {
 		printf("%s: error in get mmc part\n", __FUNCTION__);
 		ret = FASTBOOT_UL_ERROR;
