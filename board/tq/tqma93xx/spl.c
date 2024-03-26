tq-systems/u-boot-tqmaxx

commit c44565528398d3cbe87c049ec597040983a18642

board/tq: add initial TQMa93xxLA support
Basic board support with MBa93xxCA

- SD/e-MMC
- FlexSPI
- I2C
- Network
- defconfig for board
- including original VDD_SOC (buck1) pmic standby voltage
  configuration.
- defaults for MTDPARTS_DEFAULT and MTDIDS_DEFAULT (needed for UBI)

Initial defconfig with standard set of commands and features.
Ported from our v2022.04 based fork of u-boot-imx and use new
shared environment file.

Adjustments in spl to align with NXP EVK code to make it compile.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma93xx/spl.c
@@ -1,0 +1,141 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <common.h>
+#include <cpu_func.h>
+#include <hang.h>
+#include <image.h>
+#include <init.h>
+#include <log.h>
+#include <spl.h>
+#include <asm/global_data.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/ddr.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch/trdc.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/mach-imx/ele_api.h>
+#include <dm/device.h>
+#include <power/pmic.h>
+#include <power/pca9450.h>
+
+#include "../common/tq_bb.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+extern struct dram_timing_info tqma93xxla_dram_timing;
+
+int spl_board_boot_device(enum boot_device boot_dev_spl)
+{
+	return BOOT_DEVICE_BOOTROM;
+}
+
+void spl_board_init(void)
+{
+	int ret;
+
+	if (is_usb_boot())
+		puts("USB Boot\n");
+	else
+		puts("Normal Boot\n");
+
+	ret = ahab_start_rng();
+	if (ret)
+		printf("Fail to start RNG: %d\n", ret);
+}
+
+void spl_dram_init(void)
+{
+	ddr_init(&tqma93xxla_dram_timing);
+}
+
+static int power_init_board(void)
+{
+#if CONFIG_IS_ENABLED(DM_PMIC_PCA9450)
+	struct udevice *dev;
+	int ret;
+
+	ret = pmic_get("pmic@25", &dev);
+	if (ret == -ENODEV) {
+		puts("ERROR: pca9450@25 not found\n");
+		return 0;
+	}
+	if (ret != 0) {
+		pr_err("ERROR: request pca9450@25 %d\n", ret);
+		return ret;
+	}
+
+	/* BUCKxOUT_DVS0/1 control BUCK123 output */
+	pmic_reg_write(dev, PCA9450_BUCK123_DVS, 0x29);
+
+/* TODO: LOW_DRIVE_MODE / OVERDRIVE + PWRCTRL_TOFF_DEB -> imx93-evk */
+
+	/* enable DVS control through PMIC_STBY_REQ */
+	pmic_reg_write(dev, PCA9450_BUCK1CTRL, 0x59);
+	/* 0.9 V */
+	pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, 0x18);
+	pmic_reg_write(dev, PCA9450_BUCK3OUT_DVS0, 0x18);
+	/* set standby voltage to 0.65v */
+	pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS1, 0x4);
+
+	/* I2C_LT_EN*/
+	pmic_reg_write(dev, 0xa, 0x3);
+
+	/* set WDOG_B_CFG to cold reset */
+	pmic_reg_write(dev, PCA9450_RESET_CTRL, 0xA1);
+	return 0;
+#endif
+}
+
+extern int imx9_probe_mu(void *ctx, struct event *event);
+
+void board_init_f(ulong dummy)
+{
+	int ret;
+
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	timer_init();
+
+	arch_cpu_init();
+
+	board_early_init_f();
+
+	spl_early_init();
+
+	preloader_console_init();
+
+	ret = imx9_probe_mu(NULL, NULL);
+	if (ret) {
+		printf("ERROR: init ELE API %d\n", ret);
+	} else {
+		printf("SOC rev:   0x%x\n", gd->arch.soc_rev);
+		printf("lifecycle: 0x%x\n", gd->arch.lifecycle);
+	}
+
+	power_init_board();
+
+	if (!IS_ENABLED(CONFIG_IMX9_LOW_DRIVE_MODE))
+		set_arm_core_max_clk();
+
+	/* Init power of mix */
+	soc_power_init();
+
+	/* Setup TRDC for DDR access */
+	trdc_init();
+
+	/* DDR initialization */
+	spl_dram_init();
+
+	/* Put M33 into CPUWAIT for following kick */
+	ret = m33_prepare();
+	if (ret)
+		printf("ERROR: M33 prepare %d\n", ret);
+
+	board_init_r(NULL, 0);
+}

arch/arm/dts/imx93-tqma9352-mba93xxca-u-boot.dtsi
@@ -1,0 +1,82 @@
+// SPDX-License-Identifier: (GPL-2.0-or-later OR MIT)
+/*
+ * Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+/*
+ * include the generic, SOM specific part of U-Boot modifications
+ * for device tree
+ */
+#include "imx93-tqma9352-u-boot.dtsi"
+
+/ {
+	aliases {
+		usbgadget0 = &usbg1;
+	};
+
+	config {
+		u-boot,mmc-env-offset = <0x400000>;
+		u-boot,mmc-env-offset-redundant = <0x410000>;
+	};
+
+	usbg1: usbg1 {
+		compatible = "fsl,imx27-usb-gadget";
+		dr_mode = "peripheral";
+		chipidea,usb = <&usbotg1>;
+		status = "okay";
+	};
+};
+
+&lpi2c3 {
+	u-boot,dm-spl;
+};
+
+&lpi2c5 {
+	u-boot,dm-spl;
+};
+
+&lpuart1 {
+	u-boot,dm-spl;
+};
+
+&pinctrl_lpi2c3 {
+	u-boot,dm-spl;
+};
+
+&pinctrl_lpi2c5 {
+	u-boot,dm-spl;
+};
+
+&pinctrl_uart1 {
+	u-boot,dm-spl;
+};
+
+&pinctrl_usdhc2_gpio {
+	u-boot,dm-spl;
+};
+
+&pinctrl_usdhc2_hs {
+	u-boot,dm-spl;
+};
+
+&pinctrl_usdhc2_uhs {
+	u-boot,dm-spl;
+};
+
+&pinctrl_reg_usdhc2_vmmc {
+	u-boot,dm-spl;
+};
+
+&reg_usdhc2_vmmc {
+	u-boot,off-on-delay-us = <20000>;
+	u-boot,dm-spl;
+};
+
+&usdhc2 {
+	u-boot,dm-spl;
+	fsl,signal-voltage-switch-extra-delay-ms = <8>;
+	sd-uhs-sdr104;
+	sd-uhs-ddr50;
+};

arch/arm/dts/imx93-tqma9352-u-boot.dtsi
@@ -1,0 +1,131 @@
+// SPDX-License-Identifier: (GPL-2.0-or-later OR MIT)
+/*
+ * Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+/ {
+	aliases {
+		i2c_eeprom0 = &eeprom0;
+	};
+
+	wdt-reboot {
+		compatible = "wdt-reboot";
+		wdt = <&wdog3>;
+		u-boot,dm-spl;
+	};
+
+	firmware {
+		optee {
+			compatible = "linaro,optee-tz";
+			method = "smc";
+		};
+	};
+};
+
+&aips1 {
+	u-boot,dm-spl;
+	u-boot,dm-pre-reloc;
+};
+
+&aips2 {
+	u-boot,dm-spl;
+};
+
+&aips3 {
+	u-boot,dm-spl;
+};
+
+&clk {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+	/delete-property/ assigned-clocks;
+	/delete-property/ assigned-clock-rates;
+	/delete-property/ assigned-clock-parents;
+};
+
+&clk_ext1 {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+};
+
+&eeprom0 {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+	/delete-property/ read-only;
+	/delete-property/ vcc-supply;
+};
+
+&gpio1 {
+	u-boot,dm-spl;
+};
+
+&gpio2 {
+	u-boot,dm-spl;
+};
+
+&gpio3 {
+	u-boot,dm-spl;
+};
+
+&gpio4 {
+	u-boot,dm-spl;
+};
+
+&iomuxc {
+	u-boot,dm-spl;
+};
+
+&lpi2c1 {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+};
+
+&osc_24m {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+};
+
+&osc_32k {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+};
+
+&pca9451a {
+	u-boot,dm-spl;
+};
+
+&pca9451a_regulators {
+	u-boot,dm-spl;
+};
+
+&pinctrl_flexspi1 {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+};
+
+&pinctrl_lpi2c1 {
+	u-boot,dm-spl;
+};
+
+&s4muap {
+	u-boot,dm-spl;
+};
+
+&{/soc@0} {
+	u-boot,dm-pre-reloc;
+	u-boot,dm-spl;
+};
+
+&usdhc1 {
+	u-boot,dm-spl;
+	mmc-hs400-1_8v;
+	mmc-hs400-enhanced-strobe;
+	/*
+	 * prevents voltage switch warn: driver will switch even at
+	 * fixed voltage
+	 */
+	/delete-property/ vmmc-supply;
+	/delete-property/ vqmmc-supply;
+};

arch/arm/mach-imx/imx9/Kconfig
@@ -41,10 +41,20 @@ config TARGET_IMX93_9X9_QSB
 	select IMX93
 	select IMX9_LPDDR4X
 
+config TARGET_TQMA93XX
+	bool "TQMa93xx family of SOM"
+	select IMX93
+	select IMX9_LPDDR4X
+	help
+	  This enables support for TQ-Systems GmbH family of i.MX93
+	  based SOM supporting different i.MX93 variants and different
+	  SOM form factors
+
 endchoice
 
 source "board/freescale/imx93_evk/Kconfig"
 source "board/freescale/imx93_qsb/Kconfig"
+source "board/tq/tqma93xx/Kconfig"
 
 endif
 

board/tq/tqma93xx/Kconfig
@@ -1,0 +1,49 @@
+if TARGET_TQMA93XX
+
+config SYS_BOARD
+	default "tqma93xx"
+
+config SYS_VENDOR
+	default "tq"
+
+config SYS_CONFIG_NAME
+	default "tqma93xx"
+
+config TQMA93XX_DEFAULTS
+	bool
+	select IMX93
+	select IMX9_LPDDR4X
+	select TQ_BB
+	select TQ_EEPROM
+	select TQ_RTC
+	select TQ_SDMMC
+	imply I2C_EEPROM
+
+choice
+	prompt "TQMa93xx mainboard variant"
+	default TQMA93XX_BB_MBA93XXCA
+	help
+	  Select mainboard variant for the SOM. Default is the
+	  starterkit.
+
+config TQMA93XX_BB_MBA93XXCA
+	bool "MBa93xxCA starterkit"
+	select DM_GPIO
+	select ENV_IS_NOWHERE
+	select TQMA93XX_DEFAULTS
+	help
+	  Select the MBa93xxCA starterkit. The evaluation board can
+	  demonstrate most of the features of the SOM and CPU and can be
+	  used for LGA and connector SOM variants
+
+endchoice
+
+config MTDPARTS_DEFAULT
+	default "mtdparts=425e0000.spi:4m@0k(bootstream),64k@4096k(env0),64k@4160k(env1),-@6m(ubi)"
+
+config MTDIDS_DEFAULT
+	default "nor0=425e0000.spi"
+
+source "board/tq/common/Kconfig"
+
+endif

board/tq/tqma93xx/Makefile
@@ -1,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+# D-82229 Seefeld, Germany.
+# Author: Markus Niebel
+#
+
+obj-y += tqma93xx.o
+
+obj-$(CONFIG_TQMA93XX_BB_MBA93XXCA) += tqma93xx-mba93xxca.o
+
+ifdef CONFIG_SPL_BUILD
+obj-y += spl.o
+obj-y += lpddr4x_tqma93xxla_timing.o
+endif

board/tq/tqma93xx/tqma93xx-mba93xxca.c
@@ -1,0 +1,196 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <common.h>
+#include <asm/io.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch-imx9/ccm_regs.h>
+#include <asm/arch-imx9/imx93_pins.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm-generic/gpio.h>
+
+#include "../common/tq_bb.h"
+#include "../common/tq_board_gpio.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define MBA93XXCA_BOARD_NAME "MBa93xxCA"
+
+#define UART_PAD_CTRL	(PAD_CTL_DSE(6) | PAD_CTL_FSEL2)
+
+static const iomux_v3_cfg_t uart_pads[] = {
+	MX93_PAD_UART1_RXD__LPUART1_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX93_PAD_UART1_TXD__LPUART1_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+/*
+ * NOTE: this is also used by SPL
+ */
+int tq_bb_board_early_init_f(void)
+{
+	init_uart_clk(LPUART1_CLK_ROOT);
+	imx_iomux_v3_setup_multiple_pads(uart_pads, ARRAY_SIZE(uart_pads));
+
+	return 0;
+}
+
+#if !defined(CONFIG_SPL_BUILD)
+
+enum {
+#if !CONFIG_IS_ENABLED(GPIO_HOG)
+	MPCIE_WAKE_B,
+	MPCIE_PERST_B,
+	MPCIE_WDISABLE_B,
+	WLAN_W_DISABLE_B,
+	WLAN_PERST_B,
+	WLAN_PD_B,
+#endif
+	/* expander @70 */
+	FAN_PWR_EN,
+	MPCIE_1V5_EN,
+	MPCIE_3V3_EN,
+	BUTTON_A_B,
+	BUTTON_B_B,
+	/* expander @71 */
+#if !CONFIG_IS_ENABLED(NETDEVICES)
+	ENET1_RESET_B,
+	ENET2_RESET_B,
+#endif
+	USB_RESET_B,
+	V12V_EN,
+	/* expander @72 */
+	LCD_RESET_B,
+	LCD_PWR_EN,
+	LCD_BLT_EN,
+	DP_EN,
+	MIPI_CSI_EN,
+	MIPI_CSI_RST_B,
+	USER_LED1,
+	USER_LED2,
+};
+
+static struct tq_gpio_init_data mba93xxca_gid[] = {
+#if !CONFIG_IS_ENABLED(GPIO_HOG)
+	GPIO_INIT_DATA_ENTRY(MPCIE_WAKE_B, "gpio@70_1", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(MPCIE_PERST_B, "gpio@70_4",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(MPCIE_WDISABLE_B, "gpio@70_5",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(WLAN_PD_B, "gpio@71_4",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(WLAN_W_DISABLE_B, "gpio@71_5",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(WLAN_PERST_B, "gpio@71_6",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+#endif
+	/* expander 0 */
+	GPIO_INIT_DATA_ENTRY(FAN_PWR_EN, "gpio@70_0", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(MPCIE_1V5_EN, "gpio@70_2", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(MPCIE_3V3_EN, "gpio@70_3", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(BUTTON_A_B, "gpio@70_6", GPIOD_IS_IN),
+	GPIO_INIT_DATA_ENTRY(BUTTON_B_B, "gpio@70_7", GPIOD_IS_IN),
+	/* expander 1 */
+#if !CONFIG_IS_ENABLED(NETDEVICES)
+	GPIO_INIT_DATA_ENTRY(ENET1_RESET_B, "gpio@71_0",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(ENET2_RESET_B, "gpio@71_1",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+#endif
+	GPIO_INIT_DATA_ENTRY(USB_RESET_B, "gpio@71_2",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(V12V_EN, "gpio@71_7", GPIOD_IS_OUT),
+	/* expander 2 */
+	GPIO_INIT_DATA_ENTRY(LCD_RESET_B, "gpio@72_0",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(LCD_PWR_EN, "gpio@72_1", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(LCD_BLT_EN, "gpio@72_2", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(DP_EN, "gpio@72_3", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(MIPI_CSI_EN, "gpio@72_4", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(MIPI_CSI_RST_B, "gpio@72_5",
+			     GPIOD_IS_OUT | GPIOD_ACTIVE_LOW |
+			     GPIOD_IS_OUT_ACTIVE),
+	GPIO_INIT_DATA_ENTRY(USER_LED1, "gpio@72_6", GPIOD_IS_OUT),
+	GPIO_INIT_DATA_ENTRY(USER_LED2, "gpio@72_7", GPIOD_IS_OUT),
+};
+
+const char *tq_bb_get_boardname(void)
+{
+	return MBA93XXCA_BOARD_NAME;
+}
+
+int tq_bb_checkboard(void)
+{
+	return 0;
+}
+
+/*
+ * SD0 -> mmc0 / mmcblk0
+ * SD1 -> mmc1 / mmcblk1
+ */
+int board_mmc_get_env_dev(int devno)
+{
+	return devno;
+}
+
+/*
+ * we use dt alias based indexing, so kernel uses same index. See above
+ */
+int mmc_map_to_kernel_blk(int devno)
+{
+	return devno;
+}
+
+static int setup_fec(void)
+{
+	return set_clk_enet(ENET_125MHZ);
+}
+
+static int setup_eqos(void)
+{
+	struct blk_ctrl_wakeupmix_regs *bctrl =
+		(struct blk_ctrl_wakeupmix_regs *)BLK_CTRL_WAKEUPMIX_BASE_ADDR;
+
+	/* set INTF as RGMII, enable RGMII TXC clock */
+	clrsetbits_le32(&bctrl->eqos_gpr,
+			BCTRL_GPR_ENET_QOS_INTF_MODE_MASK,
+			BCTRL_GPR_ENET_QOS_INTF_SEL_RGMII | BCTRL_GPR_ENET_QOS_CLK_GEN_EN);
+
+	return set_clk_eqos(ENET_125MHZ);
+}
+
+int tq_bb_board_init(void)
+{
+	tq_board_gpio_init(mba93xxca_gid, ARRAY_SIZE(mba93xxca_gid));
+
+	if (CONFIG_IS_ENABLED(FEC_MXC))
+		setup_fec();
+
+	if (CONFIG_IS_ENABLED(DWC_ETH_QOS))
+		setup_eqos();
+
+	return 0;
+}
+
+int tq_bb_board_late_init(void)
+{
+	if (CONFIG_IS_ENABLED(ENV_IS_IN_MMC))
+		board_late_mmc_env_init();
+
+	return 0;
+}
+
+#endif

commit 3d050ce543fd18278f905346353f85439faa4270

board: tqma93xx: fix return path of power_init_board
If compiling without DM_PMIC_PCA9450 the return statement is missing.

Fixes: c4456552839 ("board/tq: add initial TQMa93xxLA support")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma93xx/spl.c
@@ -259,8 +259,8 @@ static int power_init_board(void)
 
 	/* set WDOG_B_CFG to cold reset */
 	pmic_reg_write(dev, PCA9450_RESET_CTRL, 0xA1);
-	return 0;
 #endif
+	return 0;
 }
 
 extern int imx9_probe_mu(void *ctx, struct event *event);