beagleboard/u-boot

commit 8712298323f049c73a4a3b5ba017865d75bb678d

board: ti: am6*x: evm: Fix up incorrect RAM size for AM62A and AM62P
The fixup_memory_node() does no change in AM64, AM62A and AM62P when
ECC is not enabled. Instead, it causes an issue of fixing up the RAM
size as 2GB instead of 4GB and 8GB for AM62A and AM62P because the
fix up is done by the R5 SPL and R5 being a 32-bit processor, the
gd->bd->bi_dram[bank].start and gd->bd->bi_dram[bank].size values are
restricted to 32-bits. So, remove the fixup_memory_node() from
spl_perform_fixups() in AM64, AM62A and AM62P's evm files.

Fixes: 410888e38c7e ("board: ti: Pull redundant DDR functions to a common location and Fixup DDR size when ECC is enabled")
Signed-off-by: Santhosh Kumar K <s-k6@ti.com>
Reviewed-by: Neha Malcom Francis <n-francis@ti.com>

board/ti/am62ax/evm.c
@@ -35,8 +35,6 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 {
 	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
 		fixup_ddr_driver_for_ecc(spl_image);
-	else
-		fixup_memory_node(spl_image);
 }
 #endif
 

board/ti/am62px/evm.c
@@ -55,8 +55,6 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 {
 	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
 		fixup_ddr_driver_for_ecc(spl_image);
-	else
-		fixup_memory_node(spl_image);
 }
 
 static int video_setup(void)

board/ti/am64x/evm.c
@@ -105,8 +105,6 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 {
 	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
 		fixup_ddr_driver_for_ecc(spl_image);
-	else
-		fixup_memory_node(spl_image);
 
 #if CONFIG_IS_ENABLED(USB_STORAGE)
 	fixup_usb_boot(spl_image->fdt_addr);

tq-systems/u-boot-tqmaxx

commit 26c81a4d5b31b0ca6a57bcd30d115f458cb98192

board: ti: am62*x: Fix up the correct RAM size if ECC is enabled
Call a k3-ddrss fixup function to fixup device tree and resize the
available amount of DDR if ECC is enabled.

A second fixup is required from A53 SPL to fixup device tree passed to
A53 U-Boot.

Fixes: 8712298323f0 ("board: ti: am6*x: Fix up incorrect RAM size for AM62A and AM62P")
Signed-off-by: Santhosh Kumar K <s-k6@ti.com>

board/ti/am62ax/evm.c
@@ -33,8 +33,12 @@ int board_init(void)
 #if defined(CONFIG_SPL_BUILD)
 void spl_perform_fixups(struct spl_image_info *spl_image)
 {
-	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
-		fixup_ddr_driver_for_ecc(spl_image);
+	if (IS_ENABLED(CONFIG_TARGET_AM62A7_R5_EVM)) {
+		if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
+			fixup_ddr_driver_for_ecc(spl_image);
+	} else {
+		fixup_memory_node(spl_image);
+	}
 }
 #endif
 

board/ti/am62px/evm.c
@@ -54,8 +54,12 @@ int board_init(void)
 #if defined(CONFIG_SPL_BUILD)
 void spl_perform_fixups(struct spl_image_info *spl_image)
 {
-	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
-		fixup_ddr_driver_for_ecc(spl_image);
+	if (IS_ENABLED(CONFIG_TARGET_AM62P5_R5_EVM)) {
+		if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
+			fixup_ddr_driver_for_ecc(spl_image);
+	} else {
+		fixup_memory_node(spl_image);
+	}
 }
 
 static int video_setup(void)

board/ti/am64x/evm.c
@@ -105,6 +105,8 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 {
 	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
 		fixup_ddr_driver_for_ecc(spl_image);
+	else
+		fixup_memory_node(spl_image);
 
 #if CONFIG_IS_ENABLED(USB_STORAGE)
 	fixup_usb_boot(spl_image->fdt_addr);