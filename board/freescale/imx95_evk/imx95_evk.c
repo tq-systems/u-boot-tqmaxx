a1ca9063db66265989be3dcd53514b4083901861
Ye Li <ye.li@nxp.com>
2024-05-08 15:21:28 +0800

    LFU-738-1 imx95_evk: Soft reset flexspi flash before exit u-boot
    We have enabled DDR octal mode for flexspi NOR flash on iMX95 EVK.
    It needs to call soft reset to reset flash to single bit mode before
    u-boot jumping to kernel, otherwise kernel fails to read its device
    ID hence can't recognize the flash.
    So trying to remove the all SPI FLASH devices which will finally call
    to soft reset.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx95_evk/imx95_evk.c
+++ board/freescale/imx95_evk/imx95_evk.c
@@ -19,6 +19,8 @@
 #include "../dts/upstream/src/arm64/freescale/imx95-power.h"
 #include <asm/arch/sys_proto.h>
 #include <i2c.h>
+#include <dm/uclass.h>
+#include <dm/uclass-internal.h>
 
 int board_early_init_f(void)
 {
@@ -409,6 +411,7 @@ int board_phys_sdram_size(phys_size_t *size)
 void board_quiesce_devices(void)
 {
 	int ret;
+	struct uclass *uc_dev;
 
 	ret = imx9_scmi_power_domain_enable(IMX95_PD_HSIO_TOP, false);
 	if (ret) {
@@ -421,6 +424,12 @@ void board_quiesce_devices(void)
 		printf("%s: Failed for NETC MIX: %d\n", __func__, ret);
 		return;
 	}
+
+	ret = uclass_get(UCLASS_SPI_FLASH, &uc_dev);
+	if (uc_dev)
+		ret = uclass_destroy(uc_dev);
+	if (ret)
+		printf("couldn't remove SPI FLASH devices\n");
 }
 
 #if IS_ENABLED(CONFIG_OF_BOARD_FIXUP)
