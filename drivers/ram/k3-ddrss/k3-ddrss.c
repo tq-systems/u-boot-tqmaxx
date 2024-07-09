tq-systems/u-boot-tqmaxx

commit 1084f05e1ce6fb5ae8090e4dbb0ae620af928495

k3-ddrss: add option to use board code to determine RAM size
With commit de74380cb2a8
("ram: k3-ddrss: Add k3_ddrss_ddr_bank_base_size_calc() to solve 'calculations restricted to 32 bits' issue")
k3-ddrss reads bank information directly from the FDT to handle 64bit
addresses in 32bit U-Boot builds. This breaks when bank sizes are updated
dynamically in board code. Add an option to use bank sizes provided by the
board instead.

Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

drivers/ram/k3-ddrss/k3-ddrss.c
@@ -15,6 +15,7 @@
 #include <fdt_support.h>
 #include <ram.h>
 #include <hang.h>
+#include <init.h>
 #include <log.h>
 #include <asm/io.h>
 #include <power-domain.h>
@@ -906,38 +907,48 @@ static void k3_ddrss_ddr_bank_base_size_calc(struct k3_ddrss_desc *ddrss)
 	int bank, na, ns, len, parent;
 	const fdt32_t *ptr, *end;
 
-	for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {
-		ddrss->ddr_bank_base[bank] = 0;
-		ddrss->ddr_bank_size[bank] = 0;
-	}
+	if (IS_ENABLED(CONFIG_K3_DDRSS_BOARD_DRAM_INIT)) {
+		dram_init_banksize();
 
-	ofnode mem = ofnode_null();
-
-	do {
-		mem = ofnode_by_prop_value(mem, "device_type", "memory", 7);
-	} while (!ofnode_is_enabled(mem));
-
-	const void *fdt = ofnode_to_fdt(mem);
-	int node = ofnode_to_offset(mem);
-	const char *property = "reg";
-
-	parent = fdt_parent_offset(fdt, node);
-	na = fdt_address_cells(fdt, parent);
-	ns = fdt_size_cells(fdt, parent);
-	ptr = fdt_getprop(fdt, node, property, &len);
-	end = ptr + len / sizeof(*ptr);
-
-	for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {
-		if (ptr + na + ns <= end) {
-			if (CONFIG_IS_ENABLED(OF_TRANSLATE))
-				ddrss->ddr_bank_base[bank] = fdt_translate_address(fdt, node, ptr);
-			else
-				ddrss->ddr_bank_base[bank] = fdtdec_get_number(ptr, na);
-
-			ddrss->ddr_bank_size[bank] = fdtdec_get_number(&ptr[na], ns);
+		for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {
+			ddrss->ddr_bank_base[bank] = gd->bd->bi_dram[bank].start;
+			ddrss->ddr_bank_size[bank] = gd->bd->bi_dram[bank].size;
+		}
+	} else {
+		for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {
+			ddrss->ddr_bank_base[bank] = 0;
+			ddrss->ddr_bank_size[bank] = 0;
 		}
 
-		ptr += na + ns;
+		ofnode mem = ofnode_null();
+
+		do {
+			mem = ofnode_by_prop_value(mem, "device_type", "memory", 7);
+		} while (!ofnode_is_enabled(mem));
+
+		const void *fdt = ofnode_to_fdt(mem);
+		int node = ofnode_to_offset(mem);
+		const char *property = "reg";
+
+		parent = fdt_parent_offset(fdt, node);
+		na = fdt_address_cells(fdt, parent);
+		ns = fdt_size_cells(fdt, parent);
+		ptr = fdt_getprop(fdt, node, property, &len);
+		end = ptr + len / sizeof(*ptr);
+
+		for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {
+			if (ptr + na + ns <= end) {
+				if (CONFIG_IS_ENABLED(OF_TRANSLATE))
+					ddrss->ddr_bank_base[bank] =
+						fdt_translate_address(fdt, node, ptr);
+				else
+					ddrss->ddr_bank_base[bank] = fdtdec_get_number(ptr, na);
+
+				ddrss->ddr_bank_size[bank] = fdtdec_get_number(&ptr[na], ns);
+			}
+
+			ptr += na + ns;
+		}
 	}
 
 	for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++)
@@ -946,7 +957,10 @@ static void k3_ddrss_ddr_bank_base_size_calc(struct k3_ddrss_desc *ddrss)
 
 static void k3_ddrss_lpddr4_ecc_calc_reserved_mem(struct k3_ddrss_desc *ddrss)
 {
-	fdtdec_setup_mem_size_base_lowest();
+	if (IS_ENABLED(CONFIG_K3_DDRSS_BOARD_DRAM_INIT))
+		dram_init();
+	else
+		fdtdec_setup_mem_size_base_lowest();
 
 	ddrss->ecc_reserved_space = ddrss->ddr_ram_size;
 	do_div(ddrss->ecc_reserved_space, 9);

drivers/ram/Kconfig
@@ -99,6 +99,14 @@ config K3_AM62A_DDRSS
 
 endchoice
 
+config K3_DDRSS_BOARD_DRAM_INIT
+	bool "Initialize RAM banksizes using dram_init() and dram_init_banksize()"
+	depends on K3_DDRSS && PHYS_64BIT
+	help
+	  Instead of reading RAM sizes from the Device Tree, use the board's
+	  provided dram_init() and dram_init_banksize(), allowing to override
+	  the settings dynamically.
+
 config K3_DDRSS_VARIANTS
 	bool "Enable K3 DDRSS RAM configuration variant support"
 	depends on K3_DDRSS && SPL_SYSINFO

commit 80fc76d747ad21fe37da7ead6a04efa40ba4e3be

ram: k3-ddrss: zero-initialize bi_dram before dram_init_banksize()
Many implementations of dram_init_banksize() do not explicitly
initialize unused RAM banks. For the most part this isn't an issue, as
later U-Boot stages will have zero-initialized the whole of gd earlier,
but the DDR initialization happens too early for that. Avoid using
uninitialized values by initializing the whole struct before calling
dram_init_banksize().

Fixes: 1084f05e1ce6 ("k3-ddrss: add option to use board code to determine RAM size")
Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

drivers/ram/k3-ddrss/k3-ddrss.c
@@ -908,6 +908,7 @@ static void k3_ddrss_ddr_bank_base_size_calc(struct k3_ddrss_desc *ddrss)
 	const fdt32_t *ptr, *end;
 
 	if (IS_ENABLED(CONFIG_K3_DDRSS_BOARD_DRAM_INIT)) {
+		memset(gd->bd->bi_dram, 0, sizeof(gd->bd->bi_dram));
 		dram_init_banksize();
 
 		for (bank = 0; bank < CONFIG_NR_DRAM_BANKS; bank++) {