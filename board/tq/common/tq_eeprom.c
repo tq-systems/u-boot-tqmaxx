tq-systems/u-boot-tqmaxx

commit 42ac94015bf42d4223ab432ec48561eb209d1100

tq/common: forward port tq_eeprom and VARD support
This adds support for module identification and VARD support.
The implementation is backward compatible. Boards without
formfactor or before adding formfactor to VARD will give "DEFAULT".

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>
Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

board/tq/common/tq_eeprom.c
@@ -1,0 +1,313 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2014-2024 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <dm/uclass.h>
+#include <i2c_eeprom.h>
+#include <i2c.h>
+#include <linux/ctype.h>
+#include <malloc.h>
+#include <net.h>
+#include <u-boot/crc.h>
+
+#include "tq_eeprom.h"
+#include "tq_som_features.h"
+
+/**
+ * Reads buffer from EEPROM given by seq nr starting at offset
+ */
+int tq_read_eeprom_buffer(int seq, uint offset, int buf_size, u_int8_t *buf)
+{
+	struct udevice *dev;
+	int ret;
+
+	ret = uclass_get_device_by_seq(UCLASS_I2C_EEPROM, seq, &dev);
+	if (ret) {
+		debug("%s: Cannot find i2c_eeprom%d\n", __func__, seq);
+		return ret;
+	}
+
+	return i2c_eeprom_read(dev, offset, (u8 *)buf, buf_size);
+};
+
+#if IS_ENABLED(CONFIG_TQ_VARD)
+
+/*
+ * checksum is calculated over whole structure but the CRC field
+ */
+static uint16_t tq_vard_chksum(const struct tq_vard *vard)
+{
+	const unsigned char *start = (const unsigned char *)(vard) +
+		sizeof(vard->crc);
+
+	return crc16_ccitt(0, start, sizeof(*vard) - sizeof(vard->crc));
+}
+
+bool tq_vard_valid(const struct tq_vard *vard)
+{
+	return (vard->crc == tq_vard_chksum(vard));
+}
+
+phys_size_t tq_vard_memsize(u8 val, unsigned int multiply, unsigned int tmask)
+{
+	phys_size_t result = 0;
+
+	if (val != VARD_MEMSIZE_DEFAULT) {
+		result = 1 << (size_t)(val & VARD_MEMSIZE_MASK_EXP);
+		if (val & tmask)
+			result *= 3;
+		result *= multiply;
+	}
+
+	return result;
+}
+
+void tq_vard_show(const struct tq_vard *vard)
+{
+	printf("CRC\t%04x (calculated %04x) [%s]\n",
+	       (unsigned int)vard->crc, (unsigned int)tq_vard_chksum(vard),
+	       tq_vard_valid(vard) ? "OKAY" : "FAIL");
+	/* display data anyway to support developer */
+	printf("HW\tREV.%02uxx\n", (unsigned int)vard->hwrev);
+	printf("RAM\ttype %u, %lu MiB, %s\n",
+	       (unsigned int)(vard->memtype & VARD_MEMTYPE_MASK_TYPE),
+	       (unsigned long)(tq_vard_ramsize(vard) / (SZ_1M)),
+	       (tq_vard_has_ramecc(vard) ? "ECC" : "no ECC"));
+	printf("RTC\t%c\nSPINOR\t%c\ne-MMC\t%c\nSE\t%c\nEEPROM\t%c\n",
+	       (tq_vard_has_rtc(vard) ? 'y' : 'n'),
+	       (tq_vard_has_spinor(vard) ? 'y' : 'n'),
+	       (tq_vard_has_emmc(vard) ? 'y' : 'n'),
+	       (tq_vard_has_secelem(vard) ? 'y' : 'n'),
+	       (tq_vard_has_eeprom(vard) ? 'y' : 'n'));
+	if (tq_vard_has_eeprom(vard))
+		printf("EEPROM\ttype %u, %lu KiB, page %lu\n",
+		       (unsigned int)(vard->eepromtype & VARD_EETYPE_MASK_MFR) >> 4,
+		       (unsigned long)(tq_vard_eepromsize(vard) / (SZ_1K)),
+		       tq_vard_eeprom_pgsize(vard));
+	puts("FORMFACTOR: ");
+	switch (tq_vard_get_formfactor(vard)) {
+	case VARD_FORMFACTOR_TYPE_LGA:
+		puts("LGA\n");
+		break;
+	case VARD_FORMFACTOR_TYPE_CONNECTOR:
+		puts("CONNECTOR\n");
+		break;
+	case VARD_FORMFACTOR_TYPE_SMARC2:
+		puts("SMARC-2\n");
+		break;
+	case VARD_FORMFACTOR_TYPE_NONE:
+		/*
+		 * applies to boards with no variants or older boards
+		 * where this field is not written
+		 */
+		puts("UNSPECIFIED\n");
+		break;
+	default:
+		/*
+		 * generic fall trough
+		 * unhandled form factor or invalid data
+		 */
+		puts("UNKNOWN\n");
+		break;
+	}
+}
+
+int tq_vard_detect_features(const struct tq_vard *vard,
+			    struct tq_som_feature_list *features)
+{
+	size_t i;
+
+	if (!vard || !features) {
+		pr_err("%s: invalid parameter\n", __func__);
+		return -EINVAL;
+	}
+
+	if (!tq_vard_valid(vard)) {
+		pr_err("%s: VARD invalid\n", __func__);
+		return -ENODATA;
+	}
+
+	for (i = 0; i < features->entries; ++i) {
+		switch (features->list[i].feature) {
+		case FEATURE_EEPROM:
+			features->list[i].present = tq_vard_has_eeprom(vard);
+			break;
+		case FEATURE_EMMC:
+			features->list[i].present = tq_vard_has_emmc(vard);
+			break;
+		case FEATURE_IMU:
+			features->list[i].present = tq_vard_has_imu(vard);
+			break;
+		case FEATURE_RTC:
+			features->list[i].present = tq_vard_has_rtc(vard);
+			break;
+		case FEATURE_SECELEM:
+			features->list[i].present = tq_vard_has_secelem(vard);
+			break;
+		case FEATURE_SPINOR:
+			features->list[i].present = tq_vard_has_spinor(vard);
+			break;
+		default:
+			pr_warn("%s: unknown feature %d\n", __func__,
+				features->list[i].feature);
+		}
+	}
+
+	return 0;
+}
+
+#endif
+
+#if !IS_ENABLED(CONFIG_SPL_BUILD)
+
+/**
+ * Reads struct tq_eeprom_data from EEPROM given by seq nr
+ * starting at offset in EEPROM array.
+ */
+int tq_read_eeprom_at(int seq, uint offset, struct tq_eeprom_data *eeprom)
+{
+	return tq_read_eeprom_buffer(seq, offset, sizeof(*eeprom),
+				     (u_int8_t *)eeprom);
+}
+
+int tq_parse_eeprom_mac(struct tq_eeprom_data * const eeprom, char *buf,
+			size_t len)
+{
+	int ret;
+	u8 *p;
+
+	if (!buf || !eeprom)
+		return -1;
+	/* MAC address */
+	p = eeprom->mac;
+	ret = snprintf(buf, len, "%02x:%02x:%02x:%02x:%02x:%02x",
+		       p[0], p[1], p[2], p[3], p[4], p[5]);
+	if (ret < 0)
+		return ret;
+	if (ret >= len)
+		return ret;
+
+	return !(is_valid_ethaddr(p));
+}
+
+static int tq_parse_eeprom_serial_new(struct tq_eeprom_data * const eeprom,
+				      char *buf)
+{
+	unsigned int i;
+
+	for (i = 0; i < (sizeof(eeprom->serial)) &&
+	     (isdigit(eeprom->serial[i]) || isupper(eeprom->serial[i])); i++)
+		buf[i] = eeprom->serial[i];
+	buf[i] = '\0';
+	if (sizeof(eeprom->serial) != strlen(buf))
+		return -EINVAL;
+
+	return 0;
+}
+
+static int tq_parse_eeprom_serial_old(struct tq_eeprom_data * const eeprom,
+				      char *buf)
+{
+	unsigned int i;
+
+	for (i = 0; i < (sizeof(eeprom->serial)) && isdigit(eeprom->serial[i]);
+	     i++)
+		buf[i] = eeprom->serial[i];
+	buf[i] = '\0';
+	if (sizeof(eeprom->serial) != strlen(buf))
+		return -EINVAL;
+
+	return 0;
+}
+
+int tq_parse_eeprom_serial(struct tq_eeprom_data * const eeprom,
+			   char *buf, size_t len)
+{
+	if (!buf || !eeprom)
+		return -EINVAL;
+	if (len < (sizeof(eeprom->serial) + 1))
+		return -EINVAL;
+
+	if (eeprom->serial[0] == 'T' && eeprom->serial[1] == 'Q')
+		return tq_parse_eeprom_serial_new(eeprom, buf);
+	else
+		return tq_parse_eeprom_serial_old(eeprom, buf);
+
+	return -EINVAL;
+}
+
+int tq_parse_eeprom_id(struct tq_eeprom_data * const eeprom, char *buf,
+		       size_t len)
+{
+	unsigned int i;
+
+	if (!buf || !eeprom)
+		return -1;
+	if (len < (sizeof(eeprom->id) + 1))
+		return -1;
+
+	for (i = 0; i < sizeof(eeprom->id) && isprint(eeprom->id[i]) &&
+	     isascii(eeprom->id[i]); ++i)
+		buf[i] = eeprom->id[i];
+	buf[i] = '\0';
+
+	return 0;
+}
+
+int tq_show_eeprom(struct tq_eeprom_data * const eeprom, const char *id)
+{
+	/* must hold largest field of eeprom data */
+	char safe_string[(TQ_EE_BDID_BYTES) + 1];
+
+	if (!eeprom)
+		return -1;
+
+	puts(id);
+	puts(" EEPROM:\n");
+	/* ID */
+	tq_parse_eeprom_id(eeprom, safe_string, ARRAY_SIZE(safe_string));
+	if (strncmp(safe_string, id, strlen(id)) == 0)
+		printf("  ID: %s\n", safe_string);
+	else
+		puts("  unknown hardware variant\n");
+
+	/* Serial number */
+	if (tq_parse_eeprom_serial(eeprom, safe_string,
+				   ARRAY_SIZE(safe_string)) == 0)
+		printf("  SN: %s\n", safe_string);
+	else
+		puts("  unknown serial number\n");
+	/* MAC address */
+	if (tq_parse_eeprom_mac(eeprom, safe_string,
+				ARRAY_SIZE(safe_string)) == 0)
+		printf("  MAC: %s\n", safe_string);
+	else
+		puts("  invalid MAC\n");
+
+	return 0;
+}
+
+int tq_board_handle_eeprom_data(const char *board_name,
+				struct tq_eeprom_data * const eeprom)
+{
+	char sstring[(TQ_EE_BDID_BYTES) + 1];
+
+	tq_parse_eeprom_id(eeprom, sstring, ARRAY_SIZE(sstring));
+
+	if (IS_ENABLED(CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG)) {
+		if (strncmp(sstring, board_name, strlen(board_name)) == 0)
+			env_set("boardtype", sstring);
+		if (tq_parse_eeprom_serial(eeprom, sstring,
+					   ARRAY_SIZE(sstring)) == 0)
+			env_set("serial#", sstring);
+		else
+			env_set("serial#", "???");
+	}
+
+	return tq_show_eeprom(eeprom, board_name);
+}
+
+#endif

board/tq/common/Kconfig
@@ -2,6 +2,11 @@ config TQ_BB
 	bool
 	default y
 
+config TQ_EEPROM
+	bool
+	select DM_I2C
+	select I2C_EEPROM
+
 #
 # This is only needed for boards with hardware revisions with/without serial
 # termination in eMMC signal lines. The code allows to decide upon the eMMC type
@@ -31,3 +36,9 @@ config TQ_SCU
 
 config TQ_SDMMC
 	bool
+
+config TQ_VARD
+	bool
+	select HASH
+	select SPL_HASH if SPL
+	select TQ_EEPROM

board/tq/common/Makefile
@@ -18,9 +18,11 @@ ifdef MINIMAL
 obj- := __dummy__.o
 else
 obj-$(CONFIG_TQ_BB) += tq_bb.o
+obj-$(CONFIG_TQ_EEPROM) += tq_eeprom.o
 obj-$(CONFIG_TQ_EMMC) += tq_emmc.o
 obj-$(CONFIG_TQ_ENVLOC) += tq_envloc.o
 obj-$(CONFIG_TQ_RTC) += tq_rtc.o
 obj-$(CONFIG_TQ_SCU) += tq_scu.o
 obj-$(CONFIG_TQ_SDMMC) += tq_sdmmc.o
+obj-$(CONFIG_TQ_VARD) += tq_som_features.o
 endif

board/tq/common/tq_eeprom.h
@@ -1,0 +1,313 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2014-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#ifndef __TQ_EEPROM_H__
+#define __TQ_EEPROM_H__
+
+#include <linux/sizes.h>
+
+struct tq_eeprom_data;
+
+#if defined(CONFIG_I2C_EEPROM)
+
+/**
+ * Reads buffer from EEPROM given by seq nr starting at offset
+ */
+int tq_read_eeprom_buffer(int seq, uint offset, int buf_size, u_int8_t *buf);
+
+/**
+ * Reads struct tq_eeprom_data from EEPROM given by seq nr
+ * starting at offset in EEPROM array.
+ */
+int tq_read_eeprom_at(int seq, uint offset, struct tq_eeprom_data *eeprom);
+
+/**
+ * Reads struct tq_eeprom_data from EEPROM given by seq nr
+ * starting at offset zero in EEPROM array.
+ */
+static inline int tq_read_eeprom(int seq, struct tq_eeprom_data *eeprom)
+{
+	return tq_read_eeprom_at(seq, 0, eeprom);
+}
+
+/**
+ * Reads struct tq_eeprom_data from EEPROM with seq nr zero
+ * starting at offset zero in EEPROM array.
+ */
+static inline int tq_read_module_eeprom(struct tq_eeprom_data *eeprom)
+{
+	return tq_read_eeprom(0, eeprom);
+}
+#else
+
+#error "need CONFIG_I2C_EEPROM"
+
+#endif
+
+#if defined(CONFIG_TQ_VARD)
+
+#define VARD_FEATURE_BYTES	8
+
+/*
+ * VARD - variant and revision detection
+ * must have an exact size of 32 bytes to fit in EEPROM just before
+ * the module data
+ */
+struct __packed tq_vard {
+	u16 crc;		/* checksum of vard data - CRC16 XMODEM */
+	u8 hwrev;		/* hardware major revision */
+	u8 memsize;		/* RAM size */
+	u8 memtype;		/* RAM Type + ECC */
+	u8 features[VARD_FEATURE_BYTES];	/* feature bitmask */
+	u8 eepromsize;		/* user eeprom size (feature EEPROM) */
+	u8 eepromtype;		/* user eeprom type (feature EEPROM) */
+	u8 formfactor;		/* SOM Form factor. mask 0xf0 */
+	u8 rsv[0x10];		/* for future use */
+};
+
+#define VARD_MEMTYPE_MASK_TYPE		0x7f /* board specific RAM Type */
+#define VARD_MEMTYPE_MASK_ECC		0x80 /* extra ECC RAM assembled */
+#define VARD_MEMTYPE_DEFAULT		0xff /* use board specific default */
+
+#define VARD_MEMSIZE_MASK_EXP		0x1f /* 2^n MBytes */
+#define VARD_MEMSIZE_MASK_FACTOR	0x20 /* x3 */
+#define VARD_MEMSIZE_DEFAULT		0xff /* use board specific default */
+
+/* feature is present if bit is zero */
+#define VARD_FEATURE_0_RESERVED		0xf0 /* Do not use */
+#define VARD_FEATURE_0_EMMC		0x08 /* e-MMC assembled */
+#define VARD_FEATURE_0_EEPROM		0x04 /* user EEPROM assembled */
+#define VARD_FEATURE_0_SPINOR		0x02 /* [Q,O]SPI-NOR assembled */
+#define VARD_FEATURE_0_SECELEM		0x01 /* secure element assembled */
+
+#define VARD_FEATURE_4_RESERVED		0xf0 /* Do not use */
+#define VARD_FEATURE_4_RTC		0x08 /* RTC assembled */
+
+#define VARD_EESIZE_MASK_EXP		0x1f /* 2^n Bytes */
+#define VARD_EETYPE_DEFAULT		0xff /* use board specific default */
+#define VARD_EETYPE_MASK_MFR		0xf0 /* manufacturer / type mask */
+#define VARD_EETYPE_MASK_PGSIZE		0x0f /* page size */
+
+#define VARD_FORMFACTOR_MASK_TYPE	0xf0 /* SOM type mask */
+#define VARD_FORMFACTOR_TYPE_CONNECTOR	0x00 /* SOM with connector, no board standard */
+#define VARD_FORMFACTOR_TYPE_LGA	0x10 /* LGA SOM, no board standard */
+#define VARD_FORMFACTOR_TYPE_SMARC2	0x20 /* SOM conforms to SMARC-2 standard */
+#define VARD_FORMFACTOR_TYPE_NONE	0xf0 /* unspecified SOM type */
+/*
+ * read module configuration data from eeprom
+ * bus: I2C bus number
+ * bus: I2C address of EEPROM
+ * This function is intended for SPL usage without DT support
+ * All supported modules have an JC42 type EEPROM with one byte offset
+ * addressing
+ */
+static inline
+int tq_vard_read(struct tq_vard *vard)
+{
+	return tq_read_eeprom_buffer(0, 0, sizeof(*vard), (u_int8_t *)vard);
+}
+
+/*
+ * check CRC - true if CRC is valid
+ */
+bool tq_vard_valid(const struct tq_vard *vard);
+
+/*
+ * all data should only be handled as valid, if CRC is OKAY
+ */
+static inline
+bool tq_vard_has_ramecc(const struct tq_vard *vard)
+{
+	return (vard->memtype & VARD_MEMTYPE_MASK_ECC);
+}
+
+/*
+ * Calculate size in byte using byte from vard
+ * This works as long as coding for EEPROM / RAM size is the same
+ * val - memsize byte from tq_vard structure
+ * multiply - multiplier, aka 1 / SZ_1K / SZ_1M
+ * tmask - mask for triple factor (use only for RAM sizes)
+ *
+ * return size in bytes or zero in case the val is equal to VARD_MEMSIZE_DEFAULT
+ */
+phys_size_t tq_vard_memsize(u8 val, unsigned int multiply, unsigned int tmask);
+
+static inline
+phys_size_t tq_vard_ramsize(const struct tq_vard *vard)
+{
+	return tq_vard_memsize(vard->memsize, SZ_1M, VARD_MEMSIZE_MASK_FACTOR);
+}
+
+static inline
+size_t tq_vard_eepromsize(const struct tq_vard *vard)
+{
+	return tq_vard_memsize(vard->eepromsize, 1, 0x0);
+}
+
+static inline
+size_t tq_vard_eeprom_pgsize(const struct tq_vard *vard)
+{
+	return 1 << (size_t)(vard->eepromtype & VARD_EETYPE_MASK_PGSIZE);
+}
+
+static inline
+int tq_vard_has_feature(const struct tq_vard *vard, unsigned int fbyte,
+			unsigned int fbit)
+{
+	if (fbyte < VARD_FEATURE_BYTES && fbit < 8)
+		return !(vard->features[fbyte] & BIT(fbit));
+	else
+		return -ERANGE;
+}
+
+static inline
+bool tq_vard_has_emmc(const struct tq_vard *vard)
+{
+	return (tq_vard_has_feature(vard, 0, 3) > 0);
+}
+
+static inline
+bool tq_vard_has_eeprom(const struct tq_vard *vard)
+{
+	return (tq_vard_has_feature(vard, 0, 2) > 0);
+}
+
+static inline
+bool tq_vard_has_spinor(const struct tq_vard *vard)
+{
+	return (tq_vard_has_feature(vard, 0, 1) > 0);
+}
+
+static inline
+bool tq_vard_has_secelem(const struct tq_vard *vard)
+{
+	return (tq_vard_has_feature(vard, 0, 0) > 0);
+}
+
+static inline
+bool tq_vard_has_rtc(const struct tq_vard *vard)
+{
+	return (tq_vard_has_feature(vard, 4, 3) > 0);
+}
+
+static inline
+bool tq_vard_has_imu(const struct tq_vard *vard)
+{
+	return (tq_vard_has_feature(vard, 4, 2) > 0);
+}
+
+static inline u32 tq_vard_get_formfactor(const struct tq_vard *vard)
+{
+	return (u32)(vard->formfactor & VARD_FORMFACTOR_MASK_TYPE);
+};
+
+static inline
+bool tq_vard_is_lga(const struct tq_vard *vard)
+{
+	return (tq_vard_get_formfactor(vard) == VARD_FORMFACTOR_TYPE_LGA);
+}
+
+static inline
+bool tq_vard_is_connector(const struct tq_vard *vard)
+{
+	return (tq_vard_get_formfactor(vard) == VARD_FORMFACTOR_TYPE_CONNECTOR);
+}
+
+static inline
+bool tq_vard_is_smarc2(const struct tq_vard *vard)
+{
+	return (tq_vard_get_formfactor(vard) == VARD_FORMFACTOR_TYPE_SMARC2);
+}
+
+void tq_vard_show(const struct tq_vard *vard);
+
+struct tq_som_feature_list;
+
+/**
+ * fill in presence information from VARD.
+ *
+ * @param[in] vard pointer to VARD structure from SOM EEPROM
+ * @param[in] features SOM specific feature list
+ *
+ * @return 0 on success
+ *
+ * Must be called after data was read to vard. The function checks
+ * if vard is valid, goes through the list and sets the present flag
+ * for each entry depending on the flags in vard.
+ */
+int tq_vard_detect_features(const struct tq_vard *vard,
+			    struct tq_som_feature_list *features);
+
+#endif
+
+#if !defined(CONFIG_SPL_BUILD)
+
+/*
+ * static EEPROM layout
+ */
+#define TQ_EE_HRCW_BYTES	0x20
+#define TQ_EE_MAC_BYTES		6
+#define TQ_EE_RSV1_BYTES	10
+#define TQ_EE_SERIAL_BYTES	8
+#define TQ_EE_RSV2_BYTES	8
+#define TQ_EE_BDID_BYTES	0x40
+
+struct __packed tq_eeprom_data {
+	union {
+#if IS_ENABLED(CONFIG_TQ_VARD)
+		struct tq_vard vard;
+		_Static_assert(sizeof(struct tq_vard) == TQ_EE_HRCW_BYTES, \
+			"struct tq_vard has incorrect size");
+#endif
+		u8 hrcw_primary[TQ_EE_HRCW_BYTES];
+	} tq_hw_data;
+	u8 mac[TQ_EE_MAC_BYTES];		/* 0x20 ... 0x25 */
+	u8 rsv1[TQ_EE_RSV1_BYTES];
+	u8 serial[TQ_EE_SERIAL_BYTES];		/* 0x30 ... 0x37 */
+	u8 rsv2[TQ_EE_RSV2_BYTES];
+	u8 id[TQ_EE_BDID_BYTES];		/* 0x40 ... 0x7f */
+};
+
+/**
+ * Parse MAC address from module EEPROM to buf, respect buf len
+ */
+int tq_parse_eeprom_mac(struct tq_eeprom_data * const eeprom, char *buf,
+			size_t len);
+
+/**
+ * Parse BCD coded serial number from module EEPROM to buf,
+ * respect buf len
+ */
+int tq_parse_eeprom_serial(struct tq_eeprom_data * const eeprom, char *buf,
+			   size_t len);
+
+/**
+ * Parse module name string number from module EEPROM to buf,
+ * respect buf len
+ */
+int tq_parse_eeprom_id(struct tq_eeprom_data * const eeprom, char *buf,
+		       size_t len);
+
+/**
+ * display the contents of the module EEPROM
+ * (MAC, serial number, module name/variant string)
+ * report error, if module name doesn't match id string or malformed
+ * MAC / serial number
+ */
+int tq_show_eeprom(struct tq_eeprom_data *const eeprom, const char *id);
+
+/**
+ * verify and display contents of the module EEPROM
+ * optionally set 'serial' and 'board_type env' if
+ * CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG in enabled
+ */
+int tq_board_handle_eeprom_data(const char *board_name,
+				struct tq_eeprom_data * const eeprom);
+
+#endif /* CONFIG_SPL_BUILD */
+
+#endif

commit 137fe9cf1802636c280b2e08490bf4ba6024c196

tq/common: tq_eeprom: add missing IMU to tq_vard_show
When adding IMU support to VARD and feature handling output in
function tq_vard_show was not added.

While at it, sort features in output lexically.

Fixes: 42ac94015bf4 ("tq/common: forward port tq_eeprom and VARD support")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/common/tq_eeprom.c
@@ -76,12 +76,13 @@ void tq_vard_show(const struct tq_vard *vard)
 	       (unsigned int)(vard->memtype & VARD_MEMTYPE_MASK_TYPE),
 	       (unsigned long)(tq_vard_ramsize(vard) / (SZ_1M)),
 	       (tq_vard_has_ramecc(vard) ? "ECC" : "no ECC"));
-	printf("RTC\t%c\nSPINOR\t%c\ne-MMC\t%c\nSE\t%c\nEEPROM\t%c\n",
-	       (tq_vard_has_rtc(vard) ? 'y' : 'n'),
-	       (tq_vard_has_spinor(vard) ? 'y' : 'n'),
+	printf("EEPROM\t%c\neMMC\t%c\nIMU\t%c\nRTC\t%c\nSE\t%c\nSPINOR\t%c\n",
+	       (tq_vard_has_eeprom(vard) ? 'y' : 'n'),
 	       (tq_vard_has_emmc(vard) ? 'y' : 'n'),
+	       (tq_vard_has_imu(vard) ? 'y' : 'n'),
+	       (tq_vard_has_rtc(vard) ? 'y' : 'n'),
 	       (tq_vard_has_secelem(vard) ? 'y' : 'n'),
-	       (tq_vard_has_eeprom(vard) ? 'y' : 'n'));
+	       (tq_vard_has_spinor(vard) ? 'y' : 'n'));
 	if (tq_vard_has_eeprom(vard))
 		printf("EEPROM\ttype %u, %lu KiB, page %lu\n",
 		       (unsigned int)(vard->eepromtype & VARD_EETYPE_MASK_MFR) >> 4,