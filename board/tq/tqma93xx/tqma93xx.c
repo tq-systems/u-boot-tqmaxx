tq-systems/u-boot-tqmaxx

commit fcfa4005e5ff35bb26c58210e7963f9a00d8a655

board/tq: add initial TQMa93xx[LA,CA] support
TQMa93xxLA is a solderable SoM while TQMa93xxCA is a click on SoM.
Both SoM share pin usage and RAM configuration. Both SoM need a
base boards. Base board support is added for MBa93xxCA starter kit
and MBa93xxLA SBC:

- SD/e-MMC
- FlexSPI
- I2C
- Network
- VDD_SOC (buck1) pmic standby voltage configuration.
- defaults for MTDPARTS_DEFAULT and MTDIDS_DEFAULT (needed for UBI)
- USB
- VARD and feature detection

The two base boards have minimal differences and use same code for
board support and configuration header at the cost of some
preprocessor guards.

MBa93xxLA SBC has following additional features compared to the
starter kit mainboard:

- soldered IOT / GSM modem
- analog frontend

DDR config output of NXP DDR Config tool made checkpatch clean:

- spaces after / before { }
- use tabs for indentation
- remove superfluous empty lines
- move SPDX tag to first line

Add new folder for TQ-Systems GmbH boards under 'doc/boards'
and add it to the documentation generation system. Add basic
documentation for the SoM.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>
Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>
Signed-off-by: Stein Alexander <alexander.stein@tq-group.com>

board/tq/tqma93xx/tqma93xx.c
@@ -1,0 +1,265 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2022-2024 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <env.h>
+#include <init.h>
+#include <mtd_node.h>
+#include <spl.h>
+#include <asm/global_data.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/arch-imx9/ccm_regs.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <jffs2/load_kernel.h>
+
+#include "../common/tq_bb.h"
+#include "../common/tq_eeprom.h"
+#include "../common/tq_som_features.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_early_init_f(void)
+{
+	tq_bb_board_early_init_f();
+
+	return 0;
+}
+
+#if !defined(CONFIG_SPL_BUILD)
+
+/**
+ * Global copy of EEPROM data to prevent multiple reads.
+ *
+ * Data is read during board_late_init
+ */
+static struct tq_eeprom_data eeprom;
+
+/**
+ * Translate detected CPU variant into TQ-Systems SOM variant short name
+ *
+ * return: string consisting of the name or string indicating unknown variant
+ */
+static const char *tq_get_boardname(void)
+{
+	switch (get_cpu_type()) {
+	case MXC_CPU_IMX93:
+		return "TQMa9352"; /* iMX93 Dual core with NPU */
+	case MXC_CPU_IMX9351:
+		return "TQMa9351"; /* iMX93 Single core with NPU */
+	case MXC_CPU_IMX9332:
+		return "TQMa9332"; /* iMX93 Dual core without NPU */
+	case MXC_CPU_IMX9331:
+		return "TQMa9331"; /* iMX93 Single core without NPU */
+	/* IMX9322 / IMX9321 / IMX9312 / IMX9311 not assembled, 9x9mm package */
+	case MXC_CPU_IMX9302:
+		return "TQMa9302"; /* iMX93 Dual core / reduced frequency without NPU */
+	case MXC_CPU_IMX9301:
+		return "TQMa9301"; /* iMX93 Single core / reduced frequency without NPU */
+	}
+
+	return "UNKNOWN";
+}
+
+int print_bootinfo(void)
+{
+	enum boot_device bt_dev;
+
+	bt_dev = get_boot_device();
+
+	puts("Boot:  ");
+	switch (bt_dev) {
+	case SD1_BOOT:
+		puts("USDHC1(SD)\n");
+		break;
+	case SD2_BOOT:
+		puts("USDHC2(SD)\n");
+		break;
+	case SD3_BOOT:
+		puts("USDHC3(SD)\n");
+		break;
+	case MMC1_BOOT:
+		puts("USDHC1(e-MMC)\n");
+		break;
+	case MMC2_BOOT:
+		puts("USDHC2(e-MMC)\n");
+		break;
+	case MMC3_BOOT:
+		puts("USDHC3(e-MMC)\n");
+		break;
+	case USB_BOOT:
+		puts("USB\n");
+		break;
+	case QSPI_BOOT:
+		puts("FlexSPI\n");
+		break;
+	default:
+		printf("Unknown/Unsupported device %u\n", bt_dev);
+		break;
+	}
+
+	return 0;
+}
+
+/**
+ * print some useful board information
+ *
+ * When not calling late, VARD is not read yet. Trying to read EEPROM here
+ * will succeed but for unknown reason calling imx9_probe_mu from event callback
+ * EVT_DM_POST_INIT will lead to system failure, since DT data seems corrupt
+ * when EVT_DM_POST_INIT will be issued from initr_dm main device model init.
+ * TODO: check if using DISPLAY_BOARDINFO_LATE
+ */
+int checkboard(void)
+{
+	print_bootinfo();
+	printf("Board: %s on a %s\n", tq_get_boardname(),
+	       tq_bb_get_boardname());
+
+	return tq_bb_checkboard();
+}
+
+int board_init(void)
+{
+	tq_bb_board_init();
+
+	return 0;
+}
+
+static struct tq_som_feature tqma93xx_som_features[] = {
+	{
+		.feature = FEATURE_EMMC,
+		.dt_path = "/soc@0/bus@42800000/mmc@42850000",
+	}, {
+		.feature = FEATURE_EEPROM,
+		.dt_path = "/soc@0/bus@44000000/i2c@44340000/eeprom@57",
+	}, {
+		.feature = FEATURE_IMU,
+		.dt_path = "/soc@0/bus@44000000/i2c@44340000/imu@6a",
+	}, {
+		.feature = FEATURE_RTC,
+		.dt_path = "/soc@0/bus@44000000/i2c@44340000/rtc@51",
+	}, {
+		.feature = FEATURE_SECELEM,
+		/* TODO: no driver yet */
+	}, {
+		.feature = FEATURE_SPINOR,
+		.dt_path = "/soc@0/bus@42000000/spi@425e0000/flash@0",
+	},
+};
+
+static struct tq_som_feature_list tqma93xx_feature_list = {
+	.list = tqma93xx_som_features,
+	.entries = ARRAY_SIZE(tqma93xx_som_features),
+};
+
+const struct tq_som_feature_list *tq_board_detect_features(void)
+{
+	return &tqma93xx_feature_list;
+}
+
+#if CONFIG_IS_ENABLED(OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	if (CONFIG_IS_ENABLED(FDT_FIXUP_PARTITIONS)) {
+		const char * const path = "/soc@0/bus@42000000/spi@425e0000";
+		static const struct node_info nodes[] = {
+			{ "jedec,spi-nor",	MTD_DEV_TYPE_NOR, },
+		};
+
+		if (tq_vard_valid(&eeprom.tq_hw_data.vard)) {
+			if (tq_vard_has_spinor(&eeprom.tq_hw_data.vard)) {
+				/*
+				 * Update MTD partition nodes using info
+				 * from mtdparts env var
+				 * for [Q]SPI this needs the device probed.
+				 */
+				puts("   Updating MTD partitions...\n");
+				tq_ft_spi_setup(blob, path, nodes,
+						ARRAY_SIZE(nodes));
+			}
+		}
+	}
+
+	return tq_bb_ft_board_setup(blob, bd);
+}
+#endif
+
+/* see MAX_CMDLINE_SIZE in boot/bootm.c */
+#define BOARD_CMDLINE_SIZE SZ_4K
+static char board_bootargs[BOARD_CMDLINE_SIZE];
+
+char *board_fdt_chosen_bootargs(void)
+{
+	char *bootargs = env_get("bootargs");
+	static const char *cortexm_args = "clk-imx93.mcore_booted=1";
+
+	if (arch_auxiliary_core_check_up(0)) {
+		size_t len = strlen(bootargs);
+
+		board_bootargs[0] = '\0';
+		strlcat(board_bootargs, bootargs, sizeof(board_bootargs));
+		strlcat(board_bootargs, " ", sizeof(board_bootargs));
+		strlcat(board_bootargs, cortexm_args, sizeof(board_bootargs));
+
+		return board_bootargs;
+	}
+
+	return bootargs;
+}
+
+int board_late_init(void)
+{
+	const char *bname = tq_get_boardname();
+	bool features_detected = false;
+	int ret;
+
+	ret = tq_read_module_eeprom(&eeprom);
+
+	if (!ret) {
+		tq_board_handle_eeprom_data(bname, &eeprom);
+		if (tq_vard_valid(&eeprom.tq_hw_data.vard)) {
+			/*
+			 * set quartz load to 7.000 femtofarads
+			 * only if RTC is assembled, to prevent warnings
+			 */
+			if (tq_vard_has_rtc(&eeprom.tq_hw_data.vard)) {
+				/* set quartz load to 7.000 femtofarads */
+				if (tq_pcf85063_adjust_capacity(0, 0x51, 7000))
+					puts("PCF85063A: adjust error\n");
+			}
+			/* fill feature presence flags from vard */
+			if (!tq_vard_detect_features(&eeprom.tq_hw_data.vard,
+						     &tqma93xx_feature_list))
+				features_detected = true;
+		}
+	} else {
+		puts("EEPROM: read error\n");
+	}
+
+	/* mark list as empty to prevent further processing */
+	if (!features_detected) {
+		printf("VARD: data not present or invalid,\n"
+		       "      no fixup in DT for optional devices!\n");
+		tqma93xx_feature_list.entries = 0;
+	}
+
+	if (CONFIG_IS_ENABLED(ENV_VARS_UBOOT_RUNTIME_CONFIG)) {
+		env_set("board_name", tq_bb_get_boardname());
+		env_set("board_rev", bname);
+
+		if (CONFIG_IS_ENABLED(AHAB_BOOT))
+			env_set("sec_boot", "yes");
+		else
+			env_set("sec_boot", "no");
+	}
+
+	tq_bb_board_late_init();
+
+	return 0;
+}
+
+#endif

arch/arm/mach-imx/imx9/Kconfig
@@ -68,6 +68,15 @@ config TARGET_IMX93_9X9_QSB
 	select IMX93
 	select IMX9_LPDDR4X
 
+config TARGET_TQMA93XX
+	bool "TQMa93xx family of SOM"
+	select IMX93
+	select IMX9_LPDDR4X
+	help
+	  This enables support for TQ-Systems GmbH family of i.MX93
+	  based SOM supporting different i.MX93 variants and different
+	  SOM form factors
+
 config TARGET_IMX95_19X19_EVK
 	bool "imx95_19x19_evk"
 	select IMX95
@@ -88,6 +97,7 @@ source "board/freescale/imx93_evk/Kconfig"
 source "board/phytec/phycore_imx93/Kconfig"
 source "board/variscite/imx93_var_som/Kconfig"
 source "board/freescale/imx93_qsb/Kconfig"
+source "board/tq/tqma93xx/Kconfig"
 source "board/freescale/imx95_evk/Kconfig"
 source "board/toradex/verdin-imx95/Kconfig"
 

board/tq/tqma93xx/Kconfig
@@ -1,0 +1,61 @@
+if TARGET_TQMA93XX
+
+config SYS_BOARD
+	default "tqma93xx"
+
+config SYS_VENDOR
+	default "tq"
+
+config SYS_CONFIG_NAME
+	default "tqma93xx"
+
+config TQMA93XX_DEFAULTS
+	bool
+	select IMX9_LPDDR4X
+	select TQ_BB
+	select TQ_EEPROM
+	select TQ_RTC
+	select TQ_SDMMC
+	select TQ_VARD
+	imply MISC
+	imply I2C_EEPROM
+
+choice
+	prompt "TQMa93xx mainboard variant"
+	default TQMA93XX_BB_MBA93XXCA
+	help
+	  Select mainboard variant for the SOM. Default is the
+	  starterkit.
+
+config TQMA93XX_BB_MBA93XXCA
+	bool "MBa93xxCA starterkit"
+	select DM_GPIO
+	select ENV_IS_NOWHERE
+	select TQMA93XX_DEFAULTS
+	select USB_TCPC if USB
+	help
+	  Select the MBa93xxCA starterkit. The evaluation board can
+	  demonstrate most of the features of the SOM and CPU and can be
+	  used for LGA and connector SOM variants
+
+config TQMA93XX_BB_MBA93XXLA
+	bool "MBa93xxLA SBC"
+	select DM_GPIO
+	select ENV_IS_NOWHERE
+	select TQMA93XX_DEFAULTS
+	select USB_TCPC if USB
+	help
+	  Select the MBa93xxLA SBC. This mainboard has the LGA SOM
+	  variant assembled.
+
+endchoice
+
+config MTDPARTS_DEFAULT
+	default "mtdparts=425e0000.spi:4m@0k(bootstream),64k@4096k(env0),64k@4160k(env1),-@6m(ubi)"
+
+config MTDIDS_DEFAULT
+	default "nor0=425e0000.spi"
+
+source "board/tq/common/Kconfig"
+
+endif

board/tq/tqma93xx/MAINTAINERS
@@ -1,0 +1,9 @@
+TQ-Systems GmbH TQMa93xx[CA/LA] SOM / MBa93xx[CA/LA] board
+L:	u-boot@ew.tq-group.com
+W:	https://www.tq-group.com/en/products/tq-embedded/
+S:	Maintained
+F:	arch/arm/dts/imx93*-tqma93*-mba93*.dts*
+F:	board/tq/tqma93xx/
+F:	configs/tqma93*_mba93_*_defconfig
+F:	doc/boards/tq/tqma9*
+F:	include/configs/tqma93*.h

board/tq/tqma93xx/Makefile
@@ -1,0 +1,20 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+# D-82229 Seefeld, Germany.
+# Author: Markus Niebel
+#
+
+obj-y += tqma93xx.o
+
+obj-$(CONFIG_TQMA93XX_BB_MBA93XXCA) += tqma93xx-mba93xx.o
+obj-$(CONFIG_TQMA93XX_BB_MBA93XXLA) += tqma93xx-mba93xx.o
+
+ifdef CONFIG_SPL_BUILD
+obj-y += spl.o
+obj-y += lpddr4x_tqma93xxca_timing_1gb.o
+obj-y += lpddr4x_tqma93xxla_timing_1gb.o
+obj-y += lpddr4x_tqma93xxla_timing_1gb5.o
+obj-y += lpddr4x_tqma93xxca_timing_2gb.o
+obj-y += lpddr4x_tqma93xxla_timing_2gb.o
+endif

commit 736f541528cfb093d294d6cfd03780f3c78e290e

tqma93xx: remove unused variable in board_fdt_chosen_bootargs
When using strlcat the size checking and limiting is not needed.

Fixes: fcfa4005e5ff ("board/tq: add initial TQMa93xx[LA,CA] support")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma93xx/tqma93xx.c
@@ -200,8 +200,6 @@ char *board_fdt_chosen_bootargs(void)
 	static const char *cortexm_args = "clk-imx93.mcore_booted=1";
 
 	if (arch_auxiliary_core_check_up(0)) {
-		size_t len = strlen(bootargs);
-
 		board_bootargs[0] = '\0';
 		strlcat(board_bootargs, bootargs, sizeof(board_bootargs));
 		strlcat(board_bootargs, " ", sizeof(board_bootargs));