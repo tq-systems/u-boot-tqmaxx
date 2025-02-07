a5fc719169e4a83550ec2baa45e91b9b7a5d0cff
Alexander Stein <alexander.stein@ew.tq-group.com>
2025-02-07 11:36:03 +0100

    tqma6ul: adjust fastboot variables
    Basic support for flashing bootloader and system image
    by declaring a raw 'bootloader' partition and an 'all'
    partition on mmc dev 0 (eMMC).
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- include/configs/tqma6ul_mba6ul.h
+++ include/configs/tqma6ul_mba6ul.h
@@ -23,15 +23,17 @@
 
 #if (IS_ENABLED(CONFIG_USB_FUNCTION_FASTBOOT))
 
-#define BB_ENV_SETTINGS                                          \
-	"fastboot_partition_alias_all=0:0\0"                     \
-	"fastboot_partition_alias_boot=0:1\0"                    \
-	"fastboot_partition_alias_rootfs=0:2\0"                  \
-	"fastboot_raw_partition_bootloader="                     \
-		__stringify(TQMA6UL_MMC_UBOOT_SECTOR_START) " "  \
-		__stringify(TQMA6UL_MMC_UBOOT_SECTOR_COUNT) "\0" \
-	"fastbootcmd=fastboot usb 0\0"                           \
-	BOOT_ENV_MBA6UL                                          \
+#define MMC_BOOT_PARTITION 1 /* 0=user 1=boot1 2=boot2 */
+
+#define BB_ENV_SETTINGS                                                \
+	"fastboot_partition_alias_all="                                \
+		__stringify(CONFIG_FASTBOOT_FLASH_MMC_DEV) ":0\0"      \
+	"fastboot_raw_partition_bootloader="                           \
+		__stringify(TQMA6UL_MMC_UBOOT_SECTOR_START) " "        \
+		__stringify(TQMA6UL_MMC_UBOOT_SECTOR_COUNT) " mmcpart "\
+		__stringify(MMC_BOOT_PARTITION)"\0"                    \
+	"fastbootcmd=fastboot usb 0\0"                                 \
+	BOOT_ENV_MBA6UL                                                \
 
 #else
 
