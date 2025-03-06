tq-systems/u-boot-tqmaxx

commit 8e44a62004c55001bd4126ab6d0cf267a52d9cae

mach-imx: spl: add conditions and cleanup for MCU RDC config
with commit 916da6e12e6b ("MA-19048-1 MCU security enhancement")
code was introduced to move hard coded RDC configuration for Cortex-M
in TF-A to U-Boot SPL for i.NX8MP and i.MX8MN.

The implementation depends on data sharing (default in OCRAM_S) and
encoding the settings in board device tree for SPL.

This commit fixes the following:

- allow to use the feature optional with setting the addresses for the
  shared data to zero
- write cookies to shared memory only if parsing from DT was successful

This allows to run with or without said settings.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

arch/arm/mach-imx/spl.c
@@ -534,13 +534,21 @@ int board_spl_fit_post_load(const void *fit, struct spl_image_info *spl_image)
 	}
 #if defined(CONFIG_IMX8MP) || defined(CONFIG_IMX8MN)
 #define MCU_RDC_MAGIC "mcu_rdc"
-	memcpy((void *)CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR, MCU_RDC_MAGIC, ALIGN(strlen(MCU_RDC_MAGIC), 4));
-	memcpy((void *)CONFIG_IMX8M_MCU_RDC_STOP_CONFIG_ADDR, MCU_RDC_MAGIC, ALIGN(strlen(MCU_RDC_MAGIC), 4));
-	board_handle_rdc_config(spl_image->fdt_addr, "start-config",
-				(void *)(CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR + ALIGN(strlen(MCU_RDC_MAGIC), 4)));
-	board_handle_rdc_config(spl_image->fdt_addr, "stop-config",
-				(void *)(CONFIG_IMX8M_MCU_RDC_STOP_CONFIG_ADDR + ALIGN(strlen(MCU_RDC_MAGIC), 4)));
-#endif
+	if (!(spl_image->flags & SPL_FIT_BYPASS_POST_LOAD) &&
+	    CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR != 0ull &&
+	    CONFIG_IMX8M_MCU_RDC_STOP_CONFIG_ADDR != 0ull) {
+		if (!board_handle_rdc_config(spl_image->fdt_addr, "start-config",
+					     (void *)(CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR +
+					     ALIGN(strlen(MCU_RDC_MAGIC), 4))))
+			memcpy((void *)CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR, MCU_RDC_MAGIC,
+			       ALIGN(strlen(MCU_RDC_MAGIC), 4));
+		if (!board_handle_rdc_config(spl_image->fdt_addr, "stop-config",
+					     (void *)(CONFIG_IMX8M_MCU_RDC_STOP_CONFIG_ADDR +
+					     ALIGN(strlen(MCU_RDC_MAGIC), 4))))
+			memcpy((void *)CONFIG_IMX8M_MCU_RDC_STOP_CONFIG_ADDR, MCU_RDC_MAGIC,
+			       ALIGN(strlen(MCU_RDC_MAGIC), 4));
+	}
+#endif /* IMX8MP || IMX8MN */
 
 #ifdef CONFIG_IMX_TRUSTY_OS
         tee_node = fit_image_get_node(fit, TEE_SUBNODE_NAME);

arch/arm/mach-imx/imx8m/Kconfig
@@ -60,10 +60,12 @@ config IMX_UNIQUE_ID
 
 config IMX8M_MCU_RDC_START_CONFIG_ADDR
 	hex "Start address of mcu rdc config when mcu starts"
+	depends on IMX8MN || IMX8MP
 	default 0x186000
 
 config IMX8M_MCU_RDC_STOP_CONFIG_ADDR
 	hex "Start address of mcu rdc config when mcu stops"
+	depends on IMX8MN || IMX8MP
 	default 0x187000
 
 choice

commit e9508d31bba0bc61eefe7a575902ff0add713025

mach-imx: spl: fix MCU RDC config for i.MX8MN / i.MX8MP
SPL_FIT_BYPASS_POST_LOAD was introduced in older vendor branch and
is not defined in this branch. Do not use the define to prevent
compilation error.

Fixes: 8e44a62004c5 ("mach-imx: spl: add conditions and cleanup for MCU RDC config")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

arch/arm/mach-imx/spl.c
@@ -534,8 +534,7 @@ int board_spl_fit_post_load(const void *fit, struct spl_image_info *spl_image)
 	}
 #if defined(CONFIG_IMX8MP) || defined(CONFIG_IMX8MN)
 #define MCU_RDC_MAGIC "mcu_rdc"
-	if (!(spl_image->flags & SPL_FIT_BYPASS_POST_LOAD) &&
-	    CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR != 0ull &&
+	if (CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR != 0ull &&
 	    CONFIG_IMX8M_MCU_RDC_STOP_CONFIG_ADDR != 0ull) {
 		if (!board_handle_rdc_config(spl_image->fdt_addr, "start-config",
 					     (void *)(CONFIG_IMX8M_MCU_RDC_START_CONFIG_ADDR +