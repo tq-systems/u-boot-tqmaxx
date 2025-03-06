tq-systems/u-boot-tqmaxx

commit fcfa4005e5ff35bb26c58210e7963f9a00d8a655

board/tq: add initial TQMa93xx[LA,CA] support
TQMa93xxLA is a solderable SoM while TQMa93xxCA is a click on SoM.
Both SoM share pin usage and RAM configuration. Both SoM need a
base boards. Base board support is added for MBa93xxCA starter kit
and MBa93xxLA SBC:

- SD/e-MMC
- FlexSPI
- I2C
- Network
- VDD_SOC (buck1) pmic standby voltage configuration.
- defaults for MTDPARTS_DEFAULT and MTDIDS_DEFAULT (needed for UBI)
- USB
- VARD and feature detection

The two base boards have minimal differences and use same code for
board support and configuration header at the cost of some
preprocessor guards.

MBa93xxLA SBC has following additional features compared to the
starter kit mainboard:

- soldered IOT / GSM modem
- analog frontend

DDR config output of NXP DDR Config tool made checkpatch clean:

- spaces after / before { }
- use tabs for indentation
- remove superfluous empty lines
- move SPDX tag to first line

Add new folder for TQ-Systems GmbH boards under 'doc/boards'
and add it to the documentation generation system. Add basic
documentation for the SoM.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>
Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>
Signed-off-by: Stein Alexander <alexander.stein@tq-group.com>

include/configs/tqma93xx.h
@@ -1,0 +1,112 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#ifndef __TQMA93XX_H
+#define __TQMA93XX_H
+
+#include <linux/sizes.h>
+#include <linux/stringify.h>
+#include <asm/arch/imx-regs.h>
+
+#define CFG_SYS_UBOOT_BASE	\
+	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
+
+#if defined(CONFIG_FASTBOOT) && !defined(CONFIG_FSL_FASTBOOT)
+
+#if !defined(CONFIG_FASTBOOT_FLASH_MMC_DEV)
+#error "FASTBOOT_FLASH_MMC_DEV must be set to device index of eMMC device"
+#endif
+
+#define CFG_MFG_ENV_SETTINGS                                           \
+	"fastboot_partition_alias_all="	                               \
+		__stringify(CONFIG_FASTBOOT_FLASH_MMC_DEV) ".0:0\0"    \
+	"fastboot_partition_alias_bootloader="                         \
+		__stringify(CONFIG_FASTBOOT_FLASH_MMC_DEV) ".1:0\0"    \
+	"emmc_dev=" __stringify(CONFIG_FASTBOOT_FLASH_MMC_DEV) "\0"    \
+	"sd_dev=1\0"
+
+#else
+
+#define CFG_MFG_ENV_SETTINGS                                           \
+	"emmc_dev=0\0"                                                 \
+	"sd_dev=1\0"
+
+#endif
+
+/* Initial environment variables */
+#define CFG_MODULE_ENV_SETTINGS                                        \
+	"scriptaddr=0x83500000\0"                                      \
+	"image=Image\0"                                                \
+	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"        \
+	"pxefile_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0"       \
+	"fdt_addr_r=0x83000000\0"                                      \
+	"initrd_addr=0x83800000\0"                                     \
+	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0"             \
+	"mmcfwpart=1\0"                                                \
+	"mmcfwpath=/\0"                                                \
+	"mmcautodetect=yes\0"                                          \
+	"loadbootscript=mmc dev ${mmcdev}; mmc rescan;"                \
+		"load mmc ${mmcdev}:${mmcfwpart} ${loadaddr} "         \
+		"${mmcfwpath}${script};\0"                             \
+	"bootscript=echo Running bootscript from mmc ...; "            \
+		"source\0"                                             \
+	"boot_os=booti ${kernel_addr_r} - ${fdt_addr_r};\0"            \
+	"uboot_mmc_start=0x40\0"                                       \
+	"uboot_mmc_size=0xfc0\0"                                       \
+	"uboot_spi_start=0x0\0"                                        \
+	"uboot_spi_size=0x400000\0"                                    \
+	"uboot=bootstream.bin\0"                                       \
+
+#if !defined(CONFIG_BOOTCOMMAND)
+#warn "CONFIG_BOOTCOMMAND missing in configuration"
+#endif
+
+/* Link Definitions */
+#define INIT_RAM_OFFSET			0x0
+
+#define CFG_SYS_INIT_RAM_ADDR	(0x80000000 + INIT_RAM_OFFSET)
+#define CFG_SYS_INIT_RAM_SIZE	0x200000
+
+#define CFG_SYS_SDRAM_BASE	0x80000000
+#define PHYS_SDRAM		0x80000000
+#define PHYS_SDRAM_SIZE		SZ_1G /* 1GB DDR */
+
+/* Using ULP WDOG for reset */
+#define WDOG_BASE_ADDR			WDG3_BASE_ADDR
+
+#ifdef CONFIG_AHAB_BOOT
+#define AHAB_ENV "sec_boot=yes\0"
+#else
+#define AHAB_ENV "sec_boot=no\0"
+#endif
+
+/* both variants use the same settings */
+#if defined(CONFIG_TQMA93XX_BB_MBA93XXCA) || defined(CONFIG_TQMA93XX_BB_MBA93XXLA)
+#include "tqma93xx-mba93xx.h"
+#else
+#error "no mainboard variant selected"
+#endif
+
+#if defined(CONFIG_ETHPRIME)
+#define NETDEV_ENV "netdev=" CONFIG_ETHPRIME "\0"
+#else
+#define NETDEV_ENV "netdev=eth0\0"
+#endif
+
+#include "tq-imx-shared-env.h"
+
+#define CFG_EXTRA_ENV_SETTINGS                                         \
+	CFG_MODULE_ENV_SETTINGS                                        \
+	TQ_IMX_SHARED_ENV_SETTINGS                                     \
+	TQ_IMX_SPI_UBOOT_UPDATE                                        \
+	BB_ENV_SETTINGS                                                \
+	CFG_MFG_ENV_SETTINGS                                           \
+	BOOTENV                                                        \
+	NETDEV_ENV                                                     \
+	AHAB_ENV
+
+#endif /* __TQMA93XX_H */

arch/arm/mach-imx/imx9/Kconfig
@@ -68,6 +68,15 @@ config TARGET_IMX93_9X9_QSB
 	select IMX93
 	select IMX9_LPDDR4X
 
+config TARGET_TQMA93XX
+	bool "TQMa93xx family of SOM"
+	select IMX93
+	select IMX9_LPDDR4X
+	help
+	  This enables support for TQ-Systems GmbH family of i.MX93
+	  based SOM supporting different i.MX93 variants and different
+	  SOM form factors
+
 config TARGET_IMX95_19X19_EVK
 	bool "imx95_19x19_evk"
 	select IMX95
@@ -88,6 +97,7 @@ source "board/freescale/imx93_evk/Kconfig"
 source "board/phytec/phycore_imx93/Kconfig"
 source "board/variscite/imx93_var_som/Kconfig"
 source "board/freescale/imx93_qsb/Kconfig"
+source "board/tq/tqma93xx/Kconfig"
 source "board/freescale/imx95_evk/Kconfig"
 source "board/toradex/verdin-imx95/Kconfig"
 

board/tq/tqma93xx/Kconfig
@@ -1,0 +1,61 @@
+if TARGET_TQMA93XX
+
+config SYS_BOARD
+	default "tqma93xx"
+
+config SYS_VENDOR
+	default "tq"
+
+config SYS_CONFIG_NAME
+	default "tqma93xx"
+
+config TQMA93XX_DEFAULTS
+	bool
+	select IMX9_LPDDR4X
+	select TQ_BB
+	select TQ_EEPROM
+	select TQ_RTC
+	select TQ_SDMMC
+	select TQ_VARD
+	imply MISC
+	imply I2C_EEPROM
+
+choice
+	prompt "TQMa93xx mainboard variant"
+	default TQMA93XX_BB_MBA93XXCA
+	help
+	  Select mainboard variant for the SOM. Default is the
+	  starterkit.
+
+config TQMA93XX_BB_MBA93XXCA
+	bool "MBa93xxCA starterkit"
+	select DM_GPIO
+	select ENV_IS_NOWHERE
+	select TQMA93XX_DEFAULTS
+	select USB_TCPC if USB
+	help
+	  Select the MBa93xxCA starterkit. The evaluation board can
+	  demonstrate most of the features of the SOM and CPU and can be
+	  used for LGA and connector SOM variants
+
+config TQMA93XX_BB_MBA93XXLA
+	bool "MBa93xxLA SBC"
+	select DM_GPIO
+	select ENV_IS_NOWHERE
+	select TQMA93XX_DEFAULTS
+	select USB_TCPC if USB
+	help
+	  Select the MBa93xxLA SBC. This mainboard has the LGA SOM
+	  variant assembled.
+
+endchoice
+
+config MTDPARTS_DEFAULT
+	default "mtdparts=425e0000.spi:4m@0k(bootstream),64k@4096k(env0),64k@4160k(env1),-@6m(ubi)"
+
+config MTDIDS_DEFAULT
+	default "nor0=425e0000.spi"
+
+source "board/tq/common/Kconfig"
+
+endif

board/tq/tqma93xx/MAINTAINERS
@@ -1,0 +1,9 @@
+TQ-Systems GmbH TQMa93xx[CA/LA] SOM / MBa93xx[CA/LA] board
+L:	u-boot@ew.tq-group.com
+W:	https://www.tq-group.com/en/products/tq-embedded/
+S:	Maintained
+F:	arch/arm/dts/imx93*-tqma93*-mba93*.dts*
+F:	board/tq/tqma93xx/
+F:	configs/tqma93*_mba93_*_defconfig
+F:	doc/boards/tq/tqma9*
+F:	include/configs/tqma93*.h

board/tq/tqma93xx/Makefile
@@ -1,0 +1,20 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (c) 2022-2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+# D-82229 Seefeld, Germany.
+# Author: Markus Niebel
+#
+
+obj-y += tqma93xx.o
+
+obj-$(CONFIG_TQMA93XX_BB_MBA93XXCA) += tqma93xx-mba93xx.o
+obj-$(CONFIG_TQMA93XX_BB_MBA93XXLA) += tqma93xx-mba93xx.o
+
+ifdef CONFIG_SPL_BUILD
+obj-y += spl.o
+obj-y += lpddr4x_tqma93xxca_timing_1gb.o
+obj-y += lpddr4x_tqma93xxla_timing_1gb.o
+obj-y += lpddr4x_tqma93xxla_timing_1gb5.o
+obj-y += lpddr4x_tqma93xxca_timing_2gb.o
+obj-y += lpddr4x_tqma93xxla_timing_2gb.o
+endif

commit 11c122c7ccd47614145274a5c3fc124691d98a6a

tqma93xx: env: remove unused variables and scripts
- [load]bootscript are remains from original port based on NXP SDK code
  and are not used from other scripts in environment
- mmcfw[part,path] are remains from original port not using shared
  environment header

Fixes: fcfa4005e5ff ("board/tq: add initial TQMa93xx[LA,CA] support")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

include/configs/tqma93xx.h
@@ -47,14 +47,7 @@
 	"fdtoverlay_addr_r=0x83080000\0"                               \
 	"initrd_addr=0x83800000\0"                                     \
 	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV) "\0"             \
-	"mmcfwpart=1\0"                                                \
-	"mmcfwpath=/\0"                                                \
 	"mmcautodetect=yes\0"                                          \
-	"loadbootscript=mmc dev ${mmcdev}; mmc rescan;"                \
-		"load mmc ${mmcdev}:${mmcfwpart} ${loadaddr} "         \
-		"${mmcfwpath}${script};\0"                             \
-	"bootscript=echo Running bootscript from mmc ...; "            \
-		"source\0"                                             \
 	"boot_os=booti ${kernel_addr_r} - ${fdt_addr_r};\0"            \
 	"emmc_bootp_start=0x0\0"                                       \
 	"uboot_mmc_start=0x40\0"                                       \