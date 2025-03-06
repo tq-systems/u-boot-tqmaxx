tq-systems/u-boot-tqmaxx

commit a5124bea6d017ad8359100f593b2eed9c4eac96c

tq/common: forward port tq_board_gpio
Ported from local fork of NXP vendor branch.

Simple table based approach to preconfigure board gpio and
provide them for further use in board code.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/common/tq_board_gpio.c
@@ -1,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2019 - 2024 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <asm/gpio.h>
+#include "tq_board_gpio.h"
+
+int tq_board_gpio_init(struct tq_gpio_init_data *data, int count)
+{
+	int ret;
+	int i;
+
+	for (i = 0; i < count; ++i) {
+		ret = dm_gpio_lookup_name(data[i].name, &data[i].desc);
+		if (ret) {
+			printf("error: gpio lookup %s\n", data[i].name);
+		} else {
+			ret = dm_gpio_request(&data[i].desc, data[i].label);
+			if (ret)
+				printf("error: gpio REQ %s\n", data[i].label);
+			else
+				dm_gpio_set_dir_flags(&data[i].desc,
+						      data[i].flags);
+		}
+	}
+
+	return 0;
+}

board/tq/common/Kconfig
@@ -2,6 +2,10 @@ config TQ_BB
 	bool
 	default y
 
+config TQ_BOARDGPIO
+	bool
+	default y if DM_GPIO
+
 config TQ_EEPROM
 	bool
 	select DM_I2C

board/tq/common/Makefile
@@ -18,6 +18,7 @@ ifdef MINIMAL
 obj- := __dummy__.o
 else
 obj-$(CONFIG_TQ_BB) += tq_bb.o
+obj-$(CONFIG_TQ_BOARDGPIO) += tq_board_gpio.o
 obj-$(CONFIG_TQ_EEPROM) += tq_eeprom.o
 obj-$(CONFIG_TQ_EMMC) += tq_emmc.o
 obj-$(CONFIG_TQ_ENVLOC) += tq_envloc.o

board/tq/common/tq_board_gpio.h
@@ -1,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2019 - 2023 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#ifndef __TQ_BOARD_GPIO_H__
+#define __TQ_BOARD_GPIO_H__
+
+#include <linux/stringify.h>
+
+struct tq_gpio_init_data {
+	const char *name;
+	const char *label;
+	const unsigned long flags;
+	struct gpio_desc desc;
+};
+
+#if defined(CONFIG_DM_GPIO)
+
+#define GPIO_INIT_DATA_ENTRY(IDX, NAME, FLAGS) \
+	[IDX] = { \
+		.name = (NAME), \
+		.label = __stringify(IDX), \
+		.flags = (FLAGS), \
+	}
+
+int tq_board_gpio_init(struct tq_gpio_init_data *data, int count);
+
+#else
+static inline int tq_board_gpio_init(struct tq_gpio_init_data *data,
+				     int count)
+{
+	return 0;
+}
+#endif
+
+#endif

commit 3375b500e40693eb2c8b8f985b6e52e094e594c1

tq: tq_board_gpio: add missing include
Use DM generic gpio header instead of SOC specific one.

Fixes: a5124bea6d01 ("tq/common: forward port tq_board_gpio")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/common/tq_board_gpio.c
@@ -5,7 +5,8 @@
  * Author: Markus Niebel
  */
 
-#include <asm/gpio.h>
+#include <asm-generic/gpio.h>
+
 #include "tq_board_gpio.h"
 
 int tq_board_gpio_init(struct tq_gpio_init_data *data, int count)