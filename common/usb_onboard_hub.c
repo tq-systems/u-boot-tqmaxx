8a7d20e4b7c246058ff50cc44060a2d319528f6b
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2025-03-14 16:09:01 +0100

    usb: usb_onboard_hub: add support for TUSB8041
    Add compatible and increase hub reset delay. The new 10 msec
    is the worst case found for devices in current upstream linux
    implementation at:
    
    https://web.git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/drivers/usb/misc/onboard_usb_dev.h
    Commit: 908f61bedb2c ("usb: misc: onboard_dev: extend platform data to add power on delay field")
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- common/usb_onboard_hub.c
+++ common/usb_onboard_hub.c
@@ -227,6 +227,10 @@ static const struct onboard_hub_data usb5744_data = {
 	.reset_us = 5,
 };
 
+static const struct onboard_hub_data usb8041_data = {
+	.reset_us = 30000,
+};
+
 static const struct udevice_id usb_onboard_hub_ids[] = {
 	/* Use generic usbVID,PID dt-bindings (usb-device.yaml) */
 	{	.compatible = "usb424,2514",	/* USB2514B USB 2.0 */
@@ -240,6 +244,12 @@ static const struct udevice_id usb_onboard_hub_ids[] = {
 	}, {
 		.compatible = "usb424,5744",	/* USB5744 USB 3.0 */
 		.data = (ulong)&usb5744_data,
+	}, {
+		.compatible = "usb451,8140",	/* TUSB8041 USB 3.0 */
+		.data = (ulong)&usb8041_data,
+	}, {
+		.compatible = "usb451,8142",	/* TUSB8041 USB 2.0 */
+		.data = (ulong)&usb8041_data,
 	}
 };
 
