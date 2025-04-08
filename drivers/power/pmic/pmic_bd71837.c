nxp-imx/uboot-imx

MLK-18387 pmic: Add Non-DM pmic driver for BD71837
The iMX8MM EVK board uses BD71837MWV pmic. Add its Non-DM driver to u-boot.

Signed-off-by: Ye Li <ye.li@nxp.com>
(cherry picked from commit e9a3bec2e95a4b2b4641223c8ee4ebd8da76d7f9)

drivers/power/pmic/pmic_bd71837.c
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2014 Gateworks Corporation
+ * Tim Harvey <tharvey@gateworks.com>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <common.h>
+#include <errno.h>
+#include <i2c.h>
+#include <power/pmic.h>
+#include <power/bd71837.h>
+
+static const char bd71837_name[] = "BD71837";
+int power_bd71837_init (unsigned char bus) {
+	struct pmic *p = pmic_alloc();
+
+	if (!p) {
+		printf("%s: POWER allocation error!\n", __func__);
+		return -ENOMEM;
+	}
+
+	p->name = bd71837_name;
+	p->interface = PMIC_I2C;
+	p->number_of_regs = BD71837_REG_NUM;
+	p->hw.i2c.addr = 0x4b;
+	p->hw.i2c.tx_num = 1;
+	p->bus = bus;
+
+	printf("power_bd71837_init\n");
+
+	return 0;
+}

tq-systems/u-boot-tqmaxx

(cherry picked from commit 2c749829010fc6e595b62abab0e39416032b9672)
(cherry picked from commit 833dd39a48b0a867e137f3f99d2251538b7bc1f9)

drivers/power/pmic/pmic_bd71837.c
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2014 Gateworks Corporation
+ * Tim Harvey <tharvey@gateworks.com>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <errno.h>
+#include <i2c.h>
+#include <power/pmic.h>
+#include <power/bd71837.h>
+
+static const char bd71837_name[] = "BD71837";
+int power_bd71837_init (unsigned char bus) {
+	struct pmic *p = pmic_alloc();
+
+	if (!p) {
+		printf("%s: POWER allocation error!\n", __func__);
+		return -ENOMEM;
+	}
+
+	p->name = bd71837_name;
+	p->interface = PMIC_I2C;
+	p->number_of_regs = BD718XX_MAX_REGISTER;
+	p->hw.i2c.addr = 0x4b;
+	p->hw.i2c.tx_num = 1;
+	p->bus = bus;
+
+	printf("power_bd71837_init\n");
+
+	return 0;
+}