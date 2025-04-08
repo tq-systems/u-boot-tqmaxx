nxp-imx/uboot-imx

MLK-16753-8 imx: mx6ul/ull: add optee support
Add different defconfigs.
Enable Trustzone.
Update env to runtime boot OP-TEE.

Signed-off-by: Peng Fan <peng.fan@nxp.com>

board/freescale/mx6ul_14x14_evk/mx6ul_14x14_evk.c
@@ -900,6 +900,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	setenv("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	setenv("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 	setenv("board_name", "EVK");
 

tq-systems/u-boot-tqmaxx

(cherry picked from commit af4e26c34e091994615ec721bc357b45e43c89e9)
(cherry picked from commit cd8397e9c973ac31766adf9565b2af18db9f9606)
(cherry picked from commit 79e2e2fbe7ddf376619efb43c4b4e6c4285a6f5c)
(cherry picked from commit 65fd479c23fa3d36d54c1853e3e2645ebcc56e96)
(cherry picked from commit df85fa7f7517b61852a9441db361015e4bf86cb6)
(cherry picked from commit 7b33b260cf77d37a906dcf1ca974c2f9ac7077fb)
(cherry picked from commit 838c7b9cc12c05e2f96bea5cda3cf775ba6c8fcb)
(cherry picked from commit 99452f78d8485d2d2baccf21ce04190e44a123d6)

board/freescale/mx6ul_14x14_evk/mx6ul_14x14_evk.c
@@ -351,6 +351,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	env_set("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	env_set("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 	env_set("board_name", "EVK");
 