nxp-imx/uboot-imx

MLK-16753-7 imx: mx6sx: add optee support
Add different defconfigs.
Enable Trustzone.
Update env to runtime boot OP-TEE.

Signed-off-by: Peng Fan <peng.fan@nxp.com>

board/freescale/mx6sxsabresd/mx6sxsabresd.c
@@ -963,6 +963,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	setenv("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	setenv("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

tq-systems/u-boot-tqmaxx

(cherry picked from commit 7c93e616d69f57668fb7e5ac24d8f7fa98dbe1f2)
(cherry picked from commit eb9063ee64bc30f51a20eec9da0750b0e82f3799)
(cherry picked from commit 580e0b29ca6501f706af6140357019e7f83692dd)
(cherry picked from commit c86df3e535c9528e43f36bfb8d362ca24f71098a)
(cherry picked from commit 306ccb6547769e79d2ee81a66fb993e467d87f02)
(cherry picked from commit 3443579fc35dca74c6ee295aacde911c901f6566)
(cherry picked from commit bbc395ef0603344a12be71cc1fabc707264bb9f0)
(cherry picked from commit be9fc723aae016192a0092e226fd9c16c5d07d33)

board/freescale/mx6sxsabresd/mx6sxsabresd.c
@@ -819,6 +819,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	env_set("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	env_set("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif