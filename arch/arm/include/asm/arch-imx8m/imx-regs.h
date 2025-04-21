nxp-imx/uboot-imx

LFU-219 crypto/fsl: iMX8M: Disable CAAM JR0 in SPL and Uboot
JRO is reserved in ATF for use by HAB(secure boot)
and not released to non secure(NS).
CAAM crypto operation are failing when use JR0 in uboot.

when DM is enabled
set the status of JR0 as disabled in device tree,
so JR0 is not initialized.
JR1 is initialized in SPL and Uboot.

when DM is not enabled
SEC job ring address is defined with JR1 offset(0x2000).

Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
Reviewed-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Franck LENORMAND <franck.lenormand@nxp.com>

arch/arm/include/asm/arch-imx8m/imx-regs.h
@@ -226,6 +226,7 @@
 #define CONFIG_SYS_FSL_SEC_ADDR         (CAAM_IPS_BASE_ADDR + \
 					 CONFIG_SYS_FSL_SEC_OFFSET)
 #define CONFIG_SYS_FSL_JR0_OFFSET       (0x1000)
+#define CONFIG_SYS_FSL_JR1_OFFSET       (0x2000)
 #define CONFIG_SYS_FSL_JR0_ADDR         (CONFIG_SYS_FSL_SEC_ADDR + \
 					 CONFIG_SYS_FSL_JR0_OFFSET)
 #define CONFIG_SYS_FSL_MAX_NUM_OF_SEC   1

tq-systems/u-boot-tqmaxx

(cherry picked from commit ed618ce6a95b51ec30f37311d374f9178e0cc44c)

arch/arm/include/asm/arch-imx8m/imx-regs.h
@@ -257,6 +257,7 @@
 #define CFG_SYS_FSL_SEC_ADDR         (CAAM_IPS_BASE_ADDR + \
 					 CFG_SYS_FSL_SEC_OFFSET)
 #define CFG_SYS_FSL_JR0_OFFSET       (0x1000)
+#define CFG_SYS_FSL_JR1_OFFSET       (0x2000)
 #define CFG_SYS_FSL_JR0_ADDR         (CFG_SYS_FSL_SEC_ADDR + \
 					 CFG_SYS_FSL_JR0_OFFSET)
 #if !defined(__ASSEMBLY__)