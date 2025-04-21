nxp-imx/uboot-imx

LFU-175 i.MX8ULP: Enables CAAM support for i.MX8ULP in SPL and U-Boot.
Message "release CAAM" to be sent to S400 before CAAM be
accessed for initialization.

CAAM job ring one(JR1) is available.
sec is initialized based on job ring information processed
from device tree.

Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
Signed-off-by: Varun Sethi <V.Sethi@nxp.com>
Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
(cherry picked from commit c62ca64678dc7ad2f52f22957b6755d9bf9418ce)

drivers/crypto/fsl/jr.c
@@ -789,7 +789,7 @@ int sec_init_idx(uint8_t sec_idx)
 #ifndef CONFIG_ARCH_IMX8
 	ccsr_sec_t *sec = caam->sec;
 	uint32_t mcr = sec_in32(&sec->mcfgr);
-#if defined(CONFIG_SPL_BUILD) && defined(CONFIG_IMX8M)
+#if defined(CONFIG_SPL_BUILD) && (defined(CONFIG_IMX8M) || defined(CONFIG_IMX8ULP))
 	uint32_t jrdid_ms = 0;
 #endif
 #ifdef CONFIG_FSL_CORENET
@@ -821,11 +821,14 @@ int sec_init_idx(uint8_t sec_idx)
 	mcr |= (1 << MCFGR_PS_SHIFT);
 #endif
 	sec_out32(&sec->mcfgr, mcr);
-	jr_reset();
-#if defined(CONFIG_SPL_BUILD) && defined(CONFIG_IMX8M)
+#ifdef CONFIG_IMX8ULP
+	sec_reset();
+#endif
+#if defined(CONFIG_SPL_BUILD) && (defined(CONFIG_IMX8M) || defined(CONFIG_IMX8ULP))
 	jrdid_ms = JRDID_MS_TZ_OWN | JRDID_MS_PRIM_TZ | JRDID_MS_PRIM_DID;
 	sec_out32(&sec->jrliodnr[caam->jrid].ms, jrdid_ms);
 #endif
+	jr_reset();
 
 #ifdef CONFIG_FSL_CORENET
 #ifdef CONFIG_SPL_BUILD

tq-systems/u-boot-tqmaxx

(cherry picked from commit 210f84da453bbd1bece77902172feaa4333bc2dc)

drivers/crypto/fsl/jr.c
@@ -823,7 +823,7 @@ int sec_init_idx(uint8_t sec_idx)
 
 	ccsr_sec_t *sec = caam->sec;
 	uint32_t mcr = sec_in32(&sec->mcfgr);
-#if defined(CONFIG_XPL_BUILD) && defined(CONFIG_IMX8M)
+#if defined(CONFIG_XPL_BUILD) && (defined(CONFIG_IMX8M) || defined(CONFIG_IMX8ULP))
 	uint32_t jrdid_ms = 0;
 #endif
 #ifdef CONFIG_FSL_CORENET
@@ -855,7 +855,10 @@ int sec_init_idx(uint8_t sec_idx)
 	mcr |= (1 << MCFGR_PS_SHIFT);
 #endif
 	sec_out32(&sec->mcfgr, mcr);
-#if defined(CONFIG_XPL_BUILD) && defined(CONFIG_IMX8M)
+#ifdef CONFIG_IMX8ULP
+	sec_reset();
+#endif
+#if defined(CONFIG_XPL_BUILD) && (defined(CONFIG_IMX8M) || defined(CONFIG_IMX8ULP))
 	jrdid_ms = JRDID_MS_TZ_OWN | JRDID_MS_PRIM_TZ | JRDID_MS_PRIM_DID;
 	sec_out32(&sec->jrliodnr[caam->jrid].ms, jrdid_ms);
 #endif