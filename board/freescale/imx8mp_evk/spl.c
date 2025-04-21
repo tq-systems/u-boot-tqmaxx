nxp-imx/uboot-imx

MLK-24860-2 imx8mp_evk: Add DDR4 EVK board support
This board uses 4GB DDR4 and replace emmc and flexspi to a raw NAND
socket. Add the support for this board, including SD boot, NAND boot,
and inline ECC (top 1/8 DDR used for ecc).

Signed-off-by: Ye Li <ye.li@nxp.com>
Acked-by: Peng Fan <peng.fan@nxp.com>

board/freescale/imx8mp_evk/spl.c
@@ -201,6 +201,11 @@ int power_init_board(void)
 	/* To avoid timing risk from SOC to ARM,increase VDD_ARM to OD voltage 0.95v */
 	pmic_reg_write(p, PCA9450_BUCK2OUT_DVS0, 0x1C);
 
+#ifdef CONFIG_IMX8M_DDR4
+	/* Set NVCC_DRAM to 1.2v for DDR4 */
+	pmic_reg_write(p, PCA9450_BUCK6OUT, 0x18);
+#endif
+
 	/* set WDOG_B_CFG to cold reset */
 	pmic_reg_write(p, PCA9450_RESET_CTRL, 0xA1);
 

tq-systems/u-boot-tqmaxx

(cherry picked from commit 9b47aaef967bb7655312895c133e64e32e8f5b87)
(cherry picked from commit 155413f5aa41f782a1a6d68e208d523be7ab83a0)
(cherry picked from commit 7e6073fcea5174c247680e8c6a1a22903f2ec7cf)
(cherry picked from commit 87959e3b4ab6608c0207ae59585bf1d29b855fc5)

board/freescale/imx8mp_evk/spl.c
@@ -98,6 +98,11 @@ int power_init_board(void)
 
 	pmic_reg_write(dev, PCA9450_BUCK2OUT_DVS0, 0x1C);
 
+#ifdef CONFIG_IMX8M_DDR4
+	/* Set NVCC_DRAM to 1.2v for DDR4 */
+	pmic_reg_write(dev, PCA9450_BUCK6OUT, 0x18);
+#endif
+
 	return 0;
 }
 #endif