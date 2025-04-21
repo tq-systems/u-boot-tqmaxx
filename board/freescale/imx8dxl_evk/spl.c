nxp-imx/uboot-imx

MLK-24750-2 iMX8DXL: Implement the spl_board_boot_device for EVK and VAL
Re-map the boot devices of iMX8DXL EVK and VAL with SPL devices.
DDR3 Validation board does not have eMMC, so SD2 should be the first mmc
device with index 0. Otherwise, mmc uclass will fail to get the device
with default index 1.

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

board/freescale/imx8dxl_evk/spl.c
@@ -13,8 +13,31 @@
 #include <dm/device-internal.h>
 #include <dm/lists.h>
 #include <bootm.h>
+#include <asm/mach-imx/boot_mode.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+int spl_board_boot_device(enum boot_device boot_dev_spl)
+{
+	switch (boot_dev_spl) {
+	case MMC1_BOOT:
+		return BOOT_DEVICE_MMC1;
+	case SD2_BOOT:
+#ifdef CONFIG_TARGET_IMX8DXL_DDR3_VAL
+		return BOOT_DEVICE_MMC1;
+#else
+		return BOOT_DEVICE_MMC2_2;
+#endif
+	case FLEXSPI_BOOT:
+		return BOOT_DEVICE_SPI;
+	case NAND_BOOT:
+		return BOOT_DEVICE_NAND;
+	case USB_BOOT:
+		return BOOT_DEVICE_BOARD;
+	default:
+		return BOOT_DEVICE_NONE;
+	}
+}
 
 void spl_board_init(void)
 {

tq-systems/u-boot-tqmaxx

(cherry picked from commit e6db58b5e0846a9610e65d9f5549ba9d006b4305)
(cherry picked from commit d0c0a6c9f41b8298453c3981033a5c210d20076d)
(cherry picked from commit e7d537b991306beed7f16650ee560ddfba418549)
(cherry picked from commit 0054823002e17efdb585c1dd9a40d89c4736f4a7)

board/freescale/imx8dxl_evk/spl.c
@@ -17,8 +17,31 @@
 #include <dm/lists.h>
 #include <bootm.h>
 #include <asm/sections.h>
+#include <asm/mach-imx/boot_mode.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+int spl_board_boot_device(enum boot_device boot_dev_spl)
+{
+	switch (boot_dev_spl) {
+	case MMC1_BOOT:
+		return BOOT_DEVICE_MMC1;
+	case SD2_BOOT:
+#ifdef CONFIG_TARGET_IMX8DXL_DDR3_EVK
+		return BOOT_DEVICE_MMC1;
+#else
+		return BOOT_DEVICE_MMC2_2;
+#endif
+	case FLEXSPI_BOOT:
+		return BOOT_DEVICE_SPI;
+	case NAND_BOOT:
+		return BOOT_DEVICE_NAND;
+	case USB_BOOT:
+		return BOOT_DEVICE_BOARD;
+	default:
+		return BOOT_DEVICE_NONE;
+	}
+}
 
 void spl_board_init(void)
 {