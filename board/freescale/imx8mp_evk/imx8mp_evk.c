ZexiangW/u-boot

LF-13724-2 usb: xhci-imx8m: set USB_PWR pad to GPIO mode before boot linux
When usb controller starts, the pinctrl driver will set USB_PWR pad to
default state (PWR mode) and keeps this state even after usb controller
stops. This will bring an issue on VBUS line after kernel startup. We can
see some glitches on VBUS line when USB device attached from the beginning.
The glitch may be caused by usb hardware, then some devices may not work
well under such condition. To avoid such glitch, this will set USB_PWR pad
to GPIO mode after controller stops and before boot linux. The VBUS will
be low after board_prep_linux() called in uboot until kernel enable it to
high.

Reviewed-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Xu Yang <xu.yang_2@nxp.com>

board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -26,6 +26,8 @@
 #include <usb.h>
 #include <dwc3-uboot.h>
 #include <mmc.h>
+#include <dm/uclass-internal.h>
+#include <dm/pinctrl.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -489,6 +491,18 @@ int board_late_init(void)
 
 	return 0;
 }
+
+#ifndef CONFIG_SPL_BUILD
+void board_prep_linux(struct bootm_headers *images)
+{
+	int ret;
+	struct udevice *dwc3_usb;
+
+	ret = uclass_find_device_by_seq(UCLASS_USB, 1, &dwc3_usb);
+	if (!ret)
+		pinctrl_select_state(dwc3_usb, "gpio");
+}
+#endif
 
 #ifdef CONFIG_ANDROID_SUPPORT
 bool is_power_key_pressed(void) {

tq-systems/u-boot-tqmaxx

board/freescale/imx8mp_evk/imx8mp_evk.c
@@ -26,6 +26,8 @@
 #include "../common/tcpc.h"
 #include <usb.h>
 #include <dwc3-uboot.h>
+#include <dm/uclass-internal.h>
+#include <dm/pinctrl.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -484,3 +486,15 @@ int board_late_init(void)
 
 	return 0;
 }
+
+#ifndef CONFIG_XPL_BUILD
+void board_prep_linux(struct bootm_headers *images)
+{
+	int ret;
+	struct udevice *dwc3_usb;
+
+	ret = uclass_find_device_by_seq(UCLASS_USB, 1, &dwc3_usb);
+	if (!ret)
+		pinctrl_select_state(dwc3_usb, "gpio");
+}
+#endif