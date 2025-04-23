nxp-imx/uboot-imx

MA-21373-2 trusty: add rng u-boot driver
CAAM hardware may be reserved for trusty os and no CAAM
hardware can be used in bootloader in such scenario.
Register a 'UCLASS_RNG' RNG driver based on hwcrypto RNG
interface to provide RNG functions.

Also delay the trusty ipc shutdown so the hwcrypto service
is availabe at the point of kaslr generation.

Change-Id: I75b3eee8f2d9376c5d6b24e44fbc3852c56de175
Signed-off-by: Ji Luo <ji.luo@nxp.com>

arch/arm/mach-imx/imx8m/soc.c
@@ -1779,7 +1779,7 @@ void reset_cpu(void)
 #if defined(CONFIG_ARCH_MISC_INIT)
 int arch_misc_init(void)
 {
-#if !defined(CONFIG_ANDROID_SUPPORT) || defined(CONFIG_SPL_BUILD)
+#if !defined(CONFIG_IMX_TRUSTY_OS) || defined(CONFIG_SPL_BUILD)
 	if (IS_ENABLED(CONFIG_FSL_CAAM)) {
 		struct udevice *dev;
 		int ret;

tq-systems/u-boot-tqmaxx

arch/arm/mach-imx/imx8m/soc.c
@@ -1759,7 +1759,7 @@ void reset_cpu(void)
 #if defined(CONFIG_ARCH_MISC_INIT)
 int arch_misc_init(void)
 {
-#if !defined(CONFIG_ANDROID_SUPPORT) || defined(CONFIG_XPL_BUILD)
+#if !defined(CONFIG_IMX_TRUSTY_OS) || defined(CONFIG_XPL_BUILD)
 	if (IS_ENABLED(CONFIG_FSL_CAAM)) {
 		struct udevice *dev;
 		int ret;