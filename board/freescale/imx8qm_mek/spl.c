nxp-imx/uboot-imx

MA-17916-1 Clean Up: Guard codes with correct configs
This commit does some clean-up to guard the codes/configs with
correct configs, so we can enable/disable the feature without
modifying the codes.

Test: builds and boots on imx8qm.

Signed-off-by: Ji Luo <ji.luo@nxp.com>
Change-Id: Ic4cf4d9f47bd5a4317b2621a5378cb4b192e52fb

board/freescale/imx8qm_mek/spl.c
@@ -31,9 +31,12 @@ void spl_board_init(void)
 
 	timer_init();
 
+#ifdef CONFIG_SPL_SERIAL_SUPPORT
 	preloader_console_init();
 
 	puts("Normal Boot\n");
+#endif
+
 }
 
 void spl_board_prepare_for_boot(void)

board/freescale/imx8qxp_mek/spl.c
@@ -31,9 +31,11 @@ void spl_board_init(void)
 
 	timer_init();
 
+#ifdef CONFIG_SPL_SERIAL_SUPPORT
 	preloader_console_init();
 
 	puts("Normal Boot\n");
+#endif
 }
 
 void spl_board_prepare_for_boot(void)

tq-systems/u-boot-tqmaxx

(cherry picked from commit 2c2363e47b858bd178c44869328eca211b2c3f62)
(cherry picked from commit cdf01e8a0fbf29df6230344889d1ec5035b6c978)
(cherry picked from commit b89b45f60d56b5fe7c138b67792e84cd356bab53)

board/freescale/imx8qm_mek/spl.c
@@ -38,9 +38,12 @@ void spl_board_init(void)
 
 	timer_init();
 
+#ifdef CONFIG_SPL_SERIAL
 	preloader_console_init();
 
 	puts("Normal Boot\n");
+#endif
+
 }
 
 void spl_board_prepare_for_boot(void)

board/freescale/imx8qxp_mek/spl.c
@@ -56,9 +56,11 @@ void spl_board_init(void)
 	imx8_iomux_setup_multiple_pads(usdhc2_sd_pwr, ARRAY_SIZE(usdhc2_sd_pwr));
 	gpio_direction_output(USDHC2_SD_PWR, 0);
 
+#ifdef CONFIG_SPL_SERIAL
 	preloader_console_init();
 
 	puts("Normal Boot\n");
+#endif
 }
 
 void spl_board_prepare_for_boot(void)