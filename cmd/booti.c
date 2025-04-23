nxp-imx/uboot-imx

MA-16877 Skip kernel hab authentication for android
Android use AVB to verify the kernel, hab authentication is
not necessary for boot image.

For imx8m, don't authenticate the kernel image when AVB
(CONFIG_AVB_SUPPORT) is enabled. For imx8q, as android uses
different 'CONFIG_EXTRA_ENV_SETTINGS' and 'CONFIG_BOOTCOMMAND'
with linux bsp, so it won't try to do kernel hab authentication.
by default.

Test: boot imx8mp with "CONFIG_SECURE_BOOT" and imx8qxp with
      'CONFIG_AHAB_BOOT'.

Change-Id: I1b2087ce7d8f9795422a053b6b68a694c86f0b3d
Signed-off-by: Ji Luo <ji.luo@nxp.com>

cmd/booti.c
@@ -41,7 +41,7 @@ static int booti_start(cmd_tbl_t *cmdtp, int flag, int argc,
 	if (ret != 0)
 		return 1;
 
-#ifdef CONFIG_SECURE_BOOT
+#if defined(CONFIG_SECURE_BOOT) && !defined(CONFIG_AVB_SUPPORT)
 	extern int authenticate_image(
 		uint32_t ddr_start, uint32_t raw_image_size);
 	if (authenticate_image(ld, image_size) != 0) {

tq-systems/u-boot-tqmaxx

Test: boot imx8mp with "CONFIG_IMX_HAB" and imx8qxp with
(cherry picked from commit f907e4ac090e960ba5110b8039cccc4296841595)
(cherry picked from commit f9f6ae742696f51326ec7da5b0c74a7fca21f774)
(cherry picked from commit cadfe9a4fa40333dc1813506cd58c5c5000027f8)

cmd/booti.c
@@ -125,7 +125,7 @@ static int booti_start(struct bootm_info *bmi)
 	if (ret)
 		return 1;
 
-#ifdef CONFIG_IMX_HAB
+#if defined(CONFIG_IMX_HAB) && !defined(CONFIG_AVB_SUPPORT)
 	extern int authenticate_image(
 		uint32_t ddr_start, uint32_t raw_image_size);
 	if (authenticate_image(ld, image_size) != 0) {