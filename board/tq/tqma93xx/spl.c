tq-systems/u-boot-tqmaxx

commit e79509c42ec92e17b06f773a5812e02de5a66201

boards: tqma93xx: support for TQMa91xx variants
Since this CPU family is pin to pin compatible the board support
code is reused at the cost of some preprocessor conditionals.

MAINTAINERS file is updated and documentation including SRAM
usage is added.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma93xx/spl.c
@@ -37,6 +37,9 @@ extern struct dram_timing_info tqma93xxla_dram_timing_1gb5;
 extern struct dram_timing_info tqma93xxca_dram_timing_2gb;
 extern struct dram_timing_info tqma93xxla_dram_timing_2gb;
 
+extern struct dram_timing_info tqma91xxca_dram_timing_1gb;
+extern struct dram_timing_info tqma91xxla_dram_timing_1gb;
+
 enum tqma93xxxa_ram_size {
 	TQMA93XXXA_RAM_SIZE_1G = 1,
 	TQMA93XXXA_RAM_SIZE_1G5,
@@ -50,11 +53,16 @@ struct dram_info {
 };
 
 static const struct dram_info tqma93xx_dram_info[]  = {
+#if IS_ENABLED(CONFIG_IMX91)
+	{ &tqma91xxca_dram_timing_1gb, SZ_1G * 1ULL, 'c' },
+	{ &tqma91xxla_dram_timing_1gb, SZ_1G * 1ULL, 'l' },
+#elif IS_ENABLED(CONFIG_IMX93)
 	{ &tqma93xxca_dram_timing_1gb, SZ_1G * 1ULL, 'c' },
 	{ &tqma93xxca_dram_timing_2gb, SZ_1G * 2ULL, 'c' },
 	{ &tqma93xxla_dram_timing_1gb, SZ_1G * 1ULL, 'l' },
 	{ &tqma93xxla_dram_timing_1gb5, SZ_512M * 3ULL, 'l' },
 	{ &tqma93xxla_dram_timing_2gb, SZ_1G * 2ULL, 'l' },
+#endif
 };
 
 static int tqma93xx_ram_timing_idx = -1;
@@ -296,6 +304,16 @@ static int power_init_board(void)
 		/* set standby voltage to 0.65v */
 		pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS1, trim_val);
 
+		if (IS_ENABLED(CONFIG_IMX91)) {
+			puts("TQMa91xx: LPDDR4\n");
+			/* Set VDDQ to 1.1V from buck2 (LPDDR4): 600 mV + 40 * 12.5 mV */
+			pmic_reg_write(dev, PCA9450_BUCK2OUT_DVS0, 40);
+
+		} else if (IS_ENABLED(CONFIG_IMX93)) {
+			puts("TQMa93xx: LPDDR4x\n");
+			/* Set VDDQ to 0.6V from buck2 (LPDDR4x): 600 mV + 0 * 12.5 mV */
+			pmic_reg_write(dev, PCA9450_BUCK2OUT_DVS0, 0);
+		}
 		/*
 		 * I2C_LT_EN: I2C level translator Forcedly Disable (POR default)
 		 * usage is dicouraged by TQ Systems according to in house testing
@@ -359,10 +377,12 @@ void board_init_f(ulong dummy)
 	tq_vard_show(&vard);
 	spl_dram_init(ramtype);
 
-	/* Put M33 into CPUWAIT for following kick */
-	ret = m33_prepare();
-	if (ret)
-		printf("ERROR: M33 prepare %d\n", ret);
+	if (CONFIG_IS_ENABLED(IMX93)) {
+		/* Put M33 into CPUWAIT for following kick */
+		ret = m33_prepare();
+		if (ret)
+			printf("ERROR: M33 prepare %d\n", ret);
+	}
 
 	board_init_r(NULL, 0);
 }

arch/arm/mach-imx/imx9/Kconfig
@@ -44,6 +44,15 @@ config TARGET_IMX91_9X9_QSB
 	select IMX91
 	select IMX9_LPDDR4X
 
+config TARGET_TQMA91XX
+	bool "TQMa91xx family of SOM"
+	select IMX91
+	select IMX9_LPDDR4X
+	help
+	  This enables support for TQ-Systems GmbH family of i.MX91
+	  based SOM supporting different i.MX91 variants and different
+	  SOM form factors
+
 config TARGET_IMX93_14X14_EVK
 	bool "imx93_14x14_evk"
 	select IMX93

board/tq/tqma93xx/Kconfig
@@ -1,4 +1,4 @@
-if TARGET_TQMA93XX
+if TARGET_TQMA91XX || TARGET_TQMA93XX
 
 config SYS_BOARD
 	default "tqma93xx"

board/tq/tqma93xx/MAINTAINERS
@@ -2,10 +2,14 @@ TQ-Systems GmbH TQMa93xx[CA/LA] SOM / MBa93xx[CA/LA] board
 L:	u-boot@ew.tq-group.com
 W:	https://www.tq-group.com/en/products/tq-embedded/
 S:	Maintained
+F:	arch/arm/dts/imx91*-tqma91*-mba91*.dts*
+F:	arch/arm/dts/imx91*-tqma91*-mba93*.dts*
 F:	arch/arm/dts/imx93*-tqma93*-mba91*.dts*
 F:	arch/arm/dts/imx93*-tqma93*-mba93*.dts*
 F:	board/tq/tqma93xx/
+F:	configs/tqma91*_mba91*_defconfig
+F:	configs/tqma91*_mba93*_defconfig
 F:	configs/tqma93*_mba91*_defconfig
-F:	configs/tqma93*_mba93_*_defconfig
-F:	doc/boards/tq/tqma9*
+F:	configs/tqma93*_mba93*_defconfig
+F:	doc/board/tq/tqma91*
 F:	include/configs/tqma93*.h

board/tq/tqma93xx/Makefile
@@ -13,6 +13,8 @@ obj-$(CONFIG_TQMA93XX_BB_MBA93XXLA) += tqma93xx-mba93xx.o
 
 ifdef CONFIG_SPL_BUILD
 obj-y += spl.o
+obj-$(CONFIG_IMX91) += lpddr4_tqma91xxca_timing_1gb.o
+obj-$(CONFIG_IMX91) += lpddr4_tqma91xxla_timing_1gb.o
 obj-$(CONFIG_IMX93) += lpddr4x_tqma93xxca_timing_1gb.o
 obj-$(CONFIG_IMX93) += lpddr4x_tqma93xxla_timing_1gb.o
 obj-$(CONFIG_IMX93) += lpddr4x_tqma93xxla_timing_1gb5.o

board/tq/tqma93xx/tqma93xx-mba91xx.c
@@ -12,7 +12,11 @@
 #include <asm/arch/clock.h>
 #include <asm/arch/sys_proto.h>
 #include <asm/arch-imx9/ccm_regs.h>
+#if CONFIG_IS_ENABLED(IMX91)
+#include <asm/arch-imx9/imx91_pins.h>
+#elif CONFIG_IS_ENABLED(IMX93)
 #include <asm/arch-imx9/imx93_pins.h>
+#endif
 #include <asm/mach-imx/iomux-v3.h>
 #include <asm-generic/gpio.h>
 
@@ -26,8 +30,13 @@ DECLARE_GLOBAL_DATA_PTR;
 #define UART_PAD_CTRL	(PAD_CTL_DSE(6) | PAD_CTL_FSEL2)
 
 static const iomux_v3_cfg_t uart_pads[] = {
+#if CONFIG_IS_ENABLED(MX91)
+	MX91_PAD_UART1_RXD__LPUART1_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	MX91_PAD_UART1_TXD__LPUART1_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+#elif CONFIG_IS_ENABLED(MX93)
 	MX93_PAD_UART1_RXD__LPUART1_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
 	MX93_PAD_UART1_TXD__LPUART1_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+#endif
 };
 
 /*

commit c4d541142523599beaf1d194b5307d02c279ab63

tqma933xx: spl: fix conditional around m33_prepare
CONFIG_IS_ENABLED(IMX93) will expand to false in SPL since it checks for
CONFIG_SPL_IMX93. The correct macro to use is IS_ENABLED(CONFIG_IMX93).
Fix this to be able to use Cortex-M33 for i.MX93 SoM.

Fixes: e79509c42ec9 ("boards: tqma93xx: support for TQMa91xx variants")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma93xx/spl.c
@@ -377,7 +377,7 @@ void board_init_f(ulong dummy)
 	tq_vard_show(&vard);
 	spl_dram_init(ramtype);
 
-	if (CONFIG_IS_ENABLED(IMX93)) {
+	if (IS_ENABLED(CONFIG_IMX93)) {
 		/* Put M33 into CPUWAIT for following kick */
 		ret = m33_prepare();
 		if (ret)