18ed3edbafbf217d641358bef23220d0fc073f3a
Markus Niebel <Markus.Niebel@ew.tq-group.com>
2025-06-04 16:53:18 +0200

    mach-imx: imx8m: soc: rewrite board_get_serial
    Add support for 128bit UID on i.MX8MP. Use same approach as
    implemented for linux in commit: fd0bf2bb3221 ("soc: imx8m:
    Dump higher 64bits UID").
    
    While at it print UID the same way as it was implemented for
    imx9.
    
    Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -858,16 +858,35 @@ bool is_usb_boot(void)
 	return get_boot_device() == USB_BOOT;
 }
 
+#define OCOTP_UID_LOW			0x410
+#define OCOTP_UID_HIGH			0x420
+
+#define IMX8MP_OCOTP_UID_OFFSET		0x10
+#define IMX8MP_OCOTP_UID_HIGH		0xE00
+
 #ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 void get_board_serial(struct tag_serialnr *serialnr)
 {
-	struct ocotp_regs *ocotp = (struct ocotp_regs *)OCOTP_BASE_ADDR;
-	struct fuse_bank *bank = &ocotp->bank[0];
-	struct fuse_bank0_regs *fuse =
-		(struct fuse_bank0_regs *)bank->fuse_regs;
+	void __iomem *base = (void *)OCOTP_BASE_ADDR;
+	u32 uid[4];
 
-	serialnr->low = fuse->uid_low;
-	serialnr->high = fuse->uid_high;
+	if (is_imx8mp()) {
+		uid[3] = readl_relaxed(base + IMX8MP_OCOTP_UID_HIGH + 0x10);
+		uid[2] = readl_relaxed(base + IMX8MP_OCOTP_UID_HIGH);
+		uid[1] = readl_relaxed(base + OCOTP_UID_HIGH + IMX8MP_OCOTP_UID_OFFSET);
+		uid[0] = readl_relaxed(base + OCOTP_UID_LOW + IMX8MP_OCOTP_UID_OFFSET);
+
+		printf("UID: %08x%08x%08x%08x\n", uid[3], uid[2], uid[1], uid[0]);
+	} else {
+		uid[1] = readl_relaxed(base + OCOTP_UID_HIGH);
+		uid[0] = readl_relaxed(base + OCOTP_UID_LOW);
+
+		printf("UID: %08x%08x\n", uid[0], uid[1]);
+	}
+
+	serialnr->low = uid[0];
+	serialnr->high = uid[1];
+
 }
 #endif
 
