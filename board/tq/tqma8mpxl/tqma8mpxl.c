tq-systems/u-boot-tqmaxx

commit fb5a8f257caefd3c1fd6b3998cc9431b937927f5

tq: add TQMa8MPxL SoM and SBC
Add board code including the different RAM configuration data
for TQ-Systems GmbH solderable SoM based on i.MX8MP SoC family.
The SoM needs a base board.

The code is intended to be usable for different base boards.
With this commit the starter kit / SBC MBa8MPxL and SBC
MBa8MP-RAS314 are also added.

The board code was written with focus of compiled in DM
support for USB / USB gadget and ethernet. No specific
board code for these use cases is needed so far.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>
Signed-off-by: Stein Alexander <alexander.stein@tq-group.com>

board/tq/tqma8mpxl/tqma8mpxl.c
@@ -1,0 +1,308 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2021 - 2022 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+ * D-82229 Seefeld, Germany.
+ * Author: Markus Niebel
+ */
+
+#include <env.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/imx8mp_pins.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/boot_mode.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <asm/arch/clock.h>
+#include <spl.h>
+#include <asm/mach-imx/dma.h>
+#include <bloblist.h>
+#include <jffs2/load_kernel.h>
+#include <mtd_node.h>
+#include <power/pmic.h>
+#include <mmc.h>
+
+#include "../common/tq_bb.h"
+#include "../common/tq_blob.h"
+#include "../common/tq_eeprom.h"
+#include "../common/tq_som_features.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
+
+static iomux_v3_cfg_t const wdog_pads[] = {
+	MX8MP_PAD_GPIO1_IO02__WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
+};
+
+int board_early_init_f(void)
+{
+	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
+
+	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
+
+	set_wdog_reset(wdog);
+
+	return tq_bb_board_early_init_f();
+}
+
+#if !defined(CONFIG_SPL_BUILD)
+
+/**
+ * Global copy of EEPROM data to prevent multiple reads.
+ *
+ * Data is read during board_late_init
+ */
+static struct tq_eeprom_data eeprom;
+
+#if CONFIG_IS_ENABLED(BLOBLIST)
+
+int board_phys_sdram_size(phys_size_t *size)
+{
+	struct tq_raminfo *raminfo;
+
+	if (!size)
+		return -EINVAL;
+
+	raminfo = bloblist_find(BLOBLISTT_TQ_RAMSIZE, sizeof(*raminfo));
+	if (!raminfo) {
+		printf("Unable to find RAMSIZE blob from SPL\n");
+		return -ENOENT;
+	}
+
+	*size = raminfo->memsize;
+
+	return 0;
+}
+
+#endif
+
+/**
+ * Translate detected CPU variante into TQ-Systems SOM variant short name
+ *
+ * return: string consisting of the name or string indicating unknown variant
+ */
+static const char *tq_get_boardname(void)
+{
+	switch (get_cpu_type()) {
+	/* all normal quad core variants */
+	case MXC_CPU_IMX8MP:	/* NPU */
+	case MXC_CPU_IMX8MP6:	/* w/o NPU */
+		return "TQMa8MPQL";
+	/* quad lite variant */
+	case MXC_CPU_IMX8MPL:
+		return "TQMa8MPQLL";
+	/* dual core variant */
+	case MXC_CPU_IMX8MPD:
+		return "TQMa8MPDL";
+	/* add more if new variants will be assembled */
+	default:
+		return "??";
+	}
+
+	return "UNKNOWN";
+}
+
+int print_bootinfo(void)
+{
+	enum boot_device bt_dev;
+
+	bt_dev = get_boot_device();
+
+	puts("Boot:  ");
+	switch (bt_dev) {
+	case SD1_BOOT:
+		puts("USDHC1(SD)\n");
+		break;
+	case SD2_BOOT:
+		puts("USDHC2(SD)\n");
+		break;
+	case SD3_BOOT:
+		puts("USDHC3(SD)\n");
+		break;
+	case MMC1_BOOT:
+		puts("USDHC1(e-MMC)\n");
+		break;
+	case MMC2_BOOT:
+		puts("USDHC2(e-MMC)\n");
+		break;
+	case MMC3_BOOT:
+		puts("USDHC3(e-MMC)\n");
+		break;
+	case USB_BOOT:
+		puts("USB\n");
+		break;
+	case QSPI_BOOT:
+		puts("FlexSPI\n");
+		break;
+	default:
+		printf("Unknown/Unsupported device %u\n", bt_dev);
+		break;
+	}
+
+	return 0;
+}
+
+int checkboard(void)
+{
+	print_bootinfo();
+	printf("Board: %s on a %s\n", tq_get_boardname(),
+	       tq_bb_get_boardname());
+	if (CONFIG_IS_ENABLED(IMX8M_DRAM_INLINE_ECC))
+		puts("Inline ECC\n");
+
+	return tq_bb_checkboard();
+}
+
+int board_init(void)
+{
+	tq_bb_board_init();
+
+	return 0;
+}
+
+static struct tq_som_feature tqma8mpxl_som_features[] = {
+	{
+		.feature = FEATURE_EMMC,
+		.dt_path = "/soc@0/bus@30800000/mmc@30b60000",
+	}, {
+		.feature = FEATURE_EEPROM,
+		.dt_path = "/soc@0/bus@30800000/i2c@30a20000/eeprom@57",
+	}, {
+		.feature = FEATURE_SPINOR,
+		.dt_path = "/soc@0/bus@30800000/spi@30bb0000/flash@0",
+	}, {
+		.feature = FEATURE_SECELEM,
+		/* TODO: no driver yet */
+	}, {
+		.feature = FEATURE_RTC,
+		.dt_path = "/soc@0/bus@30800000/i2c@30a20000/rtc@51",
+	},
+};
+
+static struct tq_som_feature_list tqma8mpxl_feature_list = {
+	.list = tqma8mpxl_som_features,
+	.entries = ARRAY_SIZE(tqma8mpxl_som_features),
+};
+
+const struct tq_som_feature_list *tq_board_detect_features(void)
+{
+	return &tqma8mpxl_feature_list;
+}
+
+#ifdef CONFIG_OF_BOARD_SETUP
+#ifdef CONFIG_IMX8M_DRAM_INLINE_ECC
+int fixup_ecc_reserved_mem(void *blob, struct bd_info *bis)
+{
+	int ret;
+	phys_size_t ram_size = 0x0;
+	phys_addr_t ecc_start = 0x0;
+	phys_size_t ecc_size = 0x0;
+
+	board_phys_sdram_size(&ram_size);
+
+	/*
+	 * In case of inline ECC, the available ram size has been reduced by
+	 * SPL to 7/8 of total ram size, leaving 1/8 untouched for ECC parity
+	 * data.
+	 * The reserved memory node has therefore a size of 1/7 of the
+	 * available ram size (as returned by board_phys_sdram_size), starting
+	 * on top of the available ram space.
+	 */
+	ecc_size = ram_size / 7ULL;
+	ecc_start = CONFIG_SYS_SDRAM_BASE + ram_size;
+
+	ret = add_res_mem_dt_node(blob, "ecc", ecc_start, ecc_size);
+	if (ret < 0) {
+		printf("Could not create ecc reserved-memory node.\n");
+		return ret;
+	}
+
+	return 0;
+}
+#endif
+int ft_board_setup(void *blob, struct bd_info *bis)
+{
+#ifdef CONFIG_IMX8M_DRAM_INLINE_ECC
+	fixup_ecc_reserved_mem(blob, bis);
+#endif
+
+	if (CONFIG_IS_ENABLED(FDT_FIXUP_PARTITIONS)) {
+		const char * const path = "/soc@0/bus@30800000/spi@30bb0000";
+		const struct node_info nodes[] = {
+			{ "jedec,spi-nor",	MTD_DEV_TYPE_NOR, },
+		};
+
+		if (tq_vard_valid(&eeprom.tq_hw_data.vard)) {
+			if (tq_vard_has_spinor(&eeprom.tq_hw_data.vard)) {
+				/*
+				 * Update MTD partition nodes using info from
+				 * mtdparts env var
+				 * for [Q]SPI this needs the device probed.
+				 */
+				puts("   Updating MTD partitions...\n");
+				tq_ft_spi_setup(blob, path, nodes,
+						 ARRAY_SIZE(nodes));
+			}
+		}
+	}
+
+	return tq_bb_ft_board_setup(blob, bis);
+}
+#endif
+
+int board_late_init(void)
+{
+	const char *bname = tq_get_boardname();
+	int ret;
+	bool features_detected = false;
+
+	if (CONFIG_IS_ENABLED(I2C_EEPROM))
+		ret = tq_read_module_eeprom(&eeprom);
+	else
+		ret = tq_read_eeprom_at(0, 0, &eeprom);
+
+	if (!ret) {
+		tq_board_handle_eeprom_data(bname, &eeprom);
+		if (tq_vard_valid(&eeprom.tq_hw_data.vard)) {
+			/*
+			 * set quartz load to 7.000 femtofarads
+			 * only if RTC is assembled, to prevent warnings
+			 */
+			if (tq_vard_has_rtc(&eeprom.tq_hw_data.vard)) {
+				if (tq_pcf85063_adjust_capacity(0,
+								 0x51,
+								 7000))
+					puts("PCF85063: adjust error\n");
+			}
+			/*
+			 * fill feature presence flags from vard
+			 */
+			if (!tq_vard_detect_features(&eeprom.tq_hw_data.vard,
+						     &tqma8mpxl_feature_list))
+				features_detected = true;
+		}
+	} else {
+		pr_err("EEPROM: read error %d\n", ret);
+	}
+
+	/* mark list as empty to prevent further processing */
+	if (!features_detected) {
+		printf("VARD: data not present or invalid,\n"
+		       "      no fixup in DT for optional devices!\n");
+		tqma8mpxl_feature_list.entries = 0;
+	}
+
+#ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+	env_set("board_name", tq_bb_get_boardname());
+	env_set("board_rev", tq_get_boardname());
+#endif
+
+	tq_bb_board_late_init();
+
+	return 0;
+}
+
+#endif

arch/arm/mach-imx/imx8m/Kconfig
@@ -448,6 +448,16 @@ config TARGET_LIBREM5
 	select SUPPORT_SPL
 	select IMX8M_LPDDR4
 
+config TARGET_TQMA8MPXL
+	bool "TQ-Systems GmbH TQMa8MPxL SOM family"
+	select IMX8MP
+	select IMX8M_LPDDR4
+	select SUPPORT_SPL
+	select FSL_CAAM
+	select FSL_BLOB
+	select ARCH_MISC_INIT
+	select SPL_CRYPTO if SPL
+
 endchoice
 
 source "board/advantech/imx8mp_rsb3720a1/Kconfig"
@@ -484,5 +494,6 @@ source "board/variscite/imx8mn_var_som/Kconfig"
 source "board/toradex/verdin-imx8mm/Kconfig"
 source "board/toradex/verdin-imx8mp/Kconfig"
 source "board/voxelbotics/navqp/Kconfig"
+source "board/tq/tqma8mpxl/Kconfig"
 
 endif

board/tq/tqma8mpxl/Kconfig
@@ -1,0 +1,92 @@
+if TARGET_TQMA8MPXL
+
+config SYS_BOARD
+	default "tqma8mpxl"
+
+config SYS_VENDOR
+	default "tq"
+
+config SYS_CONFIG_NAME
+	default "tqma8mpxl"
+
+config IMX_CONFIG
+	default "board/freescale/imx8mp_evk/imximage-8mp-lpddr4.cfg"
+
+config TQMA8MPXL_DEFAULTS
+	bool
+	default y
+	select TQ_BB
+	select TQ_EEPROM
+	select TQ_RTC
+	select TQ_SDMMC
+	select TQ_SPI_NOR
+	select TQ_VARD
+	imply FSL_CAAM
+	imply SPL_CRYPTO if SPL
+	select I2C_EEPROM
+	select SPL_I2C_EEPROM if SPL
+	imply MISC
+	imply ENV_IS_NOWHERE
+	imply SYS_REDUNDAND_ENVIRONMENT
+	imply ENV_OVERWRITE
+
+choice
+	prompt "TQMa8MPxL RAM configuration"
+	default TQMA8MPXL_RAM_MULTI
+	help
+	  Select RAM configuration. Normally use default here but for
+	  specific setup it is possible to use a single RAM size.
+
+config TQMA8MPXL_RAM_MULTI
+	bool "TQMa8MPxL with 1/2/4/8 GB RAM - Single image"
+	select TQMA8MPXL_RAM_1024MB
+	select TQMA8MPXL_RAM_2048MB
+	select TQMA8MPXL_RAM_4096MB
+	select TQMA8MPXL_RAM_8192MB
+	help
+	  Build a single U-Boot for variants with 1/2/4/8 GB RAM. Select whether to use
+	  the inline ECC capability or not using config item IMX8M_DRAM_INLINE_ECC
+
+config TQMA8MPXL_RAM_SINGLE_2GB
+	bool "TQMa8MPxL with 2 GB RAM"
+	select TQMA8MPXL_RAM_2048MB
+	help
+	  Build U-Boot for variants with 2 GB RAM.  Select whether to use
+	  the inline ECC capability or not using config item IMX8M_DRAM_INLINE_ECC
+
+endchoice
+
+config TQMA8MPXL_RAM_1024MB
+	bool
+
+config TQMA8MPXL_RAM_2048MB
+	bool
+
+config TQMA8MPXL_RAM_4096MB
+	bool
+
+config TQMA8MPXL_RAM_8192MB
+	bool
+
+choice
+	prompt "TQMa8MPxL base board variant"
+	default TQMA8MPXL_BB_MBA8MPXL
+	help
+	  Select base board for TQMa8MPxL
+
+config TQMA8MPXL_BB_MBA8MPXL
+	bool "TQMa8MPxL on MBa8MPxL Starterkit"
+	help
+	  Select the MBa8MPxL starterkit for TQMa8MPxL board.
+	  This features most interfaces the CPU can provide.
+
+config TQMA8MPXL_BB_MBA8MP_RAS314
+	bool "TQMa8MPxL on MBa8MP-RAS314 SBC"
+	help
+	  Select the MBa8MP-RAS314 SBC for TQMa8MPxL board.
+
+endchoice
+
+source "board/tq/common/Kconfig"
+
+endif

board/tq/tqma8mpxl/MAINTAINERS
@@ -1,0 +1,10 @@
+TQ-Systems GmbH TQMa8MPxL module and base boards
+M:	Markus Niebel <Markus.Niebel@tq-group.com>
+S:	Maintained
+F:	board/tq/tqma8mpxl/
+F:	include/configs/tqma8mpxl.h
+F:	include/configs/tqma8mpxl-*.h
+F:	configs/tqma8mpxl*_defconfig
+F:	arch/arm/dts/imx8mp-tqma8mpxl*.dtsi
+F:	arch/arm/dts/imx8mp-tqma8mpql-mba8mp*.dts
+F:	arch/arm/dts/imx8mp-tqma8mpql-mba8mp*-u-boot.dtsi

board/tq/tqma8mpxl/Makefile
@@ -1,0 +1,30 @@
+## SPDX-License-Identifier: GPL-2.0-or-later
+##
+## Copyright (c) 2021-2025 TQ-Systems GmbH <u-boot@ew.tq-group.com>,
+## D-82229 Seefeld, Germany.
+## Author: Markus Niebel
+##
+
+obj-y += tqma8mpxl.o
+obj-$(CONFIG_TQMA8MPXL_BB_MBA8MPXL) += tqma8mpxl-mba8mpxl.o
+obj-$(CONFIG_TQMA8MPXL_BB_MBA8MP_RAS314) += tqma8mpxl-mba8mp-ras314.o
+
+ifdef CONFIG_SPL_BUILD
+obj-y += spl.o
+# the baseboard implementation for SPL is the same
+obj-$(CONFIG_TQMA8MPXL_BB_MBA8MPXL)$(CONFIG_TQMA8MPXL_BB_MBA8MP_RAS314) += spl-mba8mpxl.o
+
+ifdef CONFIG_IMX8M_DRAM_INLINE_ECC
+obj-$(CONFIG_TQMA8MPXL_RAM_1024MB) += lpddr4_timing_1gb_ecc.o
+obj-$(CONFIG_TQMA8MPXL_RAM_2048MB) += lpddr4_timing_2gb_ecc.o
+obj-$(CONFIG_TQMA8MPXL_RAM_4096MB) += lpddr4_timing_4gb_ecc.o
+obj-$(CONFIG_TQMA8MPXL_RAM_8192MB) += lpddr4_timing_8gb_ecc.o
+else
+obj-$(CONFIG_TQMA8MPXL_RAM_1024MB) += lpddr4_timing_1gb_no_ecc.o
+obj-$(CONFIG_TQMA8MPXL_RAM_2048MB) += lpddr4_timing_2gb_no_ecc.o
+obj-$(CONFIG_TQMA8MPXL_RAM_4096MB) += lpddr4_timing_4gb_no_ecc.o
+obj-$(CONFIG_TQMA8MPXL_RAM_8192MB) += lpddr4_timing_8gb_no_ecc.o
+endif
+
+endif
+

commit 9b88855bbeb151af59445ca69d625b3b406b9567

tqma8mpxl: fix condition check for tq_read_module_eeprom
tq_read_eeprom_at is for reading data from offset, not for
usage without I2C_EEPROM support. Since tq eeprom code now
uses only I2C_EEPROM / DM code the check here is not needed.
Remove wrong fallback, too.

Fixes: fb5a8f257cae ("tq: add TQMa8MPxL SoM and SBC")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma8mpxl/tqma8mpxl.c
@@ -261,10 +261,7 @@ int board_late_init(void)
 	int ret;
 	bool features_detected = false;
 
-	if (CONFIG_IS_ENABLED(I2C_EEPROM))
-		ret = tq_read_module_eeprom(&eeprom);
-	else
-		ret = tq_read_eeprom_at(0, 0, &eeprom);
+	ret = tq_read_module_eeprom(&eeprom);
 
 	if (!ret) {
 		tq_board_handle_eeprom_data(bname, &eeprom);