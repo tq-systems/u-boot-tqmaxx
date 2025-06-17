488c0835ca02c7ed5590e6884cd7c4772ca4aecb
Nora Schiffer <nora.schiffer@ew.tq-group.com>
2025-06-17 16:11:49 +0200

    board: tq: add TQMa62xx/MBa62xx
    The TQMa62xx is a SoM family with a pluggable board connector based on the
    TI AM62x SoCs. Add support for the SoM and its combination with our
    MBa62xx carrier board.
    
    Signed-off-by: Nora Schiffer <nora.schiffer@ew.tq-group.com>

---
+++ board/tq/tqma62xx/tqma62xx_mba62xx.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Board specific initialization for TQ-Systems MBa62xx
+ *
+ * Copyright (c) 2020-2025 TQ-Systems GmbH <u-boot@ew.tq-group.com>, D-82229 Seefeld, Germany.
+ * Author: Nora Schiffer
+ */
+
+#include <fdt_support.h>
+#include <init.h>
+
+#include "../common/common.h"
+#include "../common/sysinfo.h"
+#include "tqma62xx.h"
+
+int board_late_init(void)
+{
+	tq_common_sysinfo_setup();
+	tq_common_setup_boot_targets();
+	return 0;
+}
+
+#if IS_ENABLED(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	tq_common_sysinfo_ft_board_setup(blob);
+	tqma62xx_ft_board_setup(blob);
+	return 0;
+}
+#endif
