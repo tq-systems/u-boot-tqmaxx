93f3650dc0174817d2d700b02976000c938a6fe7
Matthias Schiffer <matthias.schiffer@tq-group.com>
2025-06-17 16:01:59 +0200

    board: tq: common: add common code for TI K3 SoC families
    The TI K3 SoC families the TQ-Systems SoMs based on them are similar
    enough that a lot of board code can be shared:
    
    - RAM setup based on tq_eeprom sysinfo
    - Fixup for USB dr_mode
    - boot_targets selection
    - Default environment location
    
    Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

---
+++ board/tq/common/common.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Common helpers for TQ-Systems SOMs
+ *
+ * Copyright (c) 2025 TQ-Systems GmbH <u-boot@ew.tq-group.com>, D-82229 Seefeld, Germany.
+ * Author: Matthias Schiffer
+ */
+
+void tq_common_setup_boot_targets(void);
