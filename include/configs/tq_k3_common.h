2cb9fe9cfa6d468dc7f2425b83f6782c92f106f5
Nora Schiffer <nora.schiffer@ew.tq-group.com>
2025-06-17 16:10:51 +0200

    board: tq: add TQMa64xxL/MBaX4XxL
    The TQMa64XxL is an LGA SoM based on the TI AM64x SoC family. Add support
    for the SoM and its combination with our MBaX4XxL carrier board.
    
    Signed-off-by: Nora Schiffer <nora.schiffer@ew.tq-group.com>

---
+++ include/configs/tq_k3_common.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Common configuration header file for TQ-Systems SOMs based on the
+ * Texas Instruments K3 SoCs families
+ *
+ * Copyright (c) 2025 TQ-Systems GmbH <u-boot@ew.tq-group.com>, D-82229 Seefeld, Germany.
+ * Author: Nora Schiffer
+ */
+
+#ifndef __CONFIG_TQ_K3_COMMON_H
+#define __CONFIG_TQ_K3_COMMON_H
+
+#define CFG_SYS_SDRAM_BASE	0x80000000
+#define CFG_SYS_SDRAM_BASE1	0x880000000
+
+#endif /* __CONFIG_TQ_K3_COMMON_H */
