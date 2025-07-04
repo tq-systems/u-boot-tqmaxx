tq-systems/u-boot-tqmaxx

commit dfd75e60e1663981ceecc139aa54a0a2699a194a

LFU-892 imx9: scmi: Enable SPL DCACHE for iMX95 and iMX94
Enable the DCache in SPL for iMX95 and iMX94 to improve SPL boot
time for FlexSPI/XSPI NOR

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

arch/arm/mach-imx/imx9/scmi/soc.c
@@ -13,6 +13,7 @@
 #include <asm/mach-imx/optee.h>
 #include <asm/mach-imx/ele_api.h>
 #include <asm/setup.h>
+#include <asm/system.h>
 #include <dm/uclass.h>
 #include <dm/device.h>
 #include <env_internal.h>
@@ -27,6 +28,7 @@
 #include <linux/bitfield.h>
 #include "common.h"
 #include <fdt_support.h>
+#include <time.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -329,6 +331,15 @@ static struct mm_region imx9_mem_map[] = {
 			 PTE_BLOCK_OUTER_SHARE
 	}, {
 #endif
+#if defined(CFG_SYS_SECURE_SDRAM_SIZE) || IS_ENABLED(CONFIG_XPL_BUILD)
+		/* DRAM2 */
+		.virt = CFG_SYS_SECURE_SDRAM_BASE,
+		.phys = CFG_SYS_SECURE_SDRAM_BASE,
+		.size = CFG_SYS_SECURE_SDRAM_SIZE,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_OUTER_SHARE
+	}, {
+#endif
 		/* empty entry to split table entry 5 if needed when TEEs are used */
 		0,
 	}, {
@@ -381,6 +392,33 @@ void enable_caches(void)
 	dcache_enable();
 }
 
+/*
+ * Initialize the MMU and activate cache in SPL stage
+ */
+static void spl_enable_caches(void)
+{
+	u64 pgtable_size;
+
+	if (!IS_ENABLED(CONFIG_XPL_BUILD))
+		return;
+
+	if (CONFIG_IS_ENABLED(SYS_ICACHE_OFF) || CONFIG_IS_ENABLED(SYS_DCACHE_OFF))
+		return;
+
+	pgtable_size = PGTABLE_SIZE;
+	if (pgtable_size > SZ_2M) /* Only first 2MB avaliable for SPL */
+		return;
+
+	gd->arch.tlb_size = pgtable_size;
+	gd->arch.tlb_addr = (unsigned long)CFG_SYS_SECURE_SDRAM_BASE;
+
+	dcache_enable();
+}
+
+void spl_board_prepare_for_boot(void)
+{
+	dcache_disable();
+}
 __weak int board_phys_sdram_size(phys_size_t *size)
 {
 	phys_size_t start, end;
@@ -1411,6 +1449,8 @@ int arch_cpu_init(void)
 		gpio_reset(GPIO6_BASE_ADDR);
 		gpio_reset(GPIO7_BASE_ADDR);
 #endif
+
+		spl_enable_caches();
 	}
 
 	return 0;

include/configs/imx94_evk.h
@@ -18,6 +18,9 @@
 #define PHYS_SDRAM_SIZE			     0x70000000UL /* 2GB  - 256MB DDR */
 #define PHYS_SDRAM_2_SIZE	             0x180000000 /* 8GB */
 
+#define CFG_SYS_SECURE_SDRAM_BASE	0x8A000000 /* Secure DDR region for A55, SPL could use first 2MB */
+#define CFG_SYS_SECURE_SDRAM_SIZE	0x06000000
+
 /* Using ULP WDOG for reset */
 #define WDOG_BASE_ADDR          WDG3_BASE_ADDR
 

include/configs/imx95_evk.h
@@ -23,6 +23,9 @@
 #define PHYS_SDRAM_2_SIZE		0x380000000 /* 14GB */
 #endif
 
+#define CFG_SYS_SECURE_SDRAM_BASE	0x8A000000 /* Secure DDR region for A55, SPL could use first 2MB */
+#define CFG_SYS_SECURE_SDRAM_SIZE	0x06000000
+
 #define WDOG_BASE_ADDR			WDG3_BASE_ADDR
 
 #ifdef CONFIG_ANDROID_SUPPORT

include/configs/verdin-imx95.h
@@ -24,6 +24,9 @@
 #define PHYS_SDRAM_SIZE			0x70000000 /* 2GB  - 256MB DDR */
 #define PHYS_SDRAM_2_SIZE 		0x380000000 /* 14GB */
 
+#define CFG_SYS_SECURE_SDRAM_BASE	0x8A000000 /* Secure DDR region for A55, SPL could use first 2MB */
+#define CFG_SYS_SECURE_SDRAM_SIZE	0x06000000
+
 /* Using ULP WDOG for reset */
 #define WDOG_BASE_ADDR          WDG3_BASE_ADDR
 

commit f9e875e512c8f3b9646296f5bfc647d5378a6264

LFU-894 imx9: Fix u-boot MMU pagetable issue for iMX95/94
After enable SPL dcache, will meet below random TRDC access error in SM:
DOM3 ns prv fetch from 0x020490EE0, MBC_N2=0.73, MBC2_DOM3_MEM0_BLK_CFG_W9[1]
DOM3 ns prv fetch from 0x08A2093C0, MRC_N0=0, MRC0_DOM3

It is due to secure memory (OCRAM and Secure DDR) are enabled in u-boot
MMU table. So speculative prefetch may access them.

Fixes: dfd75e60e166 (LFU-892 imx9: scmi: Enable SPL DCACHE for iMX95 and iMX94)
Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

arch/arm/mach-imx/imx9/scmi/soc.c
@@ -287,6 +287,7 @@ static struct mm_region imx9_mem_map[] = {
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	},
 #endif
+#if IS_ENABLED(CONFIG_XPL_BUILD)
 	{
 		/* OCRAM */
 		.virt = 0x20480000UL,
@@ -294,7 +295,9 @@ static struct mm_region imx9_mem_map[] = {
 		.size = 0xA0000UL,
 		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
 			 PTE_BLOCK_OUTER_SHARE
-	}, {
+	},
+#endif
+	{
 		/* AIPS */
 		.virt = 0x40000000UL,
 		.phys = 0x40000000UL,
@@ -331,7 +334,7 @@ static struct mm_region imx9_mem_map[] = {
 			 PTE_BLOCK_OUTER_SHARE
 	}, {
 #endif
-#if defined(CFG_SYS_SECURE_SDRAM_SIZE) || IS_ENABLED(CONFIG_XPL_BUILD)
+#if defined(CFG_SYS_SECURE_SDRAM_SIZE) && IS_ENABLED(CONFIG_XPL_BUILD)
 		/* DRAM2 */
 		.virt = CFG_SYS_SECURE_SDRAM_BASE,
 		.phys = CFG_SYS_SECURE_SDRAM_BASE,