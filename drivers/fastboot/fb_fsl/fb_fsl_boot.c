fbd2ebf05a8b63a1170f18d3717b8526a096ee6e
Ji Luo <ji.luo@nxp.com>
2025-07-10 17:59:16 +0900

    MA-23905-2 fix avb public key hash
    Calculate the sha256 of avb public key using the
    real public key length.
    
    Change-Id: I1598f3780a043529a457b103dcd42af7983a3daa
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- drivers/fastboot/fb_fsl/fb_fsl_boot.c
+++ drivers/fastboot/fb_fsl/fb_fsl_boot.c
@@ -430,16 +430,17 @@ int trusty_setbootparameter(uint32_t os_version,
 	}
 #else
 	uint8_t public_key_buf[AVB_MAX_BUFFER_LENGTH];
+	uint32_t public_key_size = sizeof(public_key_buf);
 #ifdef CONFIG_LOAD_KEY_FROM_RPMB
 	if (trusty_read_vbmeta_public_key(public_key_buf,
-						AVB_MAX_BUFFER_LENGTH) != 0) {
+						&public_key_size) != 0) {
 		printf("ERROR - failed to read public key for keymaster\n");
 		memset(boot_key_hash, '\0', AVB_SHA256_DIGEST_SIZE);
 	} else
 #else
 	memcpy(public_key_buf, fsl_public_key, AVB_SHA256_DIGEST_SIZE);
 #endif
-		sha256_csum_wd((unsigned char *)public_key_buf, AVB_SHA256_DIGEST_SIZE,
+		sha256_csum_wd((unsigned char *)public_key_buf, public_key_size,
 				(unsigned char *)boot_key_hash, CHUNKSZ_SHA256);
 #endif
 
