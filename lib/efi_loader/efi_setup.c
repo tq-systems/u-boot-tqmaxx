41864ca8542c49a8b6ef331361c83178f0ac1fed
Ji Luo <ji.luo@nxp.com>
2025-08-13 16:06:28 +0900

    MA-24049-10 gbl: add fastboot transport protocol
    Add the GBL fastboot transport protocol. This protocol
    defines interfaces that abstract platform fastboot
    transport implementation based on USB.
    
    This commit will hijack the complete callback of rx
    endpoint.
    
    Change-Id: I6d632356820418fab2deced67a137b9c9df2e7d4
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- lib/efi_loader/efi_setup.c
+++ lib/efi_loader/efi_setup.c
@@ -17,6 +17,7 @@
 #include <efi_gbl_os_configuration_protocol.h>
 #include <efi_gbl_boot_control_protocol.h>
 #include <efi_gbl_boot_memory_protocol.h>
+#include <efi_gbl_fastboot_transport_protocol.h>
 
 #define OBJ_LIST_NOT_INITIALIZED 1
 
@@ -389,6 +390,14 @@ efi_status_t efi_init_obj_list(void)
 	/* Register GBL boot memory protocol */
 	if (IS_ENABLED(CONFIG_EFI_GBL_BOOT_MEMORY)) {
 		ret = efi_gbl_boot_memory_register();
+		if (ret != EFI_SUCCESS) {
+			goto out;
+		}
+	}
+
+	/* Register GBL fastboot transport protocol */
+	if (IS_ENABLED(CONFIG_EFI_GBL_FASTBOOT_TRANSPORT)) {
+		ret = efi_gbl_fastboot_transport_register();
 		if (ret != EFI_SUCCESS) {
 			goto out;
 		}
