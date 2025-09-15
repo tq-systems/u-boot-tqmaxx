3954b618526eb4f9c3a4f9f77f377b6177bccea5
Ye Li <ye.li@nxp.com>
2025-09-15 16:10:12 +0800

    LFU-932 imx95: Print CPU part number name
    Decode the CPU part number from PART_NUM fuse and print it in log.
    
    Silicon Part number
    [7:6] : Package description
    [5:2] : Segment
    [1:0] : Number of A55 cores
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/mach-imx/imx9/scmi/soc.c
+++ arch/arm/mach-imx/imx9/scmi/soc.c
@@ -985,8 +985,63 @@ int power_on_m7(char *name)
 	return 0;
 }
 
+static char *get_cpu_variant_type_name(u32 type)
+{
+	if (type == MXC_CPU_IMX95) {
+		u32 val = 0, core, segment;
+		int ret;
+		static char *name = "9596";
+
+		ret = fuse_read(2, 1, &val);
+		if (ret)
+			return NULL;
+
+		val = (val >> 4) & 0xff; /* part num */
+		if (!val)
+			return NULL;
+
+		core = val & 0x3;
+		segment = (val >> 2) & 0xf;
+
+		switch (segment) {
+		case 0xa:
+			name[2] = 'T';
+			break;
+		case 0xb:
+			name[2] = 'V';
+			break;
+		case 0xc:
+			name[2] = 'C';
+			break;
+		case 0xd:
+			name[2] = 'G';
+			break;
+		case 0xe:
+			name[2] = 'I';
+			break;
+		case 0xf:
+			name[2] = 'N';
+			break;
+		default:
+			name[2] = segment + '0';
+			break;
+		}
+
+		name[3] = core * 2 + '0';
+
+		return name;
+	}
+
+	return NULL;
+}
+
 const char *get_imx_type(u32 imxtype)
 {
+	const char *name = get_cpu_variant_type_name(imxtype);
+
+	if (name)
+		return name;
+
 	switch (imxtype) {
 	case SCMI_CPU:
 		return IMX_PLAT_STR;
