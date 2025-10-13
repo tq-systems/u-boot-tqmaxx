f102291c01ee2b997babdc499f9284105d78344e
Alexander Feilke <alexander.feilke@ew.tq-group.com>
2025-10-13 08:57:44 +0200

    tqma8mxml: mfg_env: unconditionally run fastboot
    There is no need to check for check for application image
    
    Signed-off-by: Alexander Feilke <alexander.feilke@ew.tq-group.com>

--- include/configs/tqma8mxml.h
+++ include/configs/tqma8mxml.h
@@ -75,7 +75,7 @@
 #define CONFIG_REMAKE_ELF
 
 #define CONFIG_MFG_ENV_SETTINGS \
-	CONFIG_MFG_ENV_SETTINGS_DEFAULT \
+	"bootcmd_mfg=" FASTBOOT_CMD "\0" \
 	"initrd_addr=0x43800000\0" \
 	"initrd_high=0xffffffffffffffff\0" \
 	"emmc_dev=0\0"\
