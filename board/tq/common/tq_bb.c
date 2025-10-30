tq-systems/u-boot-tqmaxx

commit 39cbc72bffb6e1cecb2384646cca297af37ca4c2

tq/common: tq_bb: publish tq_get_boardname
The function is used by EEPROM CLI code, which will be
introduced by next patches.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/common/tq_bb.c
@@ -52,6 +52,11 @@ void __weak tq_bb_board_quiesce_devices(void)
 	;
 }
 
+const char *tq_get_boardname(void)
+{
+	return "INVALID";
+}
+
 const char * __weak tq_bb_get_boardname(void)
 {
 	return "INVALID";

board/tq/common/tq_bb.h
@@ -24,6 +24,7 @@ int tq_bb_board_late_init(void);
 int tq_bb_checkboard(void);
 void tq_bb_board_quiesce_devices(void);
 
+const char *tq_get_boardname(void);
 const char *tq_bb_get_boardname(void);
 
 #if IS_ENABLED(CONFIG_XPL_BUILD)

commit 43a06ee0ca1f1c00d8e6ae544be89be0a5dd49ee

tq: common: fix default implementation of tq_get_boardname
Must be annotaded with __weak,  otherwise it cannot be
overwritten per board.

Fixes: 39cbc72bffb6 ("tq/common: tq_bb: publish tq_get_boardname")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/common/tq_bb.c
@@ -52,7 +52,7 @@ void __weak tq_bb_board_quiesce_devices(void)
 	;
 }
 
-const char *tq_get_boardname(void)
+const char * __weak tq_get_boardname(void)
 {
 	return "INVALID";
 }