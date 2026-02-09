tq-systems/u-boot-tqmaxx

commit 776c9fa2eb8ae4f16bbd87759e446447ba457bc1

tqma8mpxl: use RTC driver for NXP PCF85063 adjustments
Now that we have a driver for the RTC used on this SOM
no board specific code is needed to adjust RTC quartz
capacity. Replace the TQ specific non DM code.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma8mpxl/tqma8mpxl.c
@@ -301,15 +301,13 @@ int board_late_init(void)
 	if (!ret) {
 		tq_board_handle_eeprom_data(bname, &eeprom);
 		if (tq_vard_valid(&eeprom.tq_hw_data.vard)) {
-			/*
-			 * set quartz load to 7.000 femtofarads
-			 * only if RTC is assembled, to prevent warnings
-			 */
 			if (tq_vard_has_rtc(&eeprom.tq_hw_data.vard)) {
-				if (tq_pcf85063_adjust_capacity(0,
-								 0x51,
-								 7000))
-					puts("PCF85063: adjust error\n");
+				struct udevice *rtc;
+
+				/* Force RTC probe to apply fixes configured in DTB */
+				ret = uclass_get_device_by_name(UCLASS_RTC, "rtc@51", &rtc);
+				if (ret)
+					pr_err("RTC: detection failed %d\n", ret);
 			}
 			/*
 			 * fill feature presence flags from vard

board/tq/tqma8mpxl/Kconfig
@@ -17,7 +17,6 @@ config TQMA8MPXL_DEFAULTS
 	default y
 	select TQ_BB
 	select TQ_EEPROM
-	select TQ_RTC
 	select TQ_SDMMC
 	select TQ_SPI_NOR
 	select TQ_VARD

commit dc9c1bf57b6ab5e33b1855a9bfbba3946c46cea2

tqma8mpxl: add missing include
While at it, reorder includes lexically.

Fixes: 776c9fa2eb8a ("tqma8mpxl: use RTC driver for NXP PCF85063 adjustments")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma8mpxl/tqma8mpxl.c
@@ -5,26 +5,27 @@
  * Author: Markus Niebel
  */
 
+#include <bloblist.h>
 #include <env.h>
 #include <errno.h>
+#include <mtd_node.h>
+#include <mmc.h>
+#include <spl.h>
 #include <asm/bootm.h>
 #include <asm/io.h>
 #include <asm/setup.h>
-#include <asm/mach-imx/iomux-v3.h>
-#include <asm-generic/gpio.h>
+#include <asm/arch/clock.h>
 #include <asm/arch/imx8mp_pins.h>
 #include <asm/arch/sys_proto.h>
+#include <asm-generic/gpio.h>
 #include <asm/mach-imx/boot_mode.h>
-#include <asm/mach-imx/gpio.h>
-#include <asm/mach-imx/mxc_i2c.h>
-#include <asm/arch/clock.h>
-#include <spl.h>
 #include <asm/mach-imx/dma.h>
-#include <bloblist.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <dm/uclass.h>
 #include <jffs2/load_kernel.h>
-#include <mtd_node.h>
 #include <power/pmic.h>
-#include <mmc.h>
 
 #include "../common/tq_bb.h"
 #include "../common/tq_blob.h"