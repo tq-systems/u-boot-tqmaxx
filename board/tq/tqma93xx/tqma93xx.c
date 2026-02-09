tq-systems/u-boot-tqmaxx

commit e453d0ddbe7b02dfaa1725e7b1884f8603bda49a

tqma91/93: use RTC driver for NXP PCF85063 adjustments
Now that we have a driver for the RTC used on this SOM
no board specific code is needed to adjust RTC quartz
capacity. Replace the TQ specific non DM code.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma93xx/tqma93xx.c
@@ -277,14 +277,13 @@ int board_late_init(void)
 	if (!ret) {
 		tq_board_handle_eeprom_data(bname, &eeprom);
 		if (tq_vard_valid(&eeprom.tq_hw_data.vard)) {
-			/*
-			 * set quartz load to 7.000 femtofarads
-			 * only if RTC is assembled, to prevent warnings
-			 */
 			if (tq_vard_has_rtc(&eeprom.tq_hw_data.vard)) {
-				/* set quartz load to 7.000 femtofarads */
-				if (tq_pcf85063_adjust_capacity(0, 0x51, 7000))
-					puts("PCF85063A: adjust error\n");
+				struct udevice *rtc;
+
+				/* Force RTC probe to apply fixes configured in DTB */
+				ret = uclass_get_device_by_name(UCLASS_RTC, "rtc@51", &rtc);
+				if (ret)
+					pr_err("RTC: detection failed %d\n", ret);
 			}
 			/* fill feature presence flags from vard */
 			if (!tq_vard_detect_features(&eeprom.tq_hw_data.vard,

board/tq/tqma93xx/Kconfig
@@ -14,7 +14,6 @@ config TQMA93XX_DEFAULTS
 	select IMX9_LPDDR4X
 	select TQ_BB
 	select TQ_EEPROM
-	select TQ_RTC
 	select TQ_SDMMC
 	select TQ_VARD
 	imply MISC

commit 95f15d9c75a965d06b68ca1dd7d191ccc115d2e7

tqma93xx / tqma91xx: add missing include
Fixes: e453d0ddbe7b ("tqma91/93: use RTC driver for NXP PCF85063 adjustments")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

board/tq/tqma93xx/tqma93xx.c
@@ -16,6 +16,7 @@
 #include <asm/arch/sys_proto.h>
 #include <asm/arch-imx9/ccm_regs.h>
 #include <asm/mach-imx/boot_mode.h>
+#include <dm/uclass.h>
 #include <jffs2/load_kernel.h>
 
 #include "../common/tq_bb.h"