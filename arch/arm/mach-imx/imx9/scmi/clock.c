tq-systems/u-boot-tqmaxx

commit b365160e0b6029050befd7ff289a217519335fb0

imx9: scmi: Add missing UART clocks
For some reason only clocks for LPUART1-3 has been added to
init_uart_clk(). Add the missing clocks as well.

Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

arch/arm/mach-imx/imx9/scmi/clock.c
@@ -56,6 +56,21 @@ void init_uart_clk(u32 index)
 	case 2:
 		clock_id = SCMI_CLK(LPUART3);
 		break;
+	case 3:
+		clock_id = IMX95_CLK_LPUART4;
+		break;
+	case 4:
+		clock_id = IMX95_CLK_LPUART5;
+		break;
+	case 5:
+		clock_id = IMX95_CLK_LPUART6;
+		break;
+	case 6:
+		clock_id = IMX95_CLK_LPUART7;
+		break;
+	case 7:
+		clock_id = IMX95_CLK_LPUART8;
+		break;
 	default:
 		return;
 	}

commit c2579c6235d8b8ebb4df7d9ea3e4de538a14f92a

mach-imx: use SCMI_CLK macro for all LPUART clocks
When porting to NXP downstream based on v2025.04
the SCMI_CLK macro was not used for LPUART 3...7.
This causes compile error for i.MX94

Fixes: b365160e0b60 ("imx9: scmi: Add missing UART clocks")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

arch/arm/mach-imx/imx9/scmi/clock.c
@@ -57,19 +57,19 @@ void init_uart_clk(u32 index)
 		clock_id = SCMI_CLK(LPUART3);
 		break;
 	case 3:
-		clock_id = IMX95_CLK_LPUART4;
+		clock_id = SCMI_CLK(LPUART4);
 		break;
 	case 4:
-		clock_id = IMX95_CLK_LPUART5;
+		clock_id = SCMI_CLK(LPUART5);
 		break;
 	case 5:
-		clock_id = IMX95_CLK_LPUART6;
+		clock_id = SCMI_CLK(LPUART6);
 		break;
 	case 6:
-		clock_id = IMX95_CLK_LPUART7;
+		clock_id = SCMI_CLK(LPUART7);
 		break;
 	case 7:
-		clock_id = IMX95_CLK_LPUART8;
+		clock_id = SCMI_CLK(LPUART8);
 		break;
 	default:
 		return;