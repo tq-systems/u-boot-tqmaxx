5c778312688bd46835a93b811c51f1510e0b3cb9
Alexander Feilke <alexander.feilke@ew.tq-group.com>
2026-06-02 14:54:15 +0200

    [HACK] board: tqma64xxl: work around Nanya RAM issue
    Soft reset the module if get_ram_size yields a different value
    than the expected RAM size configured in VARD.
    
    This is a temporary workaround for inaccessible RAM after successful
    RAM training and should be reworked after discussions with TI.
    
    Signed-off-by: Alexander Feilke <alexander.feilke@ew.tq-group.com>

--- board/tq/tqma64xxl/tqma64xxl.c
+++ board/tq/tqma64xxl/tqma64xxl.c
@@ -8,7 +8,16 @@
  */
 
 #include <asm/arch/hardware.h>
+#include <command.h>
 #include <init.h>
+#include <linux/sizes.h>
+#include <spl.h>
+#include <sysinfo/tq_eeprom.h>
+
+enum {
+	RAM_TYPE_DEFAULT = 1,
+	RAM_TYPE_NANYA = 2,
+};
 
 #ifdef CONFIG_XPL_BUILD
 
@@ -22,6 +31,55 @@ void spl_board_init(void)
 	val = readl(CTRLMMR_USB0_PHY_CTRL);
 	val &= ~(CORE_VOLTAGE);
 	writel(val, CTRLMMR_USB0_PHY_CTRL);
+}
+
+int spl_board_init_f(void)
+{
+	struct udevice *sysinfo;
+	u64 ram_expected_size = 0;
+	u64 ram_size = 0;
+	int ram_type = 0;
+	int ret;
+
+	ret = sysinfo_get_and_detect(&sysinfo);
+	if (ret) {
+		printf("Failed to get sysinfo data: %d\n", ret);
+		return 1;
+	}
+
+	ret = sysinfo_get_int(sysinfo, SYSID_TQ_RAM_TYPE, &ram_type);
+	if (ret) {
+		printf("Failed to get RAM type: %d.\n", ret);
+		return 1;
+	}
+
+	/**
+	 * Temporary workaround for Nanya only:
+	 * RAM is sometimes initialized in an unusable state, causing
+	 * get_ram_size to return a value of 0 or only a few bytes.
+	 * Work around this by soft resetting the module if the accessible RAM
+	 * doesnt match the configured RAM size in VARD.
+	 */
+	if (ram_type != RAM_TYPE_NANYA) 
+		return 0;
+
+	ret = sysinfo_get_uint64(sysinfo, SYSID_RAM_SIZE, &ram_expected_size);
+	if (ret) {
+		printf("Failed to get RAM size: %d. Using fallback check instead.\n", ret);
+		ram_expected_size = SZ_128M;
+	}
+
+	ram_size = get_ram_size((void *)CFG_SYS_SDRAM_BASE, ram_expected_size);
+
+	if (ram_size < ram_expected_size) {
+		printf("\nResetting to workaround Nanya RAM issue\n");
+		do_reset(NULL, 0, 0, NULL);
+		panic("Reset failed. Please reset the board!\n");
+
+		return 1;
+	}
+
+	return 0;
 }
 
 #else /* CONFIG_XPL_BUILD */
