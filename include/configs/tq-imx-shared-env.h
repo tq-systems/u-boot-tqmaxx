tq-systems/u-boot-tqmaxx

commit b7c26a2a0715205e38621c5606d0547ab6497e9d

tq-imx-shared-env: add update script for SPI NOR distroboot script
This adds the update logic incl. a configurable file name.

While at it adjust a line length in SPI env definition block.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

include/configs/tq-imx-shared-env.h
@@ -53,7 +53,7 @@
 		"else "                                                \
 			"echo ERROR: loading kernel; "                 \
 		"fi;\0"                                                \
-	"ubimtdidx=ubi\0"                                                \
+	"ubimtdidx=ubi\0"                                              \
 	"ubirootfs=rootfs.ubifs\0"                                     \
 	"ubirootfspart=ubi\0"                                          \
 	"ubirootfsvol=root\0"                                          \
@@ -71,6 +71,22 @@
 			"fi; "                                         \
 		"fi; "                                                 \
 		"setenv filesize \0"                                   \
+	"boot_script_spi=boot-ubi.scr\0"                               \
+	"update_boot_script_spi="                                      \
+		"run check_ipaddr; "                                   \
+		"setenv filesize; "                                    \
+		"if tftp ${boot_script_spi}; then "                    \
+			"if itest ${filesize} >= ${script_size_f}; then "\
+				"echo ERROR: size to large ...; exit; "\
+			"fi; "                                         \
+			"echo Write boot script to SPI NOR ...; "      \
+			"if sf probe; then "                           \
+				"run write_boot_script_spi; "          \
+			"fi; "                                         \
+		"fi; "                                                 \
+		"setenv filesize \0"                                   \
+	"write_boot_script_spi="                                       \
+		"sf update ${loadaddr} script ${filesize}\0"           \
 
 #else
 #define TQ_IMX_SHARED_UBI_ENV_SETTINGS

commit b92657862fbc744ac4c4c9746e3ce53c9a1a1f23

include: configs: tq-imx-shared-env.h: rename bootscript_name
Unify naming with our TI modules.

Fixes: b7c26a2a0715 ("tq-imx-shared-env: add update script for SPI NOR distroboot script")

Signed-off-by: Alexander Feilke <alexander.feilke@ew.tq-group.com>

include/configs/tq-imx-shared-env.h
@@ -71,11 +71,11 @@
 			"fi; "                                         \
 		"fi; "                                                 \
 		"setenv filesize \0"                                   \
-	"boot_script_spi=boot-ubi.scr\0"                               \
+	"bootscript_name=boot-ubi.scr\0"                               \
 	"update_boot_script_spi="                                      \
 		"run check_ipaddr; "                                   \
 		"setenv filesize; "                                    \
-		"if tftp ${boot_script_spi}; then "                    \
+		"if tftp ${bootscript_name}; then "                    \
 			"if itest ${filesize} >= ${script_size_f}; then "\
 				"echo ERROR: size to large ...; exit; "\
 			"fi; "                                         \